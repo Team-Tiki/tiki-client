567a532495aa9081c5b1a5d5d4cb1457e0070133
Jeongbowoon <happyanne200212@gmail/com>
2024-07-02 01:03:35 +0900

    #1 init: 절대경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  /*절대 경로 설정*/
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+      { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
+    ],
+  },
 });
