ca030753f0d92d74f542b3c01283c308bf1cdd09
Jeongbowoon <happyanne200212@gmail/com>
2024-07-02 01:09:43 +0900

    #1 init: 프리티어, 절대경로 초기 설정

--- vite.config.ts
+++ vite.config.ts
@@ -9,8 +9,9 @@ export default defineConfig({
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
-      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
-      { find: '@components', replacement: path.resolve(__dirname, 'src/components') },
+      { find: '@common', replacement: path.resolve(__dirname, 'src/common') },
+      { find: '@page', replacement: path.resolve(__dirname, 'src/page') },
+      { find: '@shared', replacement: path.resolve(__dirname, 'src/shared') },
     ],
   },
 });
