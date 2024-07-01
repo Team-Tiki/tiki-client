150eaf61cb4d037c7daa43ff5f4e2925e3cdffc7
Jeongbowoon <happyanne200212@gmail/com>
2024-07-02 02:05:34 +0900

    #1 fix: absolute path fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 const App = () => {
-  return <></>;
+  return ;
 };
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
+
   /*절대 경로 설정*/
-  resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-      { find: '@common', replacement: path.resolve(__dirname, 'src/common') },
-      { find: '@page', replacement: path.resolve(__dirname, 'src/page') },
-      { find: '@shared', replacement: path.resolve(__dirname, 'src/shared') },
-    ],
-  },
 });
