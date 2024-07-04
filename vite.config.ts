f5ab5bb5ba557429c812e7cbeb223d646f146fa2
Jeongbowoon <happyanne200212@gmail/com>
2024-07-04 18:08:53 +0900

    #8 chore: svgr 설정 file

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
@@ -9,5 +10,6 @@ export default defineConfig({
       jsxImportSource: '@emotion/react',
     }),
     tsconfigPaths(),
+    svgr(),
   ],
 });
