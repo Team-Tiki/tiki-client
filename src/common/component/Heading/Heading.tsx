309258104ea048b377b7a3d77eeaa584ab7cf20e
kimkyuhong <bobo3766@naver.com>
2024-07-05 21:37:30 +0900

    #16 feat: 공통 Heading 컴포넌트 구현

---
+++ src/common/component/Heading/Heading.tsx
@@ -0,0 +1,31 @@
+import { ComponentPropsWithoutRef } from 'react';
+
+import { headingStyle } from './heading.style';
+
+type headingType = 'H1' | 'H2' | 'H3' | 'H4' | 'H5' | 'H6' | 'H7';
+
+interface HeadingProps extends ComponentPropsWithoutRef<'h3'> {
+  heading: headingType;
+}
+
+const headingTag = {
+  H1: 'h1',
+  H2: 'h2',
+  H3: 'h3',
+  H4: 'h4',
+  H5: 'h5',
+  H6: 'h6',
+  H7: 'h6',
+};
+
+const Heading = ({ heading = 'H3', ...props }: HeadingProps) => {
+  const Tag = headingTag[heading] as React.ElementType;
+
+  return (
+    <Tag css={headingStyle[heading]} {...props}>
+      {props.children}
+    </Tag>
+  );
+};
+
+export default Heading;
