c814d113db1bfa47a93fc880eac67bd4de15afe7
namdaeun <nde40345@gmail.com>
2024-07-05 15:54:47 +0900

    #10 style: 버튼 사이즈 담은 객체 구현

---
+++ src/common/component/Button/Button.style.ts
@@ -0,0 +1,51 @@
+import { css } from '@emotion/react';
+
+import { theme } from './../../style/theme/theme';
+import { ButtonProps } from './Button';
+
+export const buttonStyle = css({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '1.6rem',
+  border: 'none',
+  borderRadius: '8px',
+});
+
+export const variantStyle = (variant: Required<ButtonProps>['variant']) => {
+  const style = {
+    primary: css({}),
+    secondary: css({}),
+  };
+  return style[variant];
+};
+
+export const sizeStyle = (size: Required<ButtonProps>['size']) => {
+  const style = {
+    xSmall: css({
+      width: '8.9rem',
+      ...theme.label.label02,
+    }),
+    small: css({
+      width: '14.7rem',
+      ...theme.heading.heading05,
+    }),
+    medium: css({
+      width: '15.8rem',
+      ...theme.label.label01,
+    }),
+    large: css({
+      width: '32rem',
+      ...theme.label.label01,
+    }),
+    xLarge: css({
+      width: '37.5rem',
+      ...theme.label.label01,
+    }),
+    xxLarge: css({
+      width: '51.1rem',
+      ...theme.label.label01,
+    }),
+  };
+  return style[size];
+};
