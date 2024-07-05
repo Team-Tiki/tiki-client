28d3d83bc04ef5d93be0ab86467ddf99b0ba0372
Jeongbowoon <happyanne200212@gmail/com>
2024-07-05 21:24:46 +0900

    #12 init: input types 설정

--- src/common/type/design.ts
+++ src/common/type/design.ts
@@ -1,9 +1,3 @@
-export interface InputSize {
-  size: 'small' | 'medium' | 'large';
-}
-/*
-small:  { width: '32rem'; height: '4.8rem' }; 
-  medium: { width: '37.5rem'; height: '3.8rem' };
-  large: { width: '51.1rem'; height: '4.8rem' };
+export type InputLength = 'short' | 'medium' | 'long';
 
-*/
+export type InputVariant = 'outline' | 'underline';
