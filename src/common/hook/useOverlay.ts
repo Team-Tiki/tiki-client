22a18222de6b1651d0fc2a6af9bbd8a4a4253420
wuzoo <wndyd9911@gmail.com>
2024-07-05 16:16:15 +0900

    #14 feat: overlay 요소의 open 상태를 다루기 위한 useOverlay 커스텀 훅 생성

---
+++ src/common/hook/useOverlay.ts
@@ -0,0 +1,21 @@
+import { useCallback, useState } from 'react';
+
+const useOverlay = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const open = useCallback(() => {
+    setIsOpen(true);
+  }, []);
+
+  const close = useCallback(() => {
+    setIsOpen(false);
+  }, []);
+
+  const toggle = useCallback(() => {
+    setIsOpen((prev) => !prev);
+  }, []);
+
+  return { isOpen, open, close, toggle };
+};
+
+export default useOverlay;
