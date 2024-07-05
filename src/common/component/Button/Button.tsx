d8ad5f7abdaa17f8ca223baaf1bf5243256e16ca
namdaeun <nde40345@gmail.com>
2024-07-05 18:33:08 +0900

    #10 fix: children 타입 변경

--- src/common/component/Button/Button.tsx
+++ src/common/component/Button/Button.tsx
@@ -1,22 +1,21 @@
-import { ButtonHTMLAttributes } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 
 import { Size } from '@/common/type/design';
 
 import { buttonStyle, colorStyle, sizeStyle, variantStyle } from './Button.style';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  children: string;
+  children: ReactNode;
   variant: 'primary' | 'secondary';
   onClick?: () => void;
   size: Size;
-  icon?: React.ReactNode;
+  icon?: ReactNode;
   color: 'blue' | 'gray' | 'black';
 }
 
-const Button = ({ variant, children, size, icon, color }: ButtonProps) => {
+const Button = ({ variant, children, size, color }: ButtonProps) => {
   return (
     <button type="button" css={[buttonStyle, variantStyle(variant), sizeStyle(size), colorStyle(color)]}>
-      {icon && <>{icon}</>}
       {children}
     </button>
   );
