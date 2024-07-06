19b4680521cefe3e8a9a8df5ceb8938be1927ea7
namdaeun <nde40345@gmail.com>
2024-07-06 17:41:02 +0900

    #25 feat: onClick prop 넘기기

--- src/page/Archiving/component/MonthHeader.tsx
+++ src/page/Archiving/component/MonthHeader.tsx
@@ -3,13 +3,15 @@ import Button from '@/common/component/Button/Button';
 import { months } from '../constant/date';
 import { headerStyle, monthBtnStyle } from './MonthHeader.style';
 
-interface MonthHeaderProps {}
+interface MonthHeaderProps {
+  onClick: (month: string) => void;
+}
 
-const MonthHeader = ({}: MonthHeaderProps) => {
+const MonthHeader = ({ onClick }: MonthHeaderProps) => {
   return (
     <section css={headerStyle}>
       {months.map((month) => (
-        <Button key={month} variant="primary" css={[monthBtnStyle]}>
+        <Button key={month} variant="primary" css={[monthBtnStyle]} onClick={() => onClick(month)}>
           {month}
         </Button>
       ))}
