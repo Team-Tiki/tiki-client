161607f414c84d859ba54a18140f7d9d7108e4d0
cindy-chaewon <chaewon1769@daum.net>
2024-07-06 13:46:54 +0900

    #13 refactor: 같은 역할의 css 속성 모아서 다른 역할의 속성과 분리

--- src/common/component/Modal/Modal.style.ts
+++ src/common/component/Modal/Modal.style.ts
@@ -6,17 +6,21 @@ export const ModalWrapper = css({
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
+
   position: 'fixed',
   top: 0,
   left: 0,
   width: '100vw',
   height: '100vh',
+
   backgroundColor: 'rgba(0, 0, 0, 0.5)',
 });
 
 export const ModalContent = css({
   paddingTop: '4.8rem',
   paddingBottom: '4.8rem',
+
   borderRadius: '16px',
+
   background: theme.colors.white,
 });
