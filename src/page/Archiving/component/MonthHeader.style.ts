e2b806a5f28b03de9b4f178a3853b824c0ce0cce
namdaeun <nde40345@gmail.com>
2024-07-06 17:49:14 +0900

    #25 style: 스타일 디테일 추가

--- src/page/Archiving/component/MonthHeader.style.ts
+++ src/page/Archiving/component/MonthHeader.style.ts
@@ -2,9 +2,13 @@ import { theme } from '@/common/style/theme/theme';
 
 export const headerStyle = {
   display: 'flex',
+
+  gap: '4.4rem',
 };
 
 export const monthBtnStyle = {
+  padding: '1rem 1.2rem',
+
   color: theme.colors.black,
   backgroundColor: theme.colors.white,
 
