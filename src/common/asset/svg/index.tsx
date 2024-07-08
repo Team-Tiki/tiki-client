6d32434d025da7630317c672ab0287c0ea6a5a5f
kimkyuhong <bobo3766@naver.com>
2024-07-09 07:23:19 +0900

    #29 feat: 필요한 svg 삽입

--- src/common/asset/svg/index.tsx
+++ src/common/asset/svg/index.tsx
@@ -1,3 +1,6 @@
 // 파스칼케이스로 어떤 svg인지 나타내기
-export { default as SmallLogo } from './logo_tiki_sm.svg';
-export { default as MiddleLogo } from './logo_tiki_md.svg';
+export { default as SmallLogo } from './logo_tiki_sm.svg?react';
+export { default as MiddleLogo } from './logo_tiki_md.svg?react';
+export { default as Add } from './icons.svg?react';
+export { default as Arrow } from './arrow.svg?react';
+export { default as LogoSymbol } from './logo_symbol.svg?react';
