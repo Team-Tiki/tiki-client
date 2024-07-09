611e0e9c80bed670bef4431b765871a8c939f8c3
wuzoo <wndyd9911@gmail.com>
2024-07-09 18:47:36 +0900

    #45 feat: isAuthPage 불리언 값을 통한 Header UI 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import { Outlet } from 'react-router-dom';
+import { css } from '@emotion/react';
+
+import { Outlet, useLocation } from 'react-router-dom';
+
+import { Header } from '@/common/component/Header/Header';
 
 const App = () => {
+  const { pathname } = useLocation();
+
+  const regexp = new RegExp('^(/signin(/.*)?|/password(/.*)?)$');
+
+  const isAuthPage = !pathname.match(regexp);
+
   return (
-    <main>
-      <Outlet />
-    </main>
+    <>
+      <Header isLogin={false} isAuthPage={isAuthPage} />
+      <main css={layoutStyle}>
+        <Outlet />
+      </main>
+    </>
   );
 };
+
+const layoutStyle = css({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+
+  minHeight: 'calc(100vh - 95px)',
+});
 
 export default App;
