ff7112fb36d85decb406de9c16cc01cada69cb4d
kimkyuhong <bobo3766@naver.com>
2024-07-10 00:27:16 +0900

    fix: test용 컴포넌트 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { Outlet } from 'react-router-dom';
 
-import LeftSidebar from './shared/component/LeftSidebar/LeftSidebar';
-
 const App = () => {
   return (
     <main>
       <Outlet />
-      <LeftSidebar />
     </main>
   );
 };
