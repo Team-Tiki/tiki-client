27321ab7933a32b1d5a1fa705bf761e3e75e8e5d
namdaeun <nde40345@gmail.com>
2024-07-09 20:51:15 +0900

    #51 feat: 스타일 적용

---
+++ src/page/archiving/Archiving.style.ts
@@ -0,0 +1,16 @@
+import { css } from '@emotion/react';
+
+export const sectionStyle = css({
+  display: 'flex',
+
+  width: '104.6rem',
+  gap: '2rem',
+
+  flexDirection: 'column',
+});
+
+export const daySectionStyle = css({
+  display: 'flex',
+
+  overflow: 'scroll',
+});
