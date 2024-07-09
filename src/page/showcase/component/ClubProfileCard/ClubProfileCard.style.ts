9f92bca01dff4459412a3eb5fa3f1ac85b9f7938
Jeongbowoon <happyanne200212@gmail/com>
2024-07-09 16:59:46 +0900

    #47 fix: css 순서 변경

--- src/page/showcase/component/ClubProfileCard/ClubProfileCard.style.ts
+++ src/page/showcase/component/ClubProfileCard/ClubProfileCard.style.ts
@@ -5,14 +5,16 @@ import { theme } from '@/common/style/theme/theme';
 export const imageStyle = css({
   width: '100%',
   height: '14rem',
+
   borderRadius: '16px 16px 0px 0px',
 });
 
 export const descriptionStyle = css({
   display: 'flex',
   flexDirection: 'column',
-  padding: '1.6rem',
   gap: '0.6rem',
+
+  padding: '1.6rem',
 });
 
 export const titleStyle = css({
@@ -20,7 +22,9 @@ export const titleStyle = css({
 });
 
 export const detailStyle = css({
-  ...theme.text.body07,
   height: '3.4rem',
+
+  ...theme.text.body07,
+
   overflow: 'hidden',
 });
