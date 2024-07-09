8a0d7a41d63121797e5517db589167d9a3c23c9b
wuzoo <wndyd9911@gmail.com>
2024-07-09 18:21:24 +0900

    #45 style: position: sticky 적용

--- src/common/component/Header/Header.style.ts
+++ src/common/component/Header/Header.style.ts
@@ -1,9 +1,16 @@
 import { css } from '@emotion/react';
 
+import { theme } from '@/common/style/theme/theme';
+
 export const headerStyle = css({
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'space-between',
+
+  position: 'sticky',
+  top: 0,
+
+  zIndex: theme.zIndex.overlayMiddle,
 
   width: '100%',
   height: '9rem',
