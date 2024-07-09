54eb9afb937f5a8bdd368e80248cd866cbf32e6a
namdaeun <nde40345@gmail.com>
2024-07-09 21:11:51 +0900

    #51 chore: 파일명 변경 & 콘솔 제거

--- src/page/archiving/util/getMonthDate.ts
+++ src/page/archiving/util/getMonthDate.ts
@@ -4,6 +4,6 @@ import { MonthType } from '@/page/archiving/type/monthType';
 export const getMonthDate = (monthName: MonthType) => {
   const monthIndex = MONTHS.indexOf(monthName);
   const year = new Date().getFullYear();
-  console.log(new Date(year, monthIndex));
+
   return new Date(year, monthIndex);
 };
