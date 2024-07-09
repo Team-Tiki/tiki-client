08eeacb952d06285f3f7fe57e6cc7d97c5432435
wuzoo <wndyd9911@gmail.com>
2024-07-09 21:29:37 +0900

    #49 refactor: variant prop을 통한 텍스트와 이미지 위치 역전

--- src/page/landing/component/FeatureSection/FeatureSection.style.ts
+++ src/page/landing/component/FeatureSection/FeatureSection.style.ts
@@ -1,11 +1,24 @@
+import { LandingFeatureSectionProps } from '@/page/landing/component/FeatureSection/FeatureSection';
 import { css } from '@emotion/react';
 
-export const sectionStyle = css({
+export const sectionStyle = (variant: LandingFeatureSectionProps['featureVariant']) =>
+  css({
+    display: 'flex',
+    flexDirection: variant === 'first' ? 'row' : 'row-reverse',
+
+    padding: '4rem 8rem',
+
+    textAlign: variant === 'first' ? 'start' : 'end',
+
+    scrollSnapAlign: 'start',
+
+    '& > p': {
+      placeSelf: 'end',
+    },
+  });
+
+export const textWrapperStyle = css({
   display: 'flex',
   flexDirection: 'column',
-  gap: '2.4rem',
-
-  padding: '4rem 8rem',
-
-  scrollSnapAlign: 'start',
+  gap: '1.6rem',
 });
--- src/page/landing/component/FeatureSection/FeatureSection.tsx
+++ src/page/landing/component/FeatureSection/FeatureSection.tsx
@@ -1,23 +1,27 @@
-import { sectionStyle } from '@/page/landing/component/FeatureSection/FeatureSection.style';
+import { sectionStyle, textWrapperStyle } from '@/page/landing/component/FeatureSection/FeatureSection.style';
 
 import { ComponentPropsWithoutRef } from 'react';
 
 import Heading from '@/common/component/Heading/Heading';
 import Text from '@/common/component/Text/Text';
 
-interface LandingFeatureSectionProps extends ComponentPropsWithoutRef<'section'> {
+export interface LandingFeatureSectionProps extends ComponentPropsWithoutRef<'section'> {
   title: string;
   description: string;
   imgUrl?: string;
+  featureVariant: 'first' | 'second';
 }
 
-const LandingFeatureSection = ({ title, description }: LandingFeatureSectionProps) => {
+const LandingFeatureSection = ({ title, description, featureVariant }: LandingFeatureSectionProps) => {
   return (
-    <section css={sectionStyle}>
-      <Heading tag="H2">{title}</Heading>
-      <Text tag="body1" css={{ fontWeight: 400 }}>
-        {description}
-      </Text>
+    <section css={sectionStyle(featureVariant)}>
+      <div css={textWrapperStyle}>
+        <Heading tag="H2">{title}</Heading>
+        <Text tag="body2" css={{ fontWeight: 400, whiteSpace: 'pre-line' }}>
+          {description}
+        </Text>
+      </div>
+      <p>IMAGE</p>
     </section>
   );
 };
