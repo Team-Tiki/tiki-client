e4ef6ff350ae90ae9954bf91949213522d76f089
namdaeun <nde40345@gmail.com>
2024-07-10 23:14:14 +0900

    #64 feat: 선택된 날에 svg 배치

--- src/page/archiving/component/DaySection/DaySection.tsx
+++ src/page/archiving/component/DaySection/DaySection.tsx
@@ -1,5 +1,9 @@
 import { bodyStyle, dayBtnStyle, dayStyle } from '@/page/archiving/component/DaySection/DaySection.style';
 
+import Circle from '@/common/asset/svg/circle.svg?react';
+import Flex from '@/common/component/Flex/Flex';
+import { theme } from '@/common/style/theme/theme';
+
 interface DaySectionProps {
   day: number;
   isClicked: boolean;
@@ -13,6 +17,12 @@ const DaySection = ({ day, isClicked, isEven, onDayClick }: DaySectionProps) => 
       <button css={dayBtnStyle(isClicked)} onClick={onDayClick}>
         {day}
       </button>
+      {isClicked && (
+        <Flex
+          styles={{ position: 'relative', direction: 'column', align: 'center', justify: 'center', bottom: '0.5rem' }}>
+          <Circle width={9} height={8} css={{ backgroundColor: 'transparent', zIndex: theme.zIndex.overlayTop }} />
+        </Flex>
+      )}
       <div css={bodyStyle(isEven)} />
     </article>
   );
