8000b40157a6daad2c5ced1ee5db4d18ef84ddd2
Jeongbowoon <happyanne200212@gmail/com>
2024-07-10 16:54:25 +0900

    #62 feat: storybook 세팅

---
+++ src/story/page/showcase/AdvBanner.stories.tsx
@@ -0,0 +1,21 @@
+import AdvBanner from '@/page/showcase/component/AdvBanner/AdvBanner';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Logo from '@/common/asset/svg/calendar.svg';
+
+const meta = {
+  title: 'page/showcase/AdvBanner',
+  component: AdvBanner,
+
+  args: {
+    title: 'TikiTikiTikiTikiTikiTikiTiki',
+    detail: '동아리 모집 마감까지 D-3동아리 모집 마감까지 D-3동아리 모집 마감까지 D-3',
+    imageUrl: Logo,
+  },
+  argTypes: {},
+} satisfies Meta<typeof AdvBanner>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
