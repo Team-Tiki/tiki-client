92dda5ba7acf0c447bdc4dfec712bfddfdc3ae55
namdaeun <nde40345@gmail.com>
2024-07-11 01:56:12 +0900

    #64 fix: 스토리북 인자 수정

--- src/story/page/archiving/TimeBlock.stories.tsx
+++ src/story/page/archiving/TimeBlock.stories.tsx
@@ -1,6 +1,8 @@
 import TimeBlock from '@/page/archiving/component/TimeBlock/TimeBlock';
 import { Meta, StoryObj } from '@storybook/react';
 
+import { theme } from '@/common/style/theme/theme';
+
 const meta = {
   title: 'Page/Archiving/TimeBlock',
   component: TimeBlock,
@@ -9,17 +11,11 @@ const meta = {
   },
   args: {
     children: ['타임 블록'],
-    width: 10,
   },
   argTypes: {
     children: {
       control: {
         type: 'text',
-      },
-    },
-    width: {
-      control: {
-        type: 'number',
       },
     },
   },
@@ -29,5 +25,11 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    children: '타임 블록',
+    startDate: new Date(2022, 0, 1),
+    endDate: new Date(2022, 0, 2),
+    color: theme.colors.blue,
+    floor: 0,
+  },
 };
