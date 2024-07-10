8c8a04d3dbca6a0f0327cde6c1930b8efaac7863
namdaeun <nde40345@gmail.com>
2024-07-11 01:16:29 +0900

    #64 feat: 선택한 날짜에 점선 내려옴

--- src/page/archiving/component/DaySection/DaySection.tsx
+++ src/page/archiving/component/DaySection/DaySection.tsx
@@ -1,6 +1,7 @@
 import { bodyStyle, dayBtnStyle, dayStyle } from '@/page/archiving/component/DaySection/DaySection.style';
 
 import Circle from '@/common/asset/svg/circle.svg?react';
+import Dotted from '@/common/asset/svg/dotted_line.svg?react';
 import Flex from '@/common/component/Flex/Flex';
 import { theme } from '@/common/style/theme/theme';
 
@@ -18,10 +19,19 @@ const DaySection = ({ day, isClicked, isEven, onDayClick }: DaySectionProps) => 
         {day}
       </button>
       {isClicked && (
-        <Flex
-          styles={{ position: 'relative', direction: 'column', align: 'center', justify: 'center', bottom: '0.5rem' }}>
-          <Circle width={9} height={8} css={{ backgroundColor: 'transparent', zIndex: theme.zIndex.overlayTop }} />
-        </Flex>
+        <>
+          <Flex
+            styles={{
+              position: 'relative',
+              direction: 'column',
+              align: 'center',
+              justify: 'center',
+              bottom: '0.5rem',
+            }}>
+            <Circle width={9} height={8} css={{ backgroundColor: 'transparent', zIndex: theme.zIndex.overlayTop }} />
+            <Dotted width={2} height={'80%'} css={{ display: 'flex', zIndex: theme.zIndex.overlayTop }} />
+          </Flex>
+        </>
       )}
       <div css={bodyStyle(isEven)} />
     </article>
