51dd94ce02b835a38fb5ef6fc153dfa717a45075
namdaeun <nde40345@gmail.com>
2024-07-12 03:02:16 +0900

    #76 fix: 스타일 수정사항 반영

--- src/page/archiving/component/DaySection/DaySection.style.ts
+++ src/page/archiving/component/DaySection/DaySection.style.ts
@@ -4,30 +4,25 @@ import { theme } from '@/common/style/theme/theme';
 
 export const dayStyle = (isEven: boolean) =>
   css({
-    dixplay: 'flex',
-
     width: '6rem',
 
     backgroundColor: isEven ? theme.colors.white : theme.colors.gray_100,
   });
 
-export const dayBtnStyle = (isClicked: boolean) =>
-  css({
-    display: 'flex',
+export const dayHeaderStyle = css({
+  width: '6rem',
 
-    width: '6rem',
+  padding: '0.8rem 2.6rem',
+  justifyContent: 'center',
 
-    padding: '0.8rem 2.6rem',
-    justifyContent: 'center',
+  border: 'none',
 
-    border: 'none',
+  color: theme.colors.black,
+  backgroundColor: theme.colors.gray_100,
+  ...theme.text.body04,
 
-    color: isClicked ? theme.colors.blue_900 : theme.colors.black,
-    backgroundColor: theme.colors.gray_100,
-    ...theme.text.body04,
-
-    cursor: 'pointer',
-  });
+  cursor: 'pointer',
+});
 
 export const bodyStyle = (isEven: boolean) =>
   css({
--- src/page/archiving/component/DaySection/DaySection.tsx
+++ src/page/archiving/component/DaySection/DaySection.tsx
@@ -1,34 +1,18 @@
-import {
-  bodyStyle,
-  dayBtnStyle,
-  dayStyle,
-  selectedDayStyle,
-} from '@/page/archiving/component/DaySection/DaySection.style';
+import { bodyStyle, dayHeaderStyle, dayStyle } from '@/page/archiving/component/DaySection/DaySection.style';
 
-import Circle from '@/common/asset/svg/circle.svg?react';
-import Dotted from '@/common/asset/svg/dotted_line.svg?react';
+import Flex from '@/common/component/Flex/Flex';
 
 interface DaySectionProps {
   day: number;
-  isClicked: boolean;
   isEven: boolean;
-  onDayClick: () => void;
 }
 
-const DaySection = ({ day, isClicked, isEven, onDayClick }: DaySectionProps) => {
+const DaySection = ({ day, isEven }: DaySectionProps) => {
   return (
-    <div css={dayStyle(isEven)}>
-      <button css={dayBtnStyle(isClicked)} type="button" onClick={onDayClick}>
-        {day}
-      </button>
-      {isClicked && (
-        <div css={selectedDayStyle}>
-          <Circle width={8} height={8} css={{ backgroundColor: 'transparent' }} />
-          <Dotted width={2} height={'80%'} css={{ display: 'flex' }} />
-        </div>
-      )}
-      <div css={bodyStyle(isEven)} />
-    </div>
+    <Flex css={dayStyle(isEven)}>
+      <Flex css={dayHeaderStyle}>{day}</Flex>
+      <Flex css={bodyStyle(isEven)} />
+    </Flex>
   );
 };
 
