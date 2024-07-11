0975acada8bfb02288344bea0ada1a75ac09cf9e
Jeongbowoon <happyanne200212@gmail/com>
2024-07-11 14:26:46 +0900

    #70 feat: slider 구현

---
+++ src/page/showcase/component/BannerSlider/BannerSlider.style.ts
@@ -0,0 +1,45 @@
+import { css } from '@emotion/react';
+
+import { theme } from '@/common/style/theme/theme';
+
+export const arrowStyle = css({
+  position: 'absolute',
+  top: '9.6rem',
+  zIndex: theme.zIndex.overlayBottom,
+  width: '3.2rem',
+  height: '3.2rem',
+  background: 'rgb(0,0,0,0.3)',
+  borderRadius: '16px',
+  border: 'none',
+
+  cursor: 'pointer',
+});
+
+export const dotsContaierStyle = css({
+  position: 'absolute',
+  bottom: '1rem',
+
+  '& > li': {
+    width: '0.5rem',
+  },
+
+  '& > li button:before': {
+    color: theme.colors.white,
+    opacity: 0.3,
+  },
+  '& > li:hover': {
+    '& > button:before': { color: theme.colors.white, opacity: 0.8 },
+  },
+
+  '& > li.slick-active button:before': {
+    color: theme.colors.white,
+    opacity: 0.8,
+  },
+});
+
+export const dotsStyle = css({
+  backgroundColor: 'blue',
+  padding: '1rem',
+  color: 'blue',
+  border: '1px solid black',
+});
--- src/page/showcase/component/BannerSlider/BannerSlider.tsx
+++ src/page/showcase/component/BannerSlider/BannerSlider.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import AdvBanner from '@/page/showcase/component/AdvBanner/AdvBanner';
+import { arrowStyle, dotsContaierStyle, dotsStyle } from '@/page/showcase/component/BannerSlider/BannerSlider.style';
 import { ADVBANNER } from '@/page/showcase/constant/dummy';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
@@ -16,18 +17,7 @@ interface ArrowProps {
 const NextArrow: React.FC<ArrowProps> = (props) => {
   const { onClick } = props;
   return (
-    <button
-      onClick={onClick}
-      css={{
-        position: 'absolute',
-        right: '1.6rem',
-        top: '9.6rem',
-        width: '3.2rem',
-        height: '3.2rem',
-        background: 'rgb(0,0,0,0.3)',
-        borderRadius: '16px',
-        border: 'none',
-      }}>
+    <button onClick={onClick} css={[arrowStyle, { right: '1.6rem' }]}>
       <RightArrow />
     </button>
   );
@@ -36,7 +26,7 @@ const NextArrow: React.FC<ArrowProps> = (props) => {
 const PrevArrow: React.FC<ArrowProps> = (props) => {
   const { onClick } = props;
   return (
-    <button onClick={onClick} css={{ position: 'absolute', left: '1.6rem', top: '9.6rem', zIndex: 3 }}>
+    <button onClick={onClick} css={[arrowStyle, { left: '1.6rem' }]}>
       <LeftArrow />
     </button>
   );
@@ -46,38 +36,32 @@ const BannerSlider = () => {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
+    appendDots: (dots: any) => (
+      <ul className="dotsUl" css={dotsContaierStyle}>
+        {dots}
+      </ul>
+    ),
+    dotsStyle: 'dotsUl',
   };
   return (
-    <Slider {...settings}>
-      <AdvBanner
-        title={ADVBANNER[0].name}
-        detail={ADVBANNER[0].overview}
-        imageUrl={ADVBANNER[0].imageUrl}
-        externUrl={''}
-      />
-      <AdvBanner
-        title={ADVBANNER[1].name}
-        detail={ADVBANNER[1].overview}
-        imageUrl={ADVBANNER[1].imageUrl}
-        externUrl={''}
-      />
-      <AdvBanner
-        title={ADVBANNER[2].name}
-        detail={ADVBANNER[2].overview}
-        imageUrl={ADVBANNER[2].imageUrl}
-        externUrl={''}
-      />
-      <AdvBanner
-        title={ADVBANNER[3].name}
-        detail={ADVBANNER[3].overview}
-        imageUrl={ADVBANNER[3].imageUrl}
-        externUrl={''}
-      />
+    <Slider {...settings} css={{ width: '100%' }}>
+      {ADVBANNER.map((advBanner) => {
+        return (
+          <AdvBanner
+            title={advBanner.name}
+            detail={advBanner.overview}
+            imageUrl={advBanner.imageUrl}
+            externUrl={advBanner.externUrl}
+          />
+        );
+      })}
     </Slider>
   );
 };
