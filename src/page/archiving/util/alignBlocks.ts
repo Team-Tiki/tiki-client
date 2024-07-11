0d2179f97803def0d5168df7aae9a486ce1f88f2
namdaeun <nde40345@gmail.com>
2024-07-11 16:51:11 +0900

    #76 refactor: 블록 배치 로직 리팩토링

--- src/page/archiving/util/alignBlocks.ts
+++ src/page/archiving/util/alignBlocks.ts
@@ -1,42 +1,46 @@
 import { TIME_BLOCK } from '@/page/archiving/constant/timeBlock';
 import { MonthType } from '@/page/archiving/type/monthType';
 
+const getDaysArray = (startDate: Date, endDate: Date): number[] => {
+  const days = [];
+  const date = new Date(startDate);
+  while (date <= endDate) {
+    days.push(date.getDate());
+    date.setDate(date.getDate() + 1);
+  }
+  return days;
+};
+
 export const alignBlocks = (endDay: Date, selectedMonth: MonthType, currentYear: number) => {
   const timeTable: boolean[][] = Array.from({ length: endDay.getDate() + 1 }, () =>
     Array(TIME_BLOCK.length).fill(false)
   );
   const floors: { [key: string]: number } = {};
+  const clickedMonth = parseInt(selectedMonth.split('월')[0]);
 
   TIME_BLOCK.forEach((block) => {
     const blockMonth = block.startDate.getMonth() + 1;
-    const clickedMonth = parseInt(selectedMonth.split('월')[0]);
 
     if (blockMonth === clickedMonth && block.startDate.getFullYear() === currentYear) {
+      const days = getDaysArray(block.startDate, block.endDate);
       let floor = 0;
-      const date = new Date(block.startDate);
+
       for (let depth = 1; depth <= TIME_BLOCK.length; depth++) {
-        let placeable = true;
-        for (date; date <= block.endDate; date.setDate(date.getDate() + 1)) {
-          const day = date.getDate();
-          if (timeTable[day][depth]) {
-            // 만약 day일 j층에 블록이 배치되어있다면 다음 층 탐색
-            placeable = false;
-            break;
-          }
-        }
-        if (placeable) {
+        // 해당 층에 블록을 배치할 수 있는지 확인
+        if (days.every((day) => !timeTable[day][depth])) {
           floor = depth;
           break;
         }
       }
       if (floor !== 0) {
-        for (let date = new Date(block.startDate); date <= block.endDate; date.setDate(date.getDate() + 1)) {
-          const day = date.getDate();
+        // 블록의 floor이 정해진 다음 블록 배치
+        days.forEach((day) => {
           timeTable[day][floor] = true;
-        }
+        });
         floors[block.id] = floor;
       }
     }
   });
+
   return floors;
 };
