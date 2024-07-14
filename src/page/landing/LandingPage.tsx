411dbc48e938caf354d0e02a06c1cc127c7dc2df
wuzoo <wndyd9911@gmail.com>
2024-07-15 01:27:30 +0900

    feat: Landing 페이지 퍼블리싱

--- src/page/landing/LandingPage.tsx
+++ src/page/landing/LandingPage.tsx
@@ -1,35 +1,76 @@
-import { landingStyle, sectionStyle, startedButtonStyle } from '@/page/landing/LandingPage.style';
-import LandingFeatureSection from '@/page/landing/component/FeatureSection/FeatureSection';
+import {
+  featureSectionStyle,
+  firstImgWrapperStyle,
+  hideStyle,
+  landingStyle,
+  secondImgWrapperStyle,
+  sectionStyle,
+  startedButtonStyle,
+  textWrapperStyle,
+  viewImgStyle,
+} from '@/page/landing/LandingPage.style';
 import LandingOverview from '@/page/landing/component/Overview/Overview';
+import { TEXT } from '@/page/landing/constant';
 
+import firstCharacter from '@/common/asset/img/landing01.png';
+import secondCharacter from '@/common/asset/img/landing02.png';
+import firstView from '@/common/asset/img/service01.png';
+import secondView from '@/common/asset/img/service02.png';
+import ArrowDown from '@/common/asset/svg/arrow-down.svg?react';
 import Button from '@/common/component/Button/Button';
 import Heading from '@/common/component/Heading/Heading';
+import Text from '@/common/component/Text/Text';
 
 const LandingPage = () => {
   return (
-    <main css={landingStyle}>
+    <main id="landing_view" css={landingStyle}>
       <section css={sectionStyle}>
-        <Heading tag="H1">동아리의 모든 순간을 쉽고 즐겁게</Heading>
+        <Heading tag="H1">{TEXT.LANDING_TITLE}</Heading>
         <Heading tag="H2" css={{ fontWeight: 500 }}>
-          티키로 더 쉽고 편리한 동아리 활동을 경험하세요
+          {TEXT.LANDING_SUBTITLE}
         </Heading>
         <Button css={startedButtonStyle} variant="action">
           시작하기
         </Button>
+        <ArrowDown />
       </section>
       <LandingOverview />
-      <LandingFeatureSection
-        featureVariant="first"
-        title="동아리의 모든 순간을 쉽고 즐겁게"
-        description={'우리 동아리의 타임라인을 만들고\n 활동과 파일을 함께 기록하여\n 운영상황을 한 눈에 정리하세요.'}
-      />
-      <LandingFeatureSection
-        featureVariant="second"
-        title="교내 동아리 탐색하기"
-        description={
-          '동아리 프로필을 만들고 모아볼 수 있는 기능을 통해 \n다른 학생단체와의 파트너십과 모집부원 홍보를\n 한 번에 해결하세요.'
-        }
-      />
+      <section css={[featureSectionStyle, { textAlign: 'end' }]}>
+        <div css={textWrapperStyle}>
+          <Heading css={hideStyle} tag="H1">
+            {TEXT.FEATURE_HEADING.FIRST}
+          </Heading>
+          <Text tag="body2" css={[hideStyle, { fontWeight: 400, whiteSpace: 'pre-line' }]}>
+            {TEXT.FEATURE_CONTENT.FIRST}
+          </Text>
+        </div>
+        <div css={firstImgWrapperStyle}>
+          <img
+            css={{
+              width: '15%',
+              height: '30%',
+            }}
+            src={firstCharacter}
+            alt="티키 캐릭터 1"
+          />
+          <img css={viewImgStyle} src={firstView} alt="서비스 뷰 1" />
+        </div>
+      </section>
+      <section css={[featureSectionStyle, { textAlign: 'start' }]}>
+        <div css={textWrapperStyle}>
+          <Heading css={hideStyle} tag="H1">
+            {TEXT.FEATURE_HEADING.SECOND}
+          </Heading>
+          <Text tag="body2" css={[hideStyle, { fontWeight: 400, whiteSpace: 'pre-line' }]}>
+            {TEXT.FEATURE_CONTENT.SECOND}
+          </Text>
+        </div>
+
+        <div css={secondImgWrapperStyle}>
+          <img css={{ width: '30%', height: '30%', placeSelf: 'end' }} src={secondCharacter} alt="티키 캐릭터 2" />
+          <img css={viewImgStyle} src={secondView} alt="서비스 뷰 2" />
+        </div>
+      </section>
     </main>
   );
 };
