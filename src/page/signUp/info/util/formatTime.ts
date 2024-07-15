b5b773b9e8882e8bb2f7e12dadbe18df50c6fae4
wuzoo <wndyd9911@gmail.com>
2024-07-15 19:16:47 +0900

    #99 feat: date, time 관련 유틸 함수 생성

--- src/page/signUp/info/util/formatTime.ts
+++ src/page/signUp/info/util/formatTime.ts
@@ -2,5 +2,5 @@ export const formatTime = (time: number) => {
   const minute = Math.floor(time / 60);
   const second = time % 60;
 
-  return `${minute}:${second}`;
+  return `${minute}:${String(second).padStart(2, '0')}`;
 };
---
+++ src/page/signUp/info/util/validate.ts
@@ -0,0 +1,17 @@
+export const isValidDate = (dateString: string): boolean => {
+  const datePattern = /^\d{4}\.\d{2}\.\d{2}$/;
+
+  if (!datePattern.test(dateString)) return false;
+
+  const [year, month, day] = dateString.split('/').map(Number);
+
+  const date = new Date(year, month - 1, day);
+
+  return date.getFullYear() === year && date.getMonth() === month - 1 && date.getDate() === day;
+};
+
+export const getFormatDateString = (dateString: string): string => {
+  if (dateString.length !== 8) return dateString;
+
+  return `${dateString.slice(0, 4)}.${dateString.slice(4, 6)}.${dateString.slice(6, 8)}`;
+};
