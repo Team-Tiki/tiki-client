ed4bbeb9d870516258601cfd6ced43bed9c8bed3
Jeongbowoon <happyanne200212@gmail/com>
2024-07-16 04:24:21 +0900

    #107 refactor: 코드리뷰 반영

--- src/shared/page/errorPage/ErrorPage.tsx
+++ src/shared/page/errorPage/ErrorPage.tsx
@@ -8,19 +8,19 @@ import Text from '@/common/component/Text/Text';
 import { theme } from '@/common/style/theme/theme';
 
 const ErrorPage = () => {
-  const navi = useNavigate();
+  const navigate = useNavigate();
   return (
     <Flex
       tag="section"
       styles={{
         direction: 'column',
         align: 'center',
-        justify: 'center',
+        justify: 'space-around',
         height: '100vh',
-        gap: '1rem',
-        padding: '15rem 50.3rem',
+        gap: '1.2rem',
+        padding: '15rem 52.3rem',
       }}>
-      <img src={errorImg} alt="comingsoon character" css={{ marginBottom: '1rem' }} />
+      <img src={errorImg} alt="error character" />
       <Heading tag="H3" css={{ fontWeight: '600', whiteSpace: 'nowrap' }}>
         오류가 발생했어요
       </Heading>
@@ -30,9 +30,8 @@ const ErrorPage = () => {
       <Button
         variant="action"
         onClick={() => {
-          navi(-1);
-        }}
-        css={{ marginTop: '2rem', minWidth: '32rem' }}>
+          navigate(-1);
+        }}>
         돌아가기
       </Button>
     </Flex>
