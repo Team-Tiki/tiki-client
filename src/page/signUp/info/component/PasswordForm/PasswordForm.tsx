edddc6ef93766036678642c57d367c13d97aed8e
wuzoo <wndyd9911@gmail.com>
2024-07-15 19:08:40 +0900

    #99 feat: 비밀번호 설정 상태 관리, 유효성 검사 로직 추가

---
+++ src/page/signUp/info/component/PasswordForm/PasswordForm.tsx
@@ -0,0 +1,78 @@
+import { SignUpContext } from '@/page/signUp/info/InfoFormPage';
+import { formStyle } from '@/page/signUp/info/InfoFormPage.style';
+import { PLACEHOLDER } from '@/page/signUp/info/constant';
+import { useInput } from '@/page/signUp/info/hook/useInput';
+
+import React, { HTMLAttributes, useContext } from 'react';
+
+import Button from '@/common/component/Button/Button';
+import Flex from '@/common/component/Flex/Flex';
+import Input from '@/common/component/Input/Input';
+
+interface PasswordFormProps extends HTMLAttributes<HTMLFormElement> {
+  onComplete?: () => void;
+}
+
+const PasswordForm = ({ onComplete }: PasswordFormProps) => {
+  const { value: password, onChange: onPasswordChange, onValidate, error: passwordError } = useInput('');
+  const {
+    value: passwordChecker,
+    onChange: onPasswordCheckerChange,
+    onValidate: onCheckerValidate,
+    error: checkerError,
+  } = useInput('');
+
+  const context = useContext(SignUpContext);
+
+  if (context === undefined) throw new Error();
+
+  const formValidate = () => {
+    if (!onValidate('비밀번호를 입력해주세요') || !onCheckerValidate('비밀번호 확인을 입력해주세요')) return false;
+
+    if (!onCheckerValidate('비밀번호가 일치하지 않습니다', password !== passwordChecker)) return false;
+
+    return true;
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!formValidate()) return;
+
+    context?.onRegister((prev) => ({
+      ...prev,
+      password,
+      passwordChecker,
+    }));
+
+    onComplete?.();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} css={formStyle}>
+      <Flex styles={{ direction: 'column', width: '100%', gap: '1.2rem', grow: '1' }}>
+        <Input
+          isError={Boolean(passwordError)}
+          value={password}
+          onChange={onPasswordChange}
+          variant="underline"
+          label="비밀번호 설정"
+          placeholder={PLACEHOLDER.PASSWORD}
+        />
+        <Input
+          isError={Boolean(checkerError || passwordChecker)}
+          supportingText={checkerError || passwordError}
+          value={passwordChecker}
+          onChange={onPasswordCheckerChange}
+          variant="underline"
+          placeholder={PLACEHOLDER.PASSWORD_CONFIRM}
+        />
+      </Flex>
+      <Button type="submit" variant="primary" size="large">
+        다음
+      </Button>
+    </form>
+  );
+};
+
+export default PasswordForm;
