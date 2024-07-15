283108dec16012f2e59cfbcc4fcc37270e7755ba
namdaeun <nde40345@gmail.com>
2024-07-15 19:30:49 +0900

    #92 docs: 파일명 변경

---
+++ src/shared/hook/useGetTimeBlockQuery.ts
@@ -0,0 +1,10 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { getTimeline } from '@/shared/api/api-gateway/time-blocks/team/getTimeline';
+
+export const useGetTimeBlockQuery = (timeId: number, timeline: string, date: string) => {
+  return useQuery({
+    queryKey: ['timeBlock', timeId, timeline, date],
+    queryFn: () => getTimeline(timeId, date),
+  });
+};
