c5534db4a71e5a0310cf51903b6bfb2d0044a89c
wuzoo <wndyd9911@gmail.com>
2024-07-15 22:39:50 +0900

    #99 feat: date 자동완성 및 입력 number로 제한하는 유틸 함수 구현

--- src/page/signUp/info/util/date.ts
+++ src/page/signUp/info/util/date.ts
@@ -1,9 +1,9 @@
 export const isValidDate = (dateString: string): boolean => {
-  const datePattern = /^\d{4}\.\d{2}\.\d{2}$/;
+  const datePattern = /^\d{4}-\d{2}-\d{2}$/;
 
   if (!datePattern.test(dateString)) return false;
 
-  const [year, month, day] = dateString.split('/').map(Number);
+  const [year, month, day] = dateString.split('-').map(Number);
 
   const date = new Date(year, month - 1, day);
 
@@ -13,5 +13,11 @@ export const isValidDate = (dateString: string): boolean => {
 export const getFormatDateString = (dateString: string): string => {
   if (dateString.length !== 8) return dateString;
 
-  return `${dateString.slice(0, 4)}.${dateString.slice(4, 6)}.${dateString.slice(6, 8)}`;
+  return `${dateString.slice(0, 4)}-${dateString.slice(4, 6)}-${dateString.slice(6, 8)}`;
+};
+
+export const getFormatNumberString = (value: string, maxLength: number) => {
+  const digits = value.replace(/\D/g, '');
+
+  return digits.slice(0, maxLength);
 };
