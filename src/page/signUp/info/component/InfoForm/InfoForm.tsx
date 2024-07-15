66585b182c1762d32ee4bc8aa94a938d5363b9ef
wuzoo <wndyd9911@gmail.com>
2024-07-15 19:08:12 +0900

    #99 feat: 회원가입 상태 관리 로직, 유효성 검사 구현

---
+++ src/page/signUp/info/component/InfoForm/InfoForm.tsx
@@ -0,0 +1,145 @@
+import { SignUpContext } from '@/page/signUp/info/InfoFormPage';
+import {
+  formStyle,
+  identifyStyle,
+  selectTriggerStyle,
+  timeStyle,
+} from '@/page/signUp/info/component/InfoForm/InfoForm.style';
+import { EMAIL_EXPIRED_MESSAGE, EMAIL_REMAIN_TIME, PLACEHOLDER } from '@/page/signUp/info/constant';
+import { useInput } from '@/page/signUp/info/hook/useInput';
+import { useSelect } from '@/page/signUp/info/hook/useSelect';
+import { useTimer } from '@/page/signUp/info/hook/useTimer';
+import { formatTime } from '@/page/signUp/info/util/formatTime';
+
+import React, { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import ArrowDown from '@/common/asset/svg/arrow-down.svg?react';
+import ArrowUp from '@/common/asset/svg/arrow-up.svg?react';
+import Button from '@/common/component/Button/Button';
+import Flex from '@/common/component/Flex/Flex';
+import Input from '@/common/component/Input/Input';
+import Select from '@/common/component/Select/Select';
+import { useOutsideClick, useOverlay } from '@/common/hook';
+
+const InfoForm = () => {
+  const [isMailSent, setIsMailSent] = useState(false);
+
+  const { isOpen, close, toggle } = useOverlay();
+  const ref = useOutsideClick(close);
+  const { remainTime } = useTimer(EMAIL_REMAIN_TIME, EMAIL_EXPIRED_MESSAGE);
+
+  const { selectedItem, onSelect, error, onValidate, onReset } = useSelect('');
+  const { value: name, onChange: onNameChange, error: nameError, onValidate: onNameValidate } = useInput('');
+  const { value: birth, onChange: onBirthChange, error: birthError, onValidate: onBirthValidate } = useInput('');
+  const { value: email, onChange: onEmailChange, error: emailError, onValidate: onEmailValidate } = useInput('');
+
+  const context = useContext(SignUpContext);
+
+  if (context === undefined) throw new Error();
+
+  const navigate = useNavigate();
+
+  const formValidate = () => {
+    if (
+      !onNameValidate('이름을 입력해주세요') ||
+      !onBirthValidate('생년월일을 입력해주세요') ||
+      !onValidate() ||
+      !onEmailValidate('이메일을 입력해주세요')
+    )
+      return false;
+
+    return true;
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!formValidate()) return;
+
+    context?.onRegister((prev) => ({
+      ...prev,
+      name,
+      birth,
+      univ: selectedItem,
+      email,
+    }));
+
+    navigate('password');
+  };
+
+  return (
+    <form css={formStyle} onSubmit={handleSubmit}>
+      <Flex styles={{ direction: 'column', gap: '3.2rem' }}>
+        <Input
+          value={name}
+          onChange={onNameChange}
+          isError={Boolean(nameError)}
+          variant="underline"
+          label="이름"
+          placeholder={PLACEHOLDER.NAME}
+        />
+        <Input
+          value={birth}
+          onChange={onBirthChange}
+          isError={Boolean(birthError)}
+          variant="underline"
+          label="생년월일"
+          placeholder={PLACEHOLDER.BIRTHDAY}
+        />
+
+        <Select
+          css={{ width: '100%' }}
+          ref={ref}
+          label="학교"
+          onSelect={onSelect}
+          isOpen={isOpen}
+          trigger={
+            <button
+              type="button"
+              css={selectTriggerStyle(error)}
+              onClick={() => {
+                toggle();
+                onReset();
+              }}>
+              {selectedItem || PLACEHOLDER.SCHOOL}
+              {isOpen ? <ArrowUp /> : <ArrowDown />}
+            </button>
+          }
+          options={['인하대학교', '건국대학교', '숙명여자대학교', '시립대학교', '중앙대학교']}
+        />
+
+        <Flex styles={{ align: 'end', justify: 'space-between', width: '100%', gap: '0.8rem' }}>
+          <Input
+            value={email}
+            onChange={onEmailChange}
+            isError={Boolean(emailError)}
+            css={{ width: '30rem' }}
+            variant="underline"
+            label="학교 인증"
+            placeholder={PLACEHOLDER.VERIFY}
+          />
+          <Button
+            size="large"
+            onClick={() => {
+              setIsMailSent(true);
+            }}>
+            인증 메일 발송
+          </Button>
+        </Flex>
+        {isMailSent && (
+          <Flex css={identifyStyle}>
+            <Input css={{ width: '30rem' }} variant="underline" placeholder={PLACEHOLDER.AUTH_CODE} />
+            <span css={timeStyle}>{formatTime(remainTime)}</span>
+            <Button size="large">인증하기</Button>
+          </Flex>
+        )}
+      </Flex>
+      <Button type="submit" variant="primary" size="large">
+        다음
+      </Button>
+    </form>
+  );
+};
+
+export default InfoForm;
