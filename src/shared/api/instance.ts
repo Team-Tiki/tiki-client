e0a33e7aaf8ae3fb80c4d8b7bff5d3c6d3a35098
wuzoo <wndyd9911@gmail.com>
2024-07-17 03:16:48 +0900

    #122 feat: token 관련 인터셉터 구현 및 인스턴스 연결

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
+import { checkAndUpateToken } from '@/shared/api/interceptor';
+
 export const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_BASE_URL}/api/v1`,
 
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
   },
 });
+
+axiosInstance.interceptors.request.use(checkAndUpateToken);
