6ad4ecc39c466d27abaf5ddf62f4a9c645defa34
Jeongbowoon <happyanne200212@gmail/com>
2024-07-17 03:10:59 +0900

    #94 refactor: instance 수정

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: `http://3.37.199.133:8080/api/v1`,
+  baseURL: `${import.meta.env.VITE_BASE_URL}/api/v1`,
 
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJtZW1iZXJJZCI6OCwiaWF0IjoxNzIxMTQ2Njk2LCJleHAiOjE3MjExNTg3OTJ9.WSq8y0impB0ndsq61q8UYfdHrQ6ALhmeWZkdBJK7z4dUOlufxvEr-szHNU_hxJxuh26i17pMQutFVrvkN7uEYQ`,
+    Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
   },
 });
