9d0f71439e878317c96b46de59523e83d2545798
Jeongbowoon <happyanne200212@gmail/com>
2024-07-18 05:57:42 +0900

    #129 feat: setting modal 기능 구현

--- src/shared/component/LeftSidebar/LeftSidebarItem/SettingModal/SettingModal.style.ts
+++ src/shared/component/LeftSidebar/LeftSidebarItem/SettingModal/SettingModal.style.ts
@@ -2,14 +2,33 @@ import { css } from '@emotion/react';
 
 import { theme } from '@/common/style/theme/theme';
 
-export const containerStyle = (setOpen: boolean) =>
+export const containerStyle = (isOpen: boolean) =>
   css({
-    display: `${setOpen ? '' : 'none'}`,
+    display: `${isOpen ? '' : 'none'}`,
     padding: '0.4rem 0.8rem',
+    gap: '0.4rem',
 
-    backgroundColor: theme.colors.gray_100,
     border: `1px solid ${theme.colors.gray_300}`,
     borderRadius: '4px',
 
+    backgroundColor: theme.colors.gray_100,
+
     whiteSpace: 'nowrap',
+
+    boxShadow: '0 0.4rem 0.6rem 0 rgba(53, 63, 155, 0.15)',
   });
+
+export const contentStyle = css({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.8rem',
+
+  border: 'none',
+
+  backgroundColor: 'transparent',
+});
+
+export const textStyle = css({
+  fontSize: '1.2rem',
+  color: theme.colors.black,
+});
--- src/shared/component/LeftSidebar/LeftSidebarItem/SettingModal/SettingModal.tsx
+++ src/shared/component/LeftSidebar/LeftSidebarItem/SettingModal/SettingModal.tsx
@@ -1,16 +1,60 @@
-import { HTMLAttributes } from 'react';
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 
-import { containerStyle } from '@/shared/component/LeftSidebar/LeftSidebarItem/SettingModal/SettingModal.style';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { HTMLAttributes, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Logout from '@/common/asset/svg/logout.svg?react';
+import PWResetting from '@/common/asset/svg/password.svg?react';
+import { useOutsideClick, useOverlay } from '@/common/hook';
+
+import {
+  containerStyle,
+  contentStyle,
+  textStyle,
+} from '@/shared/component/LeftSidebar/LeftSidebarItem/SettingModal/SettingModal.style';
+import { PATH } from '@/shared/constant/path';
 
 interface SettingModalProps extends HTMLAttributes<HTMLDivElement> {
   setOpen: boolean;
+  setState: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const SettingModal = ({ setOpen, ...props }: SettingModalProps) => {
+const SettingModal = ({ setOpen, setState, ...props }: SettingModalProps) => {
+  const { isOpen, close, open } = useOverlay();
+  const settingRef = useOutsideClick(close);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (setOpen) {
+      open();
+      setState(false);
+    }
+  }, [open, setOpen, setState]);
+
   return (
-    <div css={containerStyle(setOpen)} {...props}>
-      <div>로그아웃</div>
-      <div>비밀번호 재설정</div>
+    <div ref={settingRef} css={containerStyle(isOpen)} {...props}>
+      <button css={contentStyle} onClick={close}>
+        <Logout width="1.6rem" height="1.6rem" />
+        <p
+          css={textStyle}
+          onClick={() => {
+            console.log('log out');
+          }}>
+          로그아웃
+        </p>
+      </button>
+      <button css={contentStyle} onClick={close}>
+        <PWResetting width="1.6rem" height="1.6rem" />
+        <p
+          css={textStyle}
+          onClick={() => {
+            navigate(PATH.PASSWORD_RESET);
+          }}>
+          비밀번호 재설정
+        </p>
+      </button>
     </div>
   );
 };
