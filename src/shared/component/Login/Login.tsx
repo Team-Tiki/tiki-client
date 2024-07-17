49bdf48ff12fbb2677219869e01d0ae6820eaf29
wuzoo <wndyd9911@gmail.com>
2024-07-18 04:51:23 +0900

    #122 feat: 계속하여 reissue 후 access token 최신화

--- src/shared/component/Login/Login.tsx
+++ src/shared/component/Login/Login.tsx
@@ -1,12 +1,29 @@
 import { ReactNode, useEffect } from 'react';
 
+import { axiosInstance } from '@/shared/api/instance';
 import { ACCESS_TOKEN_KEY } from '@/shared/constant/api';
 import useStore from '@/shared/store/auth';
 
 const Login = ({ children }: { children: ReactNode }) => {
   const { login } = useStore();
 
+  /** 앱잼 서버 HTTP 배포로 인해 set-cookie를 통한 refresh token 쿠키에 저장 불가 */
+  const reissue = async () => {
+    /** 추후 수정 예정 */
+    const response = await axiosInstance.get('/auth/reissue', {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('refresh')}`,
+      },
+    });
+
+    const { accessToken } = response.data.data;
+
+    axiosInstance.defaults.headers.Authorization = accessToken;
+  };
+
   useEffect(() => {
+    reissue();
+
     if (localStorage.getItem(ACCESS_TOKEN_KEY)) {
       login();
     }
