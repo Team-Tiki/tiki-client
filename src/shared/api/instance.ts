6106bb291c4963964a46846f15f999fdd52fe0a7
Jeongbowoon <happyanne200212@gmail/com>
2024-07-18 02:12:40 +0900

    #129 feat: router 추가

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+/*import axios from 'axios';
 
 export const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_BASE_URL}/api/v1`,
@@ -6,6 +6,19 @@ export const axiosInstance = axios.create({
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`,
+  },
+});
+
+export default axiosInstance;
+*/
+import axios from 'axios';
+
+export const axiosInstance = axios.create({
+  baseURL: `http://3.37.199.133:8080/api/v1`,
+
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJtZW1iZXJJZCI6OCwiaWF0IjoxNzIxMjM1ODEyLCJleHAiOjE3MjEyNDc5MDh9.CqzuiWJDsY8m-672-0qIczpQ5rVn6Xw1jukRU-gF_kJ4_LOn4pdEvFljGhm-bnErzoJh33iXo89ufyl-1_aVBg`,
   },
 });
 
