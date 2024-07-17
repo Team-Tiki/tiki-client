8b5676a83714ec45409c6d1222a8ae7e962c60f6
wuzoo <wndyd9911@gmail.com>
2024-07-18 01:47:00 +0900

    #122 feat: token interceptor를 적용시키지 않은 public instance 선언

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -4,7 +4,16 @@ import { checkAndUpateToken } from '@/shared/api/interceptor';
 
 export const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_BASE_URL}/api/v1`,
+  withCredentials: true,
 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const axiosPublicInstance = axios.create({
+  baseURL: `${import.meta.env.VITE_BASE_URL}/api/v1`,
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
