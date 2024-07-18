3aeb865736e4cce44cd3bc0900918fa81cca8139
Jeongbowoon <happyanne200212@gmail/com>
2024-07-18 16:04:52 +0900

    #129 feat: useQuery router 수정

--- src/shared/api/teams/category/index.ts
+++ src/shared/api/teams/category/index.ts
@@ -1,6 +1,8 @@
 import { axiosInstance } from '@/shared/api/instance';
+import { CategoryType } from '@/shared/api/teams/category/type';
 
-export const getCategoryList = async <T>() => {
-  const response = await axiosInstance.get<T>('/teams/category');
+export const getCategoryList = async () => {
+  const response = await axiosInstance.get<CategoryType>('/teams/category');
+  console.log(response.data);
   return response.data;
 };
