9621aea6caf42b880b0d8a943013ebcae70e647e
Jeongbowoon <happyanne200212@gmail/com>
2024-07-18 16:05:30 +0900

    #129 fix: onClick함수 위치 이동

--- src/shared/component/LeftSidebar/LeftSidebarItem/SettingModal/SettingModal.tsx
+++ src/shared/component/LeftSidebar/LeftSidebarItem/SettingModal/SettingModal.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import { HTMLAttributes, useEffect } from 'react';
+import { HTMLAttributes, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import Logout from '@/common/asset/svg/logout.svg?react';
@@ -37,26 +37,27 @@ const SettingModal = ({ setOpen, setState, ...props }: SettingModalProps) => {
 
   return (
     <div ref={settingRef} css={containerStyle(isOpen)} {...props}>
-      <button css={contentStyle} onClick={close}>
+      <button
+        css={contentStyle}
+        onClick={() => {
+          localStorage.removeItem(ACCESS_TOKEN_KEY);
+          navigate(PATH.LOGIN);
+          close();
+        }}>
         <Logout width="1.6rem" height="1.6rem" />
-        <Text
-          tag="body6"
-          css={textStyle}
-          onClick={() => {
-            localStorage.removeItem(ACCESS_TOKEN_KEY);
-            navigate(PATH.LOGIN);
-          }}>
+        <Text tag="body6" css={textStyle}>
           로그아웃
         </Text>
       </button>
-      <button css={contentStyle} onClick={close}>
+
+      <button
+        css={contentStyle}
+        onClick={() => {
+          navigate(PATH.PASSWORD_AUTH);
+          close();
+        }}>
         <PWResetting width="1.6rem" height="1.6rem" />
-        <Text
-          tag="body6"
-          css={textStyle}
-          onClick={() => {
-            navigate(PATH.PASSWORD_AUTH);
-          }}>
+        <Text tag="body6" css={textStyle}>
           비밀번호 재설정
         </Text>
       </button>
