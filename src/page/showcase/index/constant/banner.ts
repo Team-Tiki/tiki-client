4b83c26a8d24dac75609c77f8c6db00e5e91bed0
Jeongbowoon <happyanne200212@gmail/com>
2024-07-19 02:27:54 +0900

    #141 style: showcase QA

--- src/page/showcase/index/constant/banner.ts
+++ src/page/showcase/index/constant/banner.ts
@@ -1,10 +1,15 @@
+import advBannerClub1 from '@/common/asset/img/advBannerClub1.png';
+import advBannerClub2 from '@/common/asset/img/advBannerClub2.png';
+import advBannerClub3 from '@/common/asset/img/advBannerClub3.png';
+import advBannerClub4 from '@/common/asset/img/advBannerClub4.png';
+
 export const ADVBANNER = [
   {
     id: 1,
     name: 'SOPT',
     overview: '동아리 모집 마감까지 D-3',
     category: '창업/취업',
-    imageUrl: 'src/common/asset/img/advBannerClub1.png',
+    imageUrl: advBannerClub1,
     externUrl: 'https://www.sopt.org/recruit',
   },
   {
@@ -12,7 +17,7 @@ export const ADVBANNER = [
     name: '유닛',
     overview: '동아리 모집 마감까지 D-6',
     category: '스포츠/레저',
-    imageUrl: 'src/common/asset/img/advBannerClub2.png',
+    imageUrl: advBannerClub2,
     externUrl: 'https://www.sopt.org/recruit',
   },
   {
@@ -20,7 +25,7 @@ export const ADVBANNER = [
     name: 'TRIPEL',
     overview: '동아리 모집 마감까지 D-5',
     category: '문화/친목',
-    imageUrl: 'src/common/asset/img/advBannerClub3.png',
+    imageUrl: advBannerClub3,
     externUrl: 'https://www.sopt.org/recruit',
   },
   {
@@ -28,7 +33,7 @@ export const ADVBANNER = [
     name: '청춘처럼',
     overview: '동아리 모집 마감까지 D-1',
     category: '봉사/사회활동',
-    imageUrl: 'src/common/asset/img/advBannerClub4.png',
+    imageUrl: advBannerClub4,
     externUrl: 'https://www.sopt.org/recruit',
   },
 ];
