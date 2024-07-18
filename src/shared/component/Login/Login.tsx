e0df6dec5b665420dc751b30f40d8b18d518a929
wuzoo <wndyd9911@gmail.com>
2024-07-18 18:10:32 +0900

    #122 feat: 권한이 있어야 하는 라우트에서 logout 콜백으로 프로텍션 적용

--- src/shared/component/Login/Login.tsx
+++ src/shared/component/Login/Login.tsx
@@ -2,10 +2,13 @@ import { ReactNode, useEffect } from 'react';
 
 import { axiosInstance } from '@/shared/api/instance';
 import { ACCESS_TOKEN_KEY } from '@/shared/constant/api';
+import { useLogout } from '@/shared/hook/common/useLogout';
 import useStore from '@/shared/store/auth';
 
 const Login = ({ children }: { children: ReactNode }) => {
   const { login } = useStore();
+
+  const { logout } = useLogout();
 
   /** 앱잼 서버 HTTP 배포로 인해 set-cookie를 통한 refresh token 쿠키에 저장 불가 */
   const reissue = async () => {
@@ -18,18 +21,20 @@ const Login = ({ children }: { children: ReactNode }) => {
 
     const { accessToken } = response.data.data;
 
-    console.log(accessToken);
-
     axiosInstance.defaults.headers.Authorization = accessToken;
   };
 
   useEffect(() => {
+    if (!localStorage.getItem(ACCESS_TOKEN_KEY)) {
+      logout();
+    }
+
     reissue();
 
     if (localStorage.getItem(ACCESS_TOKEN_KEY)) {
       login();
     }
-  }, [login]);
+  }, [login, logout]);
 
   return <>{children}</>;
 };
