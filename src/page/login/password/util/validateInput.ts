abc07f8a468b4128c76ae7a4a2d046df9a6136d5
namdaeun <nde40345@gmail.com>
2024-07-18 17:48:38 +0900

    #108 chore: util 함수 반환 값 바로 리턴

--- src/page/login/password/util/validateInput.ts
+++ src/page/login/password/util/validateInput.ts
@@ -1,12 +1,8 @@
 export const validateEmail = (email: string) => {
   const emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-  const isEmailValid = email.trim().length > 0 && emailPattern.test(email.trim());
-
-  return isEmailValid;
+  return email.trim().length > 0 && emailPattern.test(email.trim());
 };
 
 export const validateCode = (authCode: string) => {
-  const isAuthCodeValid = authCode.trim().length > 0 && authCode.trim().length === 6;
-
-  return isAuthCodeValid;
+  return authCode.trim().length > 0 && authCode.trim().length === 6;
 };
