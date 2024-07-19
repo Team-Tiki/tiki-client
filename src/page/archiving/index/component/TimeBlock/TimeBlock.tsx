1374b248c06ed4d2cb49eda53d1b7a06ed15bafd
wuzoo <wndyd9911@gmail.com>
2024-07-20 04:41:14 +0900

    #177 feat: div 타입 상속 + rest prop 전달

--- src/page/archiving/index/component/TimeBlock/TimeBlock.tsx
+++ src/page/archiving/index/component/TimeBlock/TimeBlock.tsx
@@ -1,9 +1,9 @@
 import { blockStyle, spanStyle } from '@/page/archiving/index/component/TimeBlock/TimeBlock.style';
 import { BLOCK_TYPE } from '@/page/archiving/index/constant/blockIcon';
 
-import React, { ReactNode } from 'react';
+import React, { HTMLAttributes, ReactNode } from 'react';
 
-interface TimeBlockProps {
+interface TimeBlockProps extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
   startDate: Date;
   endDate: Date;
@@ -23,13 +23,14 @@ const TimeBlock = ({
   onBlockClick,
   isSelected = false,
   blockType,
+  ...props
 }: TimeBlockProps) => {
   const blockWidth = (new Date(endDate).getDate() - new Date(startDate).getDate() + 1) * 6;
   const startPosition = (new Date(startDate).getDate() - 1) * 6;
 
   return (
     /* eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events */
-    <div css={blockStyle(blockWidth, startPosition, floor, color, isSelected)} onClick={onBlockClick}>
+    <div css={blockStyle(blockWidth, startPosition, floor, color, isSelected)} onClick={onBlockClick} {...props}>
       {BLOCK_TYPE.find((icon) => icon.name === blockType)?.icon}
       <p css={spanStyle}>{children}</p>
     </div>
