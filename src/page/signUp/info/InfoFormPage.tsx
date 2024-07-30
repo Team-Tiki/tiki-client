6f8f5c086c016db8abb28bc643169df8767b505d
wuzoo <wndyd9911@gmail.com>
2024-07-30 17:13:05 +0900

    #185 refactor: props drilling이 일어나지 않으므로, prop을 통해 데이터 전달하도록 수정

--- src/page/signUp/info/InfoFormPage.tsx
+++ src/page/signUp/info/InfoFormPage.tsx
@@ -3,21 +3,14 @@ import InfoForm from '@/page/signUp/info/component/InfoForm/InfoForm';
 import PasswordForm from '@/page/signUp/info/component/PasswordForm/PasswordForm';
 import { useSignupMutation } from '@/page/signUp/info/hook/api/useSignupMutation';
 
-import React, { SetStateAction, createContext, useState } from 'react';
-import { useMatch, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { useMatch } from 'react-router-dom';
 
 import Flex from '@/common/component/Flex/Flex';
 import Heading from '@/common/component/Heading/Heading';
 
 import { UserInfo } from '@/shared/api/signup/info/type';
 import { PATH } from '@/shared/constant/path';
-
-type Context = {
-  userInfo: UserInfo;
-  onRegister: React.Dispatch<SetStateAction<UserInfo>>;
-};
-
-export const SignUpContext = createContext<Context | undefined>(undefined);
 
 const InfoFormPage = () => {
   const [info, setInfo] = useState<UserInfo>({
@@ -30,20 +23,12 @@ const InfoFormPage = () => {
   });
 
   const { mutate } = useSignupMutation();
-  const navigate = useNavigate();
+
   const isInfoMatched = useMatch(PATH.SIGNUP_INFO);
   const isPasswordMatched = useMatch(PATH.SIGNUP_PASSWORD);
 
-  const 로그인페이지로 = () => {
-    navigate(PATH.LOGIN);
-  };
-
   const handleComplete = () => {
-    mutate(info, {
-      onSuccess: () => {
-        로그인페이지로();
-      },
-    });
+    mutate(info);
   };
 
   return (
@@ -51,11 +36,9 @@ const InfoFormPage = () => {
       <Flex tag="section" styles={{ direction: 'column', gap: '3.2rem', height: '78rem', width: '51.1rem' }}>
         <Heading css={{ padding: '1.6rem 0' }}>회원가입</Heading>
 
-        <SignUpContext.Provider value={{ userInfo: info, onRegister: setInfo }}>
-          {isInfoMatched && <InfoForm />}
+        {isInfoMatched && <InfoForm onInfoChange={setInfo} />}
 
-          {isPasswordMatched && <PasswordForm onComplete={handleComplete} />}
-        </SignUpContext.Provider>
+        {isPasswordMatched && <PasswordForm onInfoChange={setInfo} onComplete={handleComplete} />}
       </Flex>
     </Flex>
   );
