02c4a0dbeaf91a6b2ec7a8b75142d3f3cc73f183
kimkyuhong <bobo3766@naver.com>
2024-07-31 17:06:22 +0900

    #190 style: documentFilter css 수정

--- src/page/archiving/index/component/DocumentFilter/DocumentFilter.style.ts
+++ src/page/archiving/index/component/DocumentFilter/DocumentFilter.style.ts
@@ -3,8 +3,7 @@ import { css } from '@emotion/react';
 import { theme } from '@/common/style/theme/theme';
 
 export const filterItemStyle = css({
-  display: 'flex',
-
   padding: '0.8rem',
+
   '&: hover': { color: theme.colors.blue_900 },
 });
