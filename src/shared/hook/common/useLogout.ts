a7f139bcf5f439cadf250b6c335ac160bb02c416
wuzoo <wndyd9911@gmail.com>
2024-08-04 19:57:47 +0900

    #206 refactor: localstorage and queryClient clear when logout

--- src/shared/hook/common/useLogout.ts
+++ src/shared/hook/common/useLogout.ts
@@ -1,7 +1,8 @@
 import { useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { ACCESS_TOKEN_KEY } from '@/shared/constant/api';
+import { useQueryClient } from '@tanstack/react-query';
+
 import { PATH } from '@/shared/constant/path';
 import { useAuth } from '@/shared/store/auth';
 
@@ -10,17 +11,17 @@ export const useLogout = () => {
 
   const navigate = useNavigate();
 
+  const queryClient = useQueryClient();
+
   const logout = useCallback(() => {
     onLogout();
 
-    const accessToken = localStorage.getItem(ACCESS_TOKEN_KEY);
-    const refreshToken = localStorage.getItem('refresh');
+    localStorage.clear();
 
-    if (accessToken) localStorage.removeItem(ACCESS_TOKEN_KEY);
-    if (refreshToken) localStorage.removeItem('refresh');
+    queryClient.clear();
 
     navigate(PATH.LOGIN);
-  }, [navigate, onLogout]);
+  }, [navigate, onLogout, queryClient]);
 
   return { logout };
 };
