5fb92b0f0d3dca8ec904242a64e7a2e8f9b241b4
wuzoo <wndyd9911@gmail.com>
2024-08-06 00:27:56 +0900

    #212 docs: authToken 추가, sourcesmap upload 후 delete file config 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,37 @@
 import { sentryVitePlugin } from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-    }),
-    tsconfigPaths(),
-    svgr({
-      svgrOptions: {
-        icon: true,
-        exportType: 'default',
-      },
-    }),
-    sentryVitePlugin({
-      org: 'tiki-4h',
-      project: 'tiki_web',
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
-  build: {
-    sourcemap: true,
-  },
+  return {
+    plugins: [
+      react({
+        jsxImportSource: '@emotion/react',
+      }),
+      tsconfigPaths(),
+      svgr({
+        svgrOptions: {
+          icon: true,
+          exportType: 'default',
+        },
+      }),
+      sentryVitePlugin({
+        org: 'tiki-4h',
+        project: 'tiki_web',
+        authToken: env.VITE_SENTRY_AUTH_TOKEN,
+        sourcemaps: {
+          filesToDeleteAfterUpload: '**/*.ts.map',
+        },
+      }),
+    ],
+
+    build: {
+      sourcemap: true,
+    },
+  };
 });
