e89272e6f155d1eb1b297b9efe0cb051203cb933
wuzoo <wndyd9911@gmail.com>
2024-08-06 00:11:29 +0900

    #212 docs: sentry source map config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
@@ -6,19 +6,24 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    jsxImportSource: '@emotion/react',
-  }), tsconfigPaths(), svgr({
-    svgrOptions: {
-      icon: true,
-      exportType: 'default',
-    },
-  }), sentryVitePlugin({
-    org: "tiki-4h",
-    project: "tiki_web"
-  })],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+    tsconfigPaths(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        exportType: 'default',
+      },
+    }),
+    sentryVitePlugin({
+      org: 'tiki-4h',
+      project: 'tiki_web',
+    }),
+  ],
 
   build: {
-    sourcemap: true
-  }
+    sourcemap: true,
+  },
 });
