f13bc77dcabc512c7dc69073edb8665a9dabe7ed
wuzoo <wndyd9911@gmail.com>
2024-08-05 23:37:00 +0900

    #212 feat: HTTP 통신 시 발생하는 별도의 API Error를 위한 에러 객체 HTTPError 인스턴스 생성

---
+++ src/shared/api/HTTPError.ts
@@ -0,0 +1,45 @@
+import { HTTP_STATUS_CODE } from '@/shared/constant/api';
+
+/** HTTP 통신 시 발생하는 API Error를 별도의 객체로 나타내기 위한 에러 객체입니다. */
+export class HTTPError extends Error {
+  statusCode: number;
+
+  code?: number;
+
+  constructor(statusCode: number, mesaage?: string, code?: number) {
+    super(mesaage);
+
+    let name = 'HTTPError';
+
+    switch (statusCode) {
+      case HTTP_STATUS_CODE.BAD_REQUEST: {
+        name += ': BAD_REQUESET';
+        break;
+      }
+      case HTTP_STATUS_CODE.UNAUTHORIZED: {
+        name += ': UNAUTHORIZED';
+        break;
+      }
+      case HTTP_STATUS_CODE.FORBIDDEN: {
+        name += ': FORBIDDEN';
+        break;
+      }
+      case HTTP_STATUS_CODE.NOT_FOUND: {
+        name += ': NOT_FOUND';
+        break;
+      }
+      case HTTP_STATUS_CODE.CONTENT_TOO_LARGE: {
+        name += ': CONTENT_TOO_LARGE';
+        break;
+      }
+      case HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR: {
+        name += ': INTERNAL_SERVER_ERROR';
+        break;
+      }
+    }
+
+    this.name = name;
+    this.statusCode = statusCode;
+    this.code = code;
+  }
+}
