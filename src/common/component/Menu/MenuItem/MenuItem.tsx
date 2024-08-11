d7e009f1d2f2f2c8f1eaa355a2849efd49488fd3
Jeongbowoon <happyanne200212@gmail/com>
2024-08-11 09:06:53 -0700

    #219 feat: mouse, key event 추가

--- src/common/component/Menu/MenuItem/MenuItem.tsx
+++ src/common/component/Menu/MenuItem/MenuItem.tsx
@@ -1,17 +1,23 @@
 import { ButtonHTMLAttributes, ReactNode } from 'react';
 
-import { variantStyle } from '@/common/component/Menu/MenuItem/MenuItem.style';
+import { containerStyle, variantStyle } from '@/common/component/Menu/MenuItem/MenuItem.style';
 
 export interface MenuItemProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'primary';
-  // onClick?: () => void; ??
-  onKeyDown?: () => void;
+  onSelect?: () => void;
   LeftIcon?: ReactNode;
 }
 
-const MenuItem = ({ variant = 'primary', LeftIcon, children, ...props }: MenuItemProps) => {
+const MenuItem = ({ variant = 'primary', LeftIcon, onSelect, children, ...props }: MenuItemProps) => {
   return (
-    <button type="button" css={[variantStyle(variant)]} {...props}>
+    <button
+      type="button"
+      css={[containerStyle, variantStyle(variant)]}
+      onKeyDown={(e: React.KeyboardEvent<HTMLButtonElement>) => {
+        if (e.key === 'Enter') onSelect;
+      }}
+      onClick={onSelect}
+      {...props}>
       {LeftIcon}
       <p>{children}</p>
     </button>
