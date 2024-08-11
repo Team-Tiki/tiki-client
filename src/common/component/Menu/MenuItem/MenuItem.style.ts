76aba4a82289c3bc242069701b17596337c1b674
Jeongbowoon <happyanne200212@gmail/com>
2024-08-11 09:04:06 -0700

    #219 style: 전체적인 style 분리, 디테일 수정

--- src/common/component/Menu/MenuItem/MenuItem.style.ts
+++ src/common/component/Menu/MenuItem/MenuItem.style.ts
@@ -1,29 +1,32 @@
+import { css } from '@emotion/react';
+
 import { MenuItemProps } from '@/common/component/Menu/MenuItem/MenuItem';
 import { theme } from '@/common/style/theme/theme';
 
+export const containerStyle = css({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '0.8rem',
+});
+
 export const variantStyle = (variant: Required<MenuItemProps>['variant']) => {
-  //여기서 글씨체도!
   const style = {
     primary: {
-      display: 'flex',
-      alignItems: 'center',
-      gap: '0.8rem',
-
       padding: '0.8rem',
 
       backgroundColor: theme.colors.gray_100,
       borderRadius: '8px',
       border: 0,
 
+      ...theme.text.body06,
       color: theme.colors.black,
       fontWeight: 600,
 
       cursor: 'pointer',
 
-      ':hover': {
-        backgroundColor: theme.colors.blue_100,
-      },
-      ':focus': {
+      ':hover, :focus': {
+        outline: 0,
         backgroundColor: theme.colors.blue_100,
       },
     },
