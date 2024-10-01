7fa74b8b7570edfe3e3eb48597857b7f4b4dac1c
Jeongbowoon <happyanne200212@gmail/com>
2024-09-30 23:28:34 -0700

    #199 feat: Club profile에 onKeyDown 추가

--- src/page/showcase/index/component/ClubProfileCard/ClubProfileCard.tsx
+++ src/page/showcase/index/component/ClubProfileCard/ClubProfileCard.tsx
@@ -1,13 +1,13 @@
+import defaultImage from '@/common/asset/svg/ic_default_profile.svg';
+import Heading from '@/common/component/Heading/Heading';
+import Text from '@/common/component/Text/Text';
+
 import {
   containerStyle,
   descriptionStyle,
   detailStyle,
   imageStyle,
 } from '@/page/showcase/index/component/ClubProfileCard/ClubProfileCard.style';
-
-import defaultImage from '@/common/asset/svg/ic_default_profile.svg';
-import Heading from '@/common/component/Heading/Heading';
-import Text from '@/common/component/Text/Text';
 
 interface ClubProfileCardProps {
   title: string;
@@ -19,7 +19,16 @@ interface ClubProfileCardProps {
 const ClubProfileCard = ({ title, detail, imageUrl, onClick }: ClubProfileCardProps) => {
   return (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions, jsx-a11y/no-noninteractive-tabindex
-    <div role="link" css={containerStyle} onClick={onClick} tabIndex={0}>
+    <div
+      role="link"
+      css={containerStyle}
+      onClick={onClick}
+      tabIndex={0}
+      onKeyDown={(e: React.KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === 'Enter' && onClick) {
+          onClick();
+        }
+      }}>
       <img src={imageUrl ? imageUrl : defaultImage} alt={`${title}-image`} css={imageStyle} />
       <div css={descriptionStyle}>
         <Heading tag="H6">{title}</Heading>
