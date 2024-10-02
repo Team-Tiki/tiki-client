8755b96fd24ecfc16faab6d93382d9e20dce2c9e
wuzoo <wndyd9911@gmail.com>
2024-10-02 13:53:59 +0900

    feat: context를 통해 현재 보여지는 itemRef 수정

---
+++ src/common/component/Carousel/CarouselItem.tsx
@@ -0,0 +1,27 @@
+import { PropsWithChildren, useContext } from 'react';
+
+import { CarouselContext } from '@/common/component/Carousel/Carousel';
+import { itemStyle } from '@/common/component/Carousel/Carousel.style';
+
+type CarouselItemProps = PropsWithChildren<{ index: number }>;
+
+const CarouselItem = ({ index, children }: CarouselItemProps) => {
+  const { currentIndex, itemRef } = useContext(CarouselContext);
+
+  return (
+    <div
+      ref={(node) => {
+        if (currentIndex === index + 1) {
+          itemRef.current = node as HTMLDivElement;
+        }
+        return () => {
+          itemRef.current = null;
+        };
+      }}
+      css={itemStyle}>
+      {children}
+    </div>
+  );
+};
+
+export default CarouselItem;
