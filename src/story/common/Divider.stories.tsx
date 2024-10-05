7356d019aca31e019d5dbe68d49348a914b84c55
namdaeun <nde40345@gmail.com>
2024-10-05 20:33:23 +0900

    #237 feat: Divider 스토리북 세팅

---
+++ src/story/common/Divider.stories.tsx
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Divider from '@/common/component/Divider/Divider';
+
+const meta = {
+  title: 'Common/Divider',
+  component: Divider,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    type: {
+      control: { type: 'radio' },
+      options: ['horizontal', 'vertical'],
+    },
+    size: {
+      control: { type: 'number' },
+    },
+  },
+  args: {
+    type: 'horizontal',
+    size: 50,
+  },
+} satisfies Meta<typeof Divider>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Horizontal: Story = {
+  args: {
+    type: 'horizontal',
+    size: 50,
+  },
+};
+
+export const Vertical: Story = {
+  args: {
+    type: 'vertical',
+    size: 3,
+  },
+};
