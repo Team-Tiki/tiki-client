a3837e67411ffe4c3006fb0f23dc5b779e292f57
Jeongbowoon <happyanne200212@gmail/com>
2024-10-07 23:53:20 -0700

    #242 style: switch 스타일 지정

--- src/shared/component/Switch/Switch.style.ts
+++ src/shared/component/Switch/Switch.style.ts
@@ -3,12 +3,27 @@ import { css } from '@emotion/react';
 import { theme } from '@/common/style/theme/theme';
 
 export const containerStyle = css({
+  display: 'flex',
+  height: '100%',
+
   border: `1px solid ${theme.colors.gray_200}`,
   borderRadius: '100px',
+  overflow: 'hidden',
 });
 
-export const leftContentStyle = css({
-  borderRight: `1px solid ${theme.colors.gray_200}`,
-});
-
-export const rightContentStyle = css({});
+export const contentStyle = (side: 'left' | 'right', isClicked: boolean) => {
+  const sidePadding = isClicked ? 1 : 1.6;
+  const bgColor = isClicked ? theme.colors.gray_100 : theme.colors.white;
+  const style = {
+    left: {
+      padding: ` 0.8rem ${sidePadding}rem`,
+      backgroundColor: bgColor,
+      borderRight: `1px solid ${theme.colors.gray_200}`,
+    },
+    right: {
+      padding: `0.8rem ${sidePadding}rem `,
+      backgroundColor: bgColor,
+    },
+  };
+  return style[side];
+};
