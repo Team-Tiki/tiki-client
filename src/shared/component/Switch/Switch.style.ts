b53b5418b7fef9b8ab730c4d81839516eb826666
Jeongbowoon <happyanne200212@gmail/com>
2024-10-07 23:55:22 -0700

    #242 style: 가독성을 위한 수정

--- src/shared/component/Switch/Switch.style.ts
+++ src/shared/component/Switch/Switch.style.ts
@@ -4,24 +4,29 @@ import { theme } from '@/common/style/theme/theme';
 
 export const containerStyle = css({
   display: 'flex',
+
   height: '100%',
 
   border: `1px solid ${theme.colors.gray_200}`,
   borderRadius: '100px',
+
   overflow: 'hidden',
 });
 
 export const contentStyle = (side: 'left' | 'right', isClicked: boolean) => {
   const sidePadding = isClicked ? 1 : 1.6;
   const bgColor = isClicked ? theme.colors.gray_100 : theme.colors.white;
+
   const style = {
     left: {
       padding: ` 0.8rem ${sidePadding}rem`,
+
       backgroundColor: bgColor,
       borderRight: `1px solid ${theme.colors.gray_200}`,
     },
     right: {
       padding: `0.8rem ${sidePadding}rem `,
+
       backgroundColor: bgColor,
     },
   };
