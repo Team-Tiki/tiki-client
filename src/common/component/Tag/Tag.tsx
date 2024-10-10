89ef16d0b88cc2ebd58eba92e1067cde576646d3
namdaeun <nde40345@gmail.com>
2024-10-10 16:16:35 +0900

    fix:  prop 옵셔널로 설정

--- src/common/component/Tag/Tag.tsx
+++ src/common/component/Tag/Tag.tsx
@@ -3,12 +3,12 @@ import { ComponentPropsWithRef, ReactNode } from 'react';
 import { commonStyle, tagStyle } from '@/common/component/Tag/Tag.style';
 
 export interface TagProps extends ComponentPropsWithRef<'div'> {
-  variant: 'round' | 'square';
-  color: string;
+  variant?: 'round' | 'square';
+  color?: string;
   children: ReactNode;
 }
 
-const Tag = ({ children, variant, color, ...props }: TagProps) => {
+const Tag = ({ children, variant = 'round', color = '#909090', ...props }: TagProps) => {
   return (
     <div css={[commonStyle, tagStyle({ variant, color })]} {...props}>
       {children}
