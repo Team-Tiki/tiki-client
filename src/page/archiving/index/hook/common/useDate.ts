e4a6d668f237bc00cffc9d01275fcbcf96992da4
Daeun Nam <96781926+namdaeun@users.noreply.github.com>
2024-10-11 16:05:44 +0900

    [Feat] refactor archiving page (#234)
    * #233 docs: 아이콘 네이밍 통일
    
    * #233 chore: daySection key 추가
    
    * #233 feat: useDate 커스텀 훅에 month 클릭 핸들러도 포함
    
    * #233 refactor: 블록 가져오고 정렬하는 로직 분리
    
    * #233 refactor: 블록 선택시 로직 커스텀 훅으로 분리
    
    * #233 chore: 주석 추가
    
    * #233 chore: 코드 순서 변경
    
    * #233 docs: 커스텀훅 파일명과 함수명 변경
    
    * #233 feat: suspense를 통해 data가 defined인 경우에만 띄워주기
    
    * #233 docs : data 패치하는 훅 분리 안함
    
    * #233 refactor: teamId가 바뀔 때마다 현재 달로 초기화 useEffect 없이 구현
    
    * #233 fix: role 수정
    
    * #233 fix: 모달 관련 코드는 훅으로 분리하지 않음

--- src/page/archiving/index/hook/common/useDate.ts
+++ src/page/archiving/index/hook/common/useDate.ts
@@ -1,19 +1,27 @@
-import { MonthType } from '@/page/archiving/index/type/monthType';
-import { getMonthDate } from '@/page/archiving/index/util/date';
 import { endOfMonth } from 'date-fns';
 
 import { useState } from 'react';
 
-export const useDate = (ref?: React.RefObject<HTMLDivElement>) => {
+import { MonthType } from '@/page/archiving/index/type/monthType';
+import { getMonthDate } from '@/page/archiving/index/util/date';
+
+export const useDate = (ref?: React.RefObject<HTMLDivElement>, teamId?: string) => {
   const currentDate = new Date();
 
   const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
+  const [updatedTeamId, setUpdatedTeamId] = useState(teamId);
   const [selectedMonthString, setSelectedMonthString] = useState<MonthType>(
     `${currentDate.getMonth() + 1}월` as MonthType
   );
 
   const dateOfMonth = getMonthDate(selectedMonthString, currentYear);
   const endDay = endOfMonth(dateOfMonth);
+
+  /** teamId가 변경될 때마다 selectedMonthString 초기화 */
+  if (teamId !== updatedTeamId) {
+    setUpdatedTeamId(teamId);
+    setSelectedMonthString(`${currentDate.getMonth() + 1}월` as MonthType);
+  }
 
   const handlePrevYear = () => {
     setCurrentYear((prevYear) => prevYear - 1);
@@ -27,6 +35,11 @@ export const useDate = (ref?: React.RefObject<HTMLDivElement>) => {
     ref?.current?.scrollTo(0, 0);
   };
 
+  const handleMonthClick = (month: MonthType) => {
+    setSelectedMonthString(month);
+    ref?.current?.scrollTo(0, 0);
+  };
+
   return {
     currentDate,
     currentYear,
@@ -36,5 +49,6 @@ export const useDate = (ref?: React.RefObject<HTMLDivElement>) => {
     handleNextYear,
     dateOfMonth,
     endDay,
+    handleMonthClick,
   };
 };
