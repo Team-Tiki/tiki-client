e41d167f1b3a6d2ff7794cacbb763ed21ae11db5
wuzoo <wndyd9911@gmail.com>
2024-10-14 16:43:32 +0900

    style: boxShadow theme 생성

--- src/common/style/theme/theme.ts
+++ src/common/style/theme/theme.ts
@@ -96,9 +96,18 @@ const zIndex = {
   overlayBottom: 1,
 } as const;
 
+const shadow = {
+  shadow01: '0px 2px 8px 0px rgba(99, 99, 99, 0.2)', // 6
+  shadow02: '0px 4px 10px rgba(0, 0, 0, 0.1)',
+  shadow03: '0px 3px 8px rgba(0, 0, 0, 0.24)', // 5
+  shadow04: 'rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px', // 12
+  shadow05: 'rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px',
+};
+
 export const theme = {
   colors,
   heading,
   text,
   zIndex,
+  shadow,
 };
