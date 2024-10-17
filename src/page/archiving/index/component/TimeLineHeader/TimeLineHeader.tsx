e708fa4d632313e79b47dc80ad6dc31ad3916fe2
namdaeun <nde40345@gmail.com>
2024-10-17 17:13:07 +0900

    feat: 타임라인 헤더 구현

---
+++ src/page/archiving/index/component/TimeLineHeader/TimeLineHeader.tsx
@@ -0,0 +1,48 @@
+import ArrowLeft from '@/common/asset/svg/ic_arrow_back.svg?react';
+import ArrowRight from '@/common/asset/svg/ic_arrow_forward.svg?react';
+
+import {
+  arrowStyle,
+  buttonStyle,
+  commonStyle,
+  dateStyle,
+  headerStyle,
+} from '@/page/archiving/index/component/TimeLineHeader/TimeLineHeader.style';
+
+interface TimeLineHeaderProps {
+  currentYear: number;
+  currentMonth: number;
+  onPrevMonth?: () => void;
+  onNextMonth?: () => void;
+}
+
+const TimeLineHeader = ({ currentYear, currentMonth, onPrevMonth, onNextMonth }: TimeLineHeaderProps) => {
+  return (
+    <header css={headerStyle}>
+      <ArrowLeft
+        width={32}
+        height={32}
+        css={[commonStyle, arrowStyle]}
+        role="button"
+        tabIndex={0}
+        onClick={onPrevMonth}
+      />
+      <span css={dateStyle}>
+        {currentYear}년 {currentMonth}월
+      </span>
+      <ArrowRight
+        width={32}
+        height={32}
+        css={[commonStyle, arrowStyle]}
+        role="button"
+        tabIndex={0}
+        onClick={onNextMonth}
+      />
+      <button css={[commonStyle, buttonStyle]} tabIndex={0}>
+        오늘
+      </button>
+    </header>
+  );
+};
+
+export default TimeLineHeader;
