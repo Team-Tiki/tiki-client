80560837e929843551a453d6011fe6cd424a26d4
Jeongbowoon <happyanne200212@gmail/com>
2024-10-18 12:23:57 -0700

    feat: input variant구조 변경

--- src/common/component/Input/Input.style.ts
+++ src/common/component/Input/Input.style.ts
@@ -7,7 +7,7 @@ export const containerStyle = css({
   display: 'flex',
   flexDirection: 'column',
 
-  gap: '0.8rem',
+  gap: '1.2rem',
 
   width: '100%',
 });
@@ -19,75 +19,52 @@ export const inputSupportStyle = css({
   gap: '0.8rem',
 });
 
-export const warpperStyle = css({
-  display: 'flex',
-  alignItems: 'center',
-
-  paddingLeft: '0.8rem',
-});
-
 export const inputStyle = css({
   width: '100%',
 
   border: 'none',
   backgroundColor: 'transparent',
   fontWeight: 400,
-  ...theme.text.body04,
+  ...theme.text.body06,
 
   outline: 'none',
 
   '::placeholder': {
     color: theme.colors.gray_500,
-    ...theme.text.body04,
+    ...theme.text.body06,
   },
 });
 
-export const variantStyle = (variant: Required<InputProps>['variant'], isError: boolean) => {
-  const borderColor = isError ? `${theme.colors.sementic_red}` : `${theme.colors.gray_400}`;
-  const focusBorderColor = isError ? `${theme.colors.sementic_red}` : `${theme.colors.key_500}`;
+export const variantStyle = (variant: Required<InputProps>['variant'], isFilled: boolean) => {
+  const defaultStyle = {
+    display: 'flex',
+    alignItems: 'center',
 
-  const style = {
-    default: {
-      boxShadow: `inset 0px 0px 0px 1px ${borderColor}`,
-      borderRadius: '8px',
+    padding: '1.3rem 1.2rem',
 
-      '&:focus-within': {
-        boxShadow: `inset 0px 0px 0px 1px ${focusBorderColor}`,
-      },
-    },
-    underline: {
-      boxShadow: `inset 0px -1px 0px ${borderColor}`,
+    backgroundColor: isFilled ? theme.colors.gray_100 : 'none',
+    boxShadow: `inset 0px 0px 0px 1px ${theme.colors.gray_300}`,
+    borderRadius: '8px',
 
-      '&:focus-within': {
-        boxShadow: `inset 0px -1px 0px ${focusBorderColor}`,
-      },
-    },
-    colored: {
-      borderRadius: '100px',
-      backgroundColor: theme.colors.gray_100,
-
-      '& > input': {
-        '::placeholder': {
-          color: theme.colors.gray_500,
-          ...theme.text.body06,
-        },
-
-        '&:focus-within': {
-          boxShadow: `inset 0px 0px 0px 1px ${focusBorderColor}`,
-        },
-      },
+    '&:focus-within': {
+      boxShadow: `inset 0px 0px 0px 1px ${theme.colors.key_500}`,
     },
   };
 
-  return style[variant];
-};
-
-export const sizeStyle = (size: Required<InputProps>['size']) => {
   const style = {
-    small: { padding: '0.8rem 1.2rem' },
-    medium: { padding: '1.2rem 1.2rem' },
-    large: { padding: '1.6rem 1.2rem' },
+    default: {},
+    search: {
+      gap: '1rem',
+
+      '::before': {
+        content: '""',
+        width: '1.6rem',
+        height: '1.4rem',
+        backgroundImage: `url(src/common/asset/svg/ic_search.svg)`,
+      },
+    },
+    date: {},
   };
 
-  return style[size];
+  return { ...defaultStyle, ...style[variant] };
 };
--- src/common/component/Input/Input.tsx
+++ src/common/component/Input/Input.tsx
@@ -1,36 +1,27 @@
-import { ForwardedRef, InputHTMLAttributes, ReactNode, forwardRef } from 'react';
+import { ForwardedRef, InputHTMLAttributes, forwardRef } from 'react';
 
-import {
-  containerStyle,
-  inputStyle,
-  sizeStyle,
-  variantStyle,
-  warpperStyle,
-} from '@/common/component/Input/Input.style';
+import { containerStyle, inputStyle, variantStyle } from '@/common/component/Input/Input.style';
 import Label from '@/common/component/Label/Label';
 import SupportingText from '@/common/component/SupportingText/SupportingText';
 
-type InputSize = 'small' | 'medium' | 'large';
-type InputVariant = 'default' | 'underline' | 'colored';
+type InputVariant = 'default' | 'search' | 'date';
 
 export interface InputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> {
   variant?: InputVariant;
-  size?: InputSize; //default: medium(p: 1.2rem)
   label?: string;
-  LeftIcon?: ReactNode; //svg 컴포넌트
+  filled?: boolean;
   isError?: boolean;
-  isNotice?: boolean;
+  isSuccess?: boolean;
   supportingText?: string;
 }
 
 const Input = (
   {
     variant = 'default',
-    size = 'medium',
     label,
-    LeftIcon,
+    filled = false,
     isError = false,
-    isNotice = false,
+    isSuccess = false,
     supportingText,
     ...props
   }: InputProps,
@@ -39,12 +30,11 @@ const Input = (
   return (
     <article css={containerStyle}>
       {label && <Label id={label}>{label}</Label>}
-      <div css={[warpperStyle, variantStyle(variant, isError), sizeStyle(size)]}>
-        {LeftIcon}
+      <div css={variantStyle(variant, filled)}>
         <input ref={ref} css={inputStyle} {...props} />
       </div>
       {supportingText && (
-        <SupportingText isError={isError} isNotice={isNotice}>
+        <SupportingText isError={isError} isNotice={isSuccess}>
           {supportingText}
         </SupportingText>
       )}
