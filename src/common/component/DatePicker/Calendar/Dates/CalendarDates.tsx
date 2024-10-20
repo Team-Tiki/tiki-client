15962fa53aeab494067d055dd58d0505ffe32ac4
cindy-chaewon <chaewon1769@daum.net>
2024-10-20 10:19:17 +0900

    design: style 코드 수정

--- src/common/component/DatePicker/Calendar/Dates/CalendarDates.tsx
+++ src/common/component/DatePicker/Calendar/Dates/CalendarDates.tsx
@@ -31,19 +31,19 @@ const CalendarDates = ({
     selectedDate &&
     endDate &&
     isWithinInterval(date, { start: selectedDate, end: endDate }) &&
-    isSameMonth(currentMonth, date); // 현재 달 내에만 적용
+    isSameMonth(currentMonth, date);
 
   // 날짜에 따른 스타일을 결정하는 함수
   const getDateStyle = (date: Date) => {
-    const isSelected = selectedDate && isSameDay(selectedDate, date); // 시작 날짜인지 확인
-    const isEndDate = endDate && isSameDay(endDate, date); // 종료 날짜인지 확인
-    const isSameStartEndDate = isSelected && isEndDate; // 시작과 종료가 동일한 날짜인지 확인
+    const isSelected = selectedDate && isSameDay(selectedDate, date);
+    const isEndDate = endDate && isSameDay(endDate, date);
+    const isSameStartEndDate = isSelected && isEndDate;
     const isInRange = isDateInRange(date); // 현재 달의 날짜가 선택된 기간 내에 있는지 확인
 
     const isStartOfWeek = getDay(date) === 0 && isInRange; // 일요일이며 선택된 기간 내에 있는지 확인
     const isEndOfWeek = getDay(date) === 6 && isInRange; // 토요일이며 선택된 기간 내에 있는지 확인
-    const isStartOfMonth = isSameDay(date, startOfMonth(currentMonth)); // 현재 달의 첫날인지 확인
-    const isEndOfMonth = isSameDay(date, endOfMonth(currentMonth)); // 현재 달의 마지막 날인지 확인
+    const isStartOfMonth = isSameDay(date, startOfMonth(currentMonth));
+    const isEndOfMonth = isSameDay(date, endOfMonth(currentMonth));
 
     const isStartDateEndOfMonth = selectedDate && isSameDay(selectedDate, endOfMonth(currentMonth)); // 시작 날짜가 달의 마지막 날인지 확인
     const isEndDateStartOfNextMonth = endDate && isSameDay(endDate, startOfMonth(currentMonth)); // 종료 날짜가 달의 첫째 날인지 확인
@@ -59,18 +59,13 @@ const CalendarDates = ({
     }
 
     if (isSameMonth(currentMonth, date)) {
-      // 현재 달에 속하는 경우 처리
       if (endDate && selectedDate) {
-        // 시작과 종료가 같은 날짜일 때
         if (isSameStartEndDate) return selectedDateStyle;
 
-        // 종료 날짜가 달의 첫 번째 날인 경우
         if (isStartOfMonth && isEndDate) return selectedDateStyle;
 
-        // 시작 날짜가 달의 마지막 날인 경우
         if (isEndOfMonth && isSelected) return selectedDateStyle;
 
-        // 일반적인 경우는 기존 스타일
         if (isSelected || (isStartOfWeek && isInRange) || (isStartOfMonth && isInRange)) {
           return selectedStartDateStyle;
         }
@@ -79,12 +74,10 @@ const CalendarDates = ({
           return selectedEndDateStyle;
         }
 
-        // 기간 내에 있는 날짜
         if (isInRange) {
           return rangeDateStyle;
         }
       } else if (isSelected) {
-        // 단일 선택일 경우 selectedDateStyle 적용
         return selectedDateStyle;
       } else {
         return hoverDateStyle;
