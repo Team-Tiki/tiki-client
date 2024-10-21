233b97cbf318e9634da19cc4bf280700b4ae26cd
최주용 <121484561+wuzoo@users.noreply.github.com>
2024-10-21 16:37:11 +0900

    [Feat] Checkbox 컴포넌트 퍼블리싱 (#279)
    * feat: checkbox publishing
    
    * chore: checked -> isChecked

---
+++ src/common/component/CheckBox/CheckBox.style.ts
@@ -0,0 +1,34 @@
+import { css } from '@emotion/react';
+
+import { theme } from '@/common/style/theme/theme';
+
+export const inputStyle = css({
+  appearance: 'none',
+  WebkitAppearance: 'none',
+  MozAppearance: 'none',
+
+  width: '1.8rem',
+  height: '1.8rem',
+
+  border: 'none',
+  borderRadius: '2px',
+  backgroundColor: theme.colors.gray_300,
+
+  '&:checked': {
+    backgroundColor: theme.colors.key_500,
+  },
+
+  cursor: 'pointer',
+
+  '&:before': {
+    content: '"✓"',
+
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    color: theme.colors.white,
+
+    fontSize: '15px',
+  },
+});
