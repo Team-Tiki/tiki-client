5754d844d49a3a8180a3b60b8f5c0306854c94fd
chaewon <101500670+cindy-chaewon@users.noreply.github.com>
2024-10-21 23:09:01 +0900

    [Fix] DatePicker 기능 추가 및 최종 GUI 적용 (#270)
    * design: css 수정
    
    * design: 날짜 css 변경
    
    * fix: 날짜 기간 선택가능하게
    
    * fix: useDatePicker 훅 분리
    
    * fix: 날짜 초기화 로직 변경
    
    * fix: padding 값 추가
    
    * fix: 오류 해결
    
    * fix: index 스타일 파일 추가
    
    * fix: 기간 선택 로직 수정
    
    * design: style 코드 수정
    
    * refactor: 코드리뷰 반영
    
    * refactor: 코드리뷰 반영

---
+++ src/common/component/DatePicker/Trigger/DatePickerTrigger.style.ts
@@ -0,0 +1,7 @@
+import { css } from '@emotion/react';
+
+import { theme } from '@/common/style/theme/theme';
+
+export const textStyle = css({
+  color: theme.colors.gray_800,
+});
--- src/common/component/DatePicker/Trigger/DatePickerTrigger.tsx
+++ src/common/component/DatePicker/Trigger/DatePickerTrigger.tsx
@@ -1,24 +1,47 @@
 import { format } from 'date-fns';
 
+import { textStyle } from '@/common/component/DatePicker/Trigger/DatePickerTrigger.style';
+import Flex from '@/common/component/Flex/Flex';
 import Input from '@/common/component/Input/Input';
+import Text from '@/common/component/Text/Text';
 
 interface DatePickerTriggerProps {
   selectedDate: Date | null;
+  endDate?: Date | null;
   onClick: () => void;
   width?: string;
+  variant: 'single' | 'range';
 }
 
-const DatePickerTrigger = ({ selectedDate, onClick, width = '10.3rem' }: DatePickerTriggerProps) => {
+const DatePickerTrigger = ({ selectedDate, endDate, onClick, width, variant }: DatePickerTriggerProps) => {
   return (
-    <Input
-      variant="default"
-      size="large"
-      value={selectedDate ? format(selectedDate, 'yyyy.MM.dd') : ''}
-      placeholder="YYYY.MM.DD"
-      readOnly
-      onClick={onClick}
-      css={{ cursor: 'pointer', width }}
-    />
+    <Flex styles={{ direction: 'row', align: 'center', gap: '0.4rem' }}>
+      <Input
+        variant="default"
+        size="medium"
+        value={selectedDate ? format(selectedDate, 'yyyy.MM.dd') : ''}
+        placeholder="YYYY.MM.DD"
+        readOnly
+        onClick={onClick}
+        css={{ cursor: 'pointer', width }}
+      />
+      {variant === 'range' && (
+        <>
+          <Text tag="body6" css={textStyle}>
+            ~
+          </Text>
+          <Input
+            variant="default"
+            size="medium"
+            value={endDate ? format(endDate, 'yyyy.MM.dd') : ''}
+            placeholder="YYYY.MM.DD"
+            readOnly
+            onClick={onClick}
+            css={{ cursor: 'pointer', width }}
+          />
+        </>
+      )}
+    </Flex>
   );
 };
 
