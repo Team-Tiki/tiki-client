11e126f8e9aca37deb84775a42dbb556c0c2977e
kimkyuhong <bobo3766@naver.com>
2024-10-21 22:59:38 +0900

    style: ToolTip style 구현

---
+++ src/common/component/ToolTip/ToolTip.style.ts
@@ -0,0 +1,84 @@
+import { css } from '@emotion/react';
+
+import { ToolTipProps } from '@/common/component/ToolTip/ToolTip';
+import { theme } from '@/common/style/theme/theme';
+
+export const containerStyle = css({
+  position: 'relative',
+  border: '1px solid',
+  '&:hover': {
+    '& > span': {
+      display: 'block',
+    },
+  },
+});
+
+export const messageStyle = css({
+  display: 'none',
+  position: 'absolute',
+
+  padding: '1rem',
+  borderRadius: '8px',
+
+  backgroundColor: `${theme.colors.gray_900}`,
+  font: `${theme.text.body08}`,
+  color: `${theme.colors.white}`,
+});
+
+export const positionStyle = (position: Required<ToolTipProps>['position'], margin: number) => {
+  const style = {
+    top: css({
+      left: '50%',
+      bottom: '100%',
+      transform: `translateX(-50%) translateY(calc(-${margin}rem - 8px) )`,
+    }),
+    bottom: css({
+      left: '50%',
+      up: '100%',
+      transform: `translateX(-50%) translateY(calc(${margin}rem + 8px))`,
+    }),
+    left: css({
+      top: '50%',
+      right: '100%',
+      transform: `translateY(-50%) translateX(calc(-${margin}rem - 8px))`,
+    }),
+    right: css({
+      top: '50%',
+      left: '100%',
+      transform: `translateY(-50%) translateX(calc(${margin}rem + 8px))`,
+    }),
+  };
+
+  return style[position];
+};
+
+export const arrowStyle = css({
+  position: 'absolute',
+});
+
+export const arrowPositionStyle = (position: Required<ToolTipProps>['position']) => {
+  const style = {
+    top: css({
+      left: '50%',
+      top: '99%',
+      transform: `translateX(-50%) rotate(270deg)`,
+    }),
+    bottom: css({
+      left: '50%',
+      bottom: '98%',
+      transform: `translateX(-50%) rotate(90deg)`,
+    }),
+    left: css({
+      top: '50%',
+      left: '98%',
+      transform: `translateY(-50%) rotate(180deg)`,
+    }),
+    right: css({
+      top: '50%',
+      right: '98%',
+      transform: `translateY(-50%) `,
+    }),
+  };
+
+  return style[position];
+};
