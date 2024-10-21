c280cc18433d5746c9bbdeaea5154873550baa07
kimkyuhong <bobo3766@naver.com>
2024-10-21 23:00:07 +0900

    feat: storybook 작성

---
+++ src/story/common/ToolTip.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Flex from '@/common/component/Flex/Flex';
+import ToolTip from '@/common/component/ToolTip/ToolTip';
+import { theme } from '@/common/style/theme/theme';
+
+const meta = {
+  title: 'Common/ToolTip',
+  component: ToolTip,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    position: {
+      control: { type: 'radio' },
+      options: ['top', 'bottom', 'left', 'right'],
+    },
+    message: {
+      control: { type: 'text' },
+    },
+  },
+  args: {
+    position: 'top',
+    message: 'ToolTip',
+    margin: 0.8,
+  },
+} satisfies Meta<typeof ToolTip>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const DefaultToolTip: Story = {
+  args: {
+    children: (
+      <Flex
+        style={{
+          width: '36px',
+          height: '36px',
+          backgroundColor: theme.colors.purple_200,
+        }}
+        styles={{ justify: 'center', align: 'center' }}>
+        호버
+      </Flex>
+    ),
+  },
+};
