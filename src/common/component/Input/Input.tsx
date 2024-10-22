b180549742a2b60fe3b0fc67ae9c0fda3270b967
wuzoo <wndyd9911@gmail.com>
2024-10-22 21:41:59 +0900

    refactor: count onChange issue, delete and create new one

--- src/common/component/Input/Input.tsx
+++ src/common/component/Input/Input.tsx
@@ -1,51 +1,28 @@
-import { ChangeEvent, ForwardedRef, InputHTMLAttributes, ReactNode, forwardRef, useState } from 'react';
+import { ForwardedRef, InputHTMLAttributes, ReactNode, forwardRef } from 'react';
 
-import { containerStyle, contentStyle, countStyle, inputStyle } from '@/common/component/Input/Input.style';
+import { containerStyle, contentStyle, inputStyle } from '@/common/component/Input/Input.style';
 import Label from '@/common/component/Label/Label';
 import SupportingText from '@/common/component/SupportingText/SupportingText';
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
-  filled?: boolean;
   LeftIcon?: ReactNode;
-  hasCount?: boolean; //글자수 세기
+  isFilled?: boolean;
   isError?: boolean;
   isSuccess?: boolean;
   supportingText?: string;
 }
 
 const Input = (
-  {
-    label,
-    filled = false,
-    LeftIcon,
-    hasCount,
-    isError = false,
-    isSuccess = false,
-    supportingText,
-    ...props
-  }: InputProps,
+  { label, isFilled = false, LeftIcon, isError = false, isSuccess = false, supportingText, ...props }: InputProps,
   ref: ForwardedRef<HTMLInputElement>
 ) => {
-  const [count, setCount] = useState(0);
-
-  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    if (count === props.maxLength) {
-      e.target.value = e.target.value.substring(0, props.maxLength);
-    }
-    setCount(e.target.value.length);
-  };
   return (
     <div css={containerStyle}>
       {label && <Label id={label}>{label}</Label>}
-      <div css={contentStyle(filled)}>
+      <div css={contentStyle(isFilled)}>
         {LeftIcon}
-        <input ref={ref} onChange={onChange} css={inputStyle} {...props} />
-        {hasCount && (
-          <span css={countStyle}>
-            {count}/{props.maxLength}
-          </span>
-        )}
+        <input ref={ref} css={inputStyle} {...props} />
       </div>
       {supportingText && (
         <SupportingText isError={isError} isNotice={isSuccess}>
