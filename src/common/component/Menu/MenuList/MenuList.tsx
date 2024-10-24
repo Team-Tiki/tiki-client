a0b33cdb082ba359aad5a4a75101c3f331bbf096
Jeongbowoon <happyanne200212@gmail/com>
2024-10-24 15:51:23 -0700

    refactor: Children cloneElement 로직 제거

--- src/common/component/Menu/MenuList/MenuList.tsx
+++ src/common/component/Menu/MenuList/MenuList.tsx
@@ -1,9 +1,8 @@
-import { Children, HTMLAttributes, ReactElement, ReactNode, cloneElement } from 'react';
+import { HTMLAttributes } from 'react';
 
 import { containerStyle } from '@/common/component/Menu/MenuList/MenuList.style';
 
 export interface MenuListProps extends HTMLAttributes<HTMLUListElement> {
-  children: ReactNode;
   isOpen: boolean;
 }
 
@@ -11,7 +10,7 @@ const MenuList = ({ children, isOpen = false, ...props }: MenuListProps) => {
   return (
     isOpen && (
       <ul css={containerStyle} {...props}>
-        {Children.toArray(children).map((child) => cloneElement(child as ReactElement))}
+        {children}
       </ul>
     )
   );
