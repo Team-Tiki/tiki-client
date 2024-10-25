8b6a37784b5810982f9f293aa972b3f0ebbe735b
namdaeun <nde40345@gmail.com>
2024-10-26 00:10:22 +0900

    feat: 커스텀 노트 뷰 구현

---
+++ src/page/handover/note/Custom/Custom.style.ts
@@ -0,0 +1,48 @@
+import { css } from '@emotion/react';
+
+import { theme } from '@/common/style/theme/theme';
+
+export const noteWrapperStyle = css({
+  display: 'flex',
+
+  flexDirection: 'column',
+});
+
+export const layoutStyle = css({
+  display: 'flex',
+  flexDirection: 'column',
+
+  marginTop: '3rem',
+
+  gap: '1.6rem',
+
+  ':last-of-type': {
+    marginTop: '4rem',
+  },
+});
+
+export const textareaStyle = css({
+  width: '82.3rem',
+  height: '45rem',
+
+  padding: '2.1rem 2rem 2.1rem 2rem',
+
+  borderRadius: '1rem',
+  border: `1px solid ${theme.colors.gray_300}`,
+
+  resize: 'none',
+
+  '&:focus': {
+    boxShadow: theme.shadow.inset_focus,
+
+    transition: '0.2s ease-in-out',
+
+    outline: 'none',
+  },
+});
+
+export const guideStyle = css({
+  color: theme.colors.gray_800,
+
+  ...theme.text.body04,
+});
--- src/page/handover/note/Custom/Custom.tsx
+++ src/page/handover/note/Custom/Custom.tsx
@@ -1,5 +1,36 @@
+import { useRef } from 'react';
+
+import Button from '@/common/component/Button/Button';
+import Text from '@/common/component/Text/Text';
+
+import { guideStyle, layoutStyle, noteWrapperStyle, textareaStyle } from './Custom.style';
+
 const Custom = () => {
-  return <div>커스텀</div>;
+  const fileRef = useRef<HTMLInputElement>(null);
+
+  const handleFileUpload = () => {
+    fileRef.current?.click();
+  };
+
+  return (
+    <div css={noteWrapperStyle}>
+      <div css={layoutStyle}>
+        <Text tag="body4" css={guideStyle}>
+          다음 운영진이 참고할 인수인계 노트를 자유롭게 작성해 보세요.
+        </Text>
+        <textarea css={textareaStyle} placeholder="동아리의 회칙 및 대관정보 등" />
+      </div>
+      <div css={layoutStyle}>
+        <Text tag="body4" css={guideStyle}>
+          드라이브에서 연동하고 싶은 파일을 선택해주세요.
+        </Text>
+        <input type="file" style={{ display: 'none' }} ref={fileRef} />
+        <Button variant="tertiary" css={{ width: '16rem' }} onClick={handleFileUpload}>
+          파일 연동하기
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Custom;
