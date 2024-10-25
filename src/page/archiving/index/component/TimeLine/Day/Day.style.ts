07df86f0ee8087ccc9b8384ebb08a09ca4a43c83
Daeun Nam <96781926+namdaeun@users.noreply.github.com>
2024-10-25 16:02:41 +0900

    [Feat] 타임라인 GUI 수정사항 반영 (#290)
    * feat: 타임라인 헤더 구현
    
    * fix: 변경한 타임라인 헤더에 맞게 타임라인 prop 수정
    
    * chore: 필요없는 코드 제거
    
    * style: 스타일 수정사항 반영
    
    * docs: 필요없는 파일 제거
    
    * chore: 필요 없는 코드 제거
    
    * fix: 해당 달의 마지막 날 가져오도록 수정
    
    * style: daySection 전체적인 구조 수정
    
    * docs: DaySection 컴포넌트 위치 변경
    
    * docs: 타임 라인 내부 컴포넌트 TimeLine 폴더 안에 위치
    
    * chore: 임포트 경로 수정
    
    * fix: arrow -> Button 공컴 활용
    
    * docs: 타임블록 아이콘 변경
    
    * feat: 타임블록 배치 중
    
    * docs: merge develop
    
    * fix: 반응형 고려하지 않음
    
    * style: arrow padding 추가
    
    * feat: 타임블록 위치 잡기
    
    * feat: 오늘 버튼 누르면 현재 달로 이동
    
    * fix: color값은 서버에서 받아옴
    
    * chore: 타임블록 위치 조정
    
    * chore: DaySection -> Day
    
    * docs: 폴더구조 변경
    
    * chore: 컴포넌트명 수정
    
    * fix: Day에 연도와 달 넘겨주기
    
    * chore: lint에러 해결
    
    * fix: 타임 블록 아이콘 fill 구현
    
    * chore: 빌드 에러 해결
    
    * fix: padding 제거
    
    * chore: 빌드 에러 해결

---
+++ src/page/archiving/index/component/TimeLine/Day/Day.style.ts
@@ -0,0 +1,64 @@
+import { css } from '@emotion/react';
+
+import { theme } from '@/common/style/theme/theme';
+
+export const entireDayStyle = css({
+  display: 'flex',
+  flexDirection: 'column',
+
+  width: '100%',
+  height: '100%',
+});
+
+export const dayStyle = css({
+  display: 'flex',
+  flexDirection: 'row',
+
+  justifyContent: 'space-between',
+});
+
+export const dayHeaderStyle = (isToday: boolean) =>
+  css({
+    display: 'flex',
+
+    width: '2rem',
+    height: '2rem',
+
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    marginBottom: '0.6rem',
+
+    borderRadius: '0.4rem',
+
+    color: theme.colors.gray_800,
+    backgroundColor: isToday ? theme.colors.gray_200 : theme.colors.white,
+
+    ...theme.text.body08,
+
+    ':first-of-type': {
+      marginLeft: '1.9rem',
+    },
+
+    ':last-of-type': {
+      marginRight: '1.9rem',
+    },
+  });
+
+export const dayBodyStyle = css({
+  position: 'relative',
+  display: 'flex',
+
+  height: '46rem',
+
+  justifyContent: 'center',
+  alignItems: 'center',
+
+  maxHeight: 'calc(100vh - 2rem)',
+
+  overflowY: 'auto',
+
+  backgroundColor: theme.colors.gray_100,
+
+  borderRadius: '1.6rem',
+});
---
+++ src/page/archiving/index/type/color.ts
@@ -0,0 +1,1 @@
+export type BlockColor = '#FFE6E8' | '#F8E2CB' | '#C4F2E5' | '#DCD8FA' | '#E2E8F8' | '#F8E1F5' | '#D3EFFA';
