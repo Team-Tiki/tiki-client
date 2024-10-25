53fee0984fce08129071ee61d5e64664cd4e2818
wuzoo <wndyd9911@gmail.com>
2024-10-26 01:17:35 +0900

    chore: naming 오타 수정

--- src/common/hook/useDeferedSearchFilter.ts
+++ src/common/hook/useDeferedSearchFilter.ts
@@ -8,7 +8,7 @@ type Data<T> = {
  * - useDeferedValue 훅을 사용한 UI 지연 렌더링 + 필터링
  * 검색 기능 시 사용
  */
-const useDeferedSearchFilter = <T extends object>(list: Data<T>[], value: string) => {
+const useDeferredSearchFilter = <T extends object>(list: Data<T>[], value: string) => {
   const deferredValue = useDeferredValue(value);
 
   /** 상태 업데이트 시 이전 값과 지연된 값이 일치하지 않으면 isStale (아직 최신 데이터가 로드 전임을 UI로 나타내기 위함) */
@@ -28,4 +28,4 @@ const useDeferedSearchFilter = <T extends object>(list: Data<T>[], value: string
   return { deferredValue, filteredData, isStale };
 };
 
-export default useDeferedSearchFilter;
+export default useDeferredSearchFilter;
