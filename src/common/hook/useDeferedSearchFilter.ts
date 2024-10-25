1a5af638a75f66b29a3e32ad78a521bc26f96f8d
wuzoo <wndyd9911@gmail.com>
2024-10-26 01:48:27 +0900

    refactor: value로 이루어진 배열을 stringify하여 검사

--- src/common/hook/useDeferedSearchFilter.ts
+++ src/common/hook/useDeferedSearchFilter.ts
@@ -18,7 +18,7 @@ const useDeferredSearchFilter = <T extends object>(list: Data<T>[], value: strin
     Object.values(item).some((value) => {
       if (typeof value === 'object') {
         /** deep comparing 대신 객체를 문자열로 만들어 검사 */
-        return JSON.stringify(value).includes(deferredValue);
+        return JSON.stringify(Object.values(value!)).includes(deferredValue);
       } else {
         return String(value).includes(deferredValue);
       }
