7a57388dba7b86568e2ba5a42dabd3ffc4c44851
wuzoo <wuzoo@users.noreply.github.com>
2024-10-30 12:19:16 +0000

    deploy: 7fded066c76b41750597f5f128c2685fce8c0134

---
+++ assets/TabButton.stories-BJRdTX2e.js
@@ -0,0 +1,11 @@
+import{T as s}from"./TabButton-8OKMcu39.js";import"./iframe-B8JmUmEf.js";import"./emotion-react.browser.esm-D2GLAKnc.js";import"./index-eAnEFhSu.js";import"./_commonjsHelpers-dwoEeMXf.js";import"./extends-ClQVCWlZ.js";import"./theme-DRGpvjPk.js";import"../sb-preview/runtime.js";(function(){try{var t=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},r=new Error().stack;r&&(t._sentryDebugIds=t._sentryDebugIds||{},t._sentryDebugIds[r]="f76791b5-5d4b-44fa-aeff-e065c463aa93",t._sentryDebugIdIdentifier="sentry-dbid-f76791b5-5d4b-44fa-aeff-e065c463aa93")}catch{}})();const g={title:"Common/TabButton",component:s,parameters:{layout:"centered"},tags:["autodocs"],args:{children:"TabButton",tabId:0,isSelected:!1}},o={args:{variant:"round"},argTypes:{variant:{control:"radio",options:["round"]}}};var e,n,a;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    variant: 'round'
+  },
+  argTypes: {
+    variant: {
+      control: 'radio',
+      options: ['round']
+    }
+  }
+}`,...(a=(n=o.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const y=["RoundTabButton"];export{o as RoundTabButton,y as __namedExportsOrder,g as default};
---
+++ assets/WorkSpaceModal.stories-1qbGrhnm.js
@@ -0,0 +1,47 @@
+import{a as e,j as b,c as k}from"./emotion-react.browser.esm-D2GLAKnc.js";import{B as I}from"./Button-BKc31AOj.js";import{F as W}from"./Flex-CpYfq2G8.js";import{S as w}from"./Select-Dn-epzyw.js";import"./index-eAnEFhSu.js";import"./iframe-B8JmUmEf.js";import{u as x}from"./useOverlay-BWJpfkJK.js";import{W as n,a as O,b as C,c as N,d as T}from"./WorkSpaceName-D4UOjji1.js";import"./extends-ClQVCWlZ.js";import"./Button.style-CKgpLZRq.js";import"./theme-DRGpvjPk.js";import"./Label-BUIXp0X-.js";import"./scroll-B1Tk2aT7.js";import"./_commonjsHelpers-dwoEeMXf.js";import"../sb-preview/runtime.js";import"./QueryClientProvider-D9Xwfj9v.js";import"./path-2T0AD3jl.js";import"./file-C0-xS308.js";import"./Input-CAQ7fF1M.js";import"./SupportingText-7DH8dHKD.js";import"./Heading-CGyDcm-9.js";import"./Text-C34Pj9jK.js";(function(){try{var r=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},t=new Error().stack;t&&(r._sentryDebugIds=r._sentryDebugIds||{},r._sentryDebugIds[t]="e9e3f31a-32d9-46ad-894d-08efd735d4fe",r._sentryDebugIdIdentifier="sentry-dbid-e9e3f31a-32d9-46ad-894d-08efd735d4fe")}catch{}})();const ee={title:"Shared/WorkSpaceModal",component:n,parameters:{layout:"centered"},decorators:[r=>e(n,{children:e(r,{})})]},o={render:()=>e(O,{isVisible:!0})},s={render:()=>e(C,{isVisible:!0})},a={render:()=>{const{isOpen:r,toggle:t}=x();return b(W,{tag:"section",styles:{direction:"column",justify:"center",align:"center"},css:V,children:[e(N,{step:"category",title:"새로운 워크 스페이스 생성하기",info:"팀 카테고리를 선택해주세요."}),e("div",{css:{width:"32rem",marginTop:"2.4rem"},children:e(w,{css:{"& ul":{maxHeight:"25rem"}},placeholder:"선택",variant:"outline",isOpen:r,onTrigger:t,onSelect:t,options:[{value:"옵션 1입니다"},{value:"옵션 2입니다"}],className:"select-container"})}),e(I,{css:{width:"100%",marginTop:"18.8rem"},variant:"primary",size:"medium",children:"다음"})]})}},i={render:()=>e(T,{isVisible:!0})},V=k({height:"40rem"});var c,m,p;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: () => {
+    return <WorkSpaceName isVisible={true} />;
+  }
+}`,...(p=(m=o.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var d,l,u;s.parameters={...s.parameters,docs:{...(d=s.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: () => {
+    return <WorkSpaceComplete isVisible={true} />;
+  }
+}`,...(u=(l=s.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var g,S,f;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: () => {
+    const {
+      isOpen,
+      toggle
+    } = useOverlay();
+    return <Flex tag={'section'} styles={{
+      direction: 'column',
+      justify: 'center',
+      align: 'center'
+    }} css={Style}>
+        <WorkSpaceInfo step="category" title="새로운 워크 스페이스 생성하기" info="팀 카테고리를 선택해주세요." />
+        <div css={{
+        width: '32rem',
+        marginTop: '2.4rem'
+      }}>
+          <Select css={{
+          '& ul': {
+            maxHeight: '25rem'
+          }
+        }} placeholder="선택" variant="outline" isOpen={isOpen} onTrigger={toggle} onSelect={toggle} options={[{
+          value: '옵션 1입니다'
+        }, {
+          value: '옵션 2입니다'
+        }]} className="select-container" />
+        </div>
+        <Button css={{
+        width: '100%',
+        marginTop: '18.8rem'
+      }} variant="primary" size="medium">
+          다음
+        </Button>
+      </Flex>;
+  }
+}`,...(f=(S=a.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};var y,h,v;i.parameters={...i.parameters,docs:{...(y=i.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: () => {
+    return <WorkSpaceImage isVisible={true} />;
+  }
+}`,...(v=(h=i.parameters)==null?void 0:h.docs)==null?void 0:v.source}}};const re=["NameStep","CompleteStep","SelectCategoryStep","ImageUploadStep"];export{s as CompleteStep,i as ImageUploadStep,o as NameStep,a as SelectCategoryStep,re as __namedExportsOrder,ee as default};
