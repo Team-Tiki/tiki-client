9327f649c57ca1e2c923774142c65c186b61a6bd
wuzoo <wuzoo@users.noreply.github.com>
2024-10-30 11:43:40 +0000

    deploy: ffcddc3c8d809f99656a560f1154ccaa73309107

--- assets/scroll-CgP7JNU1.js
+++
@@ -1,9 +0,0 @@
-import{c as t}from"./emotion-react.browser.esm-DsyXDLlE.js";import{t as a}from"./theme-BXds7J9r.js";import"./iframe-BqCMgI7k.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},r=new Error().stack;r&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[r]="1dab6008-e19a-4d25-925e-26effe3a40aa",e._sentryDebugIdIdentifier="sentry-dbid-1dab6008-e19a-4d25-925e-26effe3a40aa")}catch{}})();const b=t`
-  ::-webkit-scrollbar {
-    width: 0.8rem;
-  }
-  ::-webkit-scrollbar-thumb {
-    background: ${a.colors.gray_300};
-    border-radius: 10rem;
-  }
-`;export{b as s};
