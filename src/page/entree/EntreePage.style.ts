b6a0e94ecc911881c9cafdd3fa5bcfe43869bead
Jeongbowoon <happyanne200212@gmail/com>
2024-10-29 22:53:36 -0700

    feat: content box로 구조잡기

---
+++ src/page/entree/EntreePage.style.ts
@@ -0,0 +1,5 @@
+import { css } from '@emotion/react';
+
+export const fileContainerStyle = css({
+  //overflowX: 'scroll',
+});
--- src/page/entree/EntreePage.tsx
+++ src/page/entree/EntreePage.tsx
@@ -1,5 +1,40 @@
+import Button from '@/common/component/Button/Button';
+import Flex from '@/common/component/Flex/Flex';
+
+import ContentBox from '@/shared/component/ContentBox/ContentBox';
+import FileGrid from '@/shared/component/FileGrid/FileGrid';
+
 const EntreePage = () => {
-  return <div>entreepage</div>;
+  return (
+    <Flex styles={{ gap: '2.4rem' }}>
+      <Flex styles={{ direction: 'column', gap: '2.4rem', width: '65%' }}>
+        <ContentBox
+          variant={'file'}
+          title={'파일'}
+          headerOption={<Button variant="outline">전체보기</Button>}
+          css={{
+            height: '21.6rem',
+            '& > header': { height: '6.1rem' },
+            '& > div': { marginTop: '0' },
+          }}>
+          <FileGrid title={'파일 파일'} type={'pdf'} volume={20} isSmall={true} />
+        </ContentBox>
+        <ContentBox
+          variant={'timeline'}
+          title={'타임라인'}
+          headerOption={<Button variant="outline">전체보기</Button>}
+          css={{ height: '40rem', '& > header': { height: '6.4rem' } }}>
+          <FileGrid title={'파일 파일'} type={'pdf'} volume={20} isSmall={true} />
+        </ContentBox>
+      </Flex>
+      <ContentBox
+        variant={'timeline'}
+        title={'인수인계 노트'}
+        headerOption={<Button variant="outline">전체보기</Button>}>
+        <FileGrid title={'파일 파일'} type={'pdf'} volume={20} isSmall={true} />
+      </ContentBox>
+    </Flex>
+  );
 };
 
 export default EntreePage;
