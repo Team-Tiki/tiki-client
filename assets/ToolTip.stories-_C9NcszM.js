48382a93d22b0dd999bb93f90fafd361425a7657
wuzoo <wuzoo@users.noreply.github.com>
2024-11-18 11:44:26 +0000

    deploy: 2fcad8f65940760883457f4f074e8d1e29d10698

---
+++ assets/ToolTip.stories-_C9NcszM.js
@@ -0,0 +1,14 @@
+import{a}from"./emotion-react.browser.esm-xyVcI5Gp.js";import{F as i}from"./Flex-DILGoq0o.js";import{T as p}from"./ToolTip-Drto4Nal.js";import{t as c}from"./theme-wXRzav9S.js";import"./iframe-CbWKwn5X.js";import"./index-BLAkgBHP.js";import"./_commonjsHelpers-DR6wHYmc.js";import"./extends-ClQVCWlZ.js";import"../sb-preview/runtime.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},t=new Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]="2da0e0ef-4c6c-42a6-ac8a-c49ffe8e766c",e._sentryDebugIdIdentifier="sentry-dbid-2da0e0ef-4c6c-42a6-ac8a-c49ffe8e766c")}catch{}})();const b={title:"Common/ToolTip",component:p,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{position:{control:{type:"radio"},options:["top","bottom","left","right"]},message:{control:{type:"text"}}},args:{position:"top",message:"ToolTip",gap:.8}},o={args:{children:a(i,{style:{width:"36px",height:"36px",backgroundColor:c.colors.purple_200},styles:{justify:"center",align:"center"},children:"호버"})}};var r,n,s;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    children: <Flex style={{
+      width: '36px',
+      height: '36px',
+      backgroundColor: theme.colors.purple_200
+    }} styles={{
+      justify: 'center',
+      align: 'center'
+    }}>
+        호버
+      </Flex>
+  }
+}`,...(s=(n=o.parameters)==null?void 0:n.docs)==null?void 0:s.source}}};const _=["DefaultToolTip"];export{o as DefaultToolTip,_ as __namedExportsOrder,b as default};
---
+++ assets/scroll-nK4PyKIk.js
@@ -0,0 +1,10 @@
+import{c as t}from"./emotion-react.browser.esm-xyVcI5Gp.js";import{t as o}from"./theme-wXRzav9S.js";import"./iframe-CbWKwn5X.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},r=new Error().stack;r&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[r]="15b3e45e-fe17-4a7a-95b3-18c27fed338a",e._sentryDebugIdIdentifier="sentry-dbid-15b3e45e-fe17-4a7a-95b3-18c27fed338a")}catch{}})();const n=t`
+  ::-webkit-scrollbar {
+    width: 0.8rem;
+    height: 0.8rem;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: ${o.colors.gray_300};
+    border-radius: 10rem;
+  }
+`;export{n as s};
