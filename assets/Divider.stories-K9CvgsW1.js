8d8933b847cfe6fd7cf59c670de4b64d59eddcf6
rtttr1 <rtttr1@users.noreply.github.com>
2024-11-20 15:04:56 +0000

    deploy: bb391b3502d4185c2fabf6e368b8012bf59af944

---
+++ assets/Divider.stories-K9CvgsW1.js
@@ -0,0 +1,11 @@
+import{a}from"./emotion-react.browser.esm-jro6eB32.js";import{D as l}from"./Divider-V4kCX_T1.js";import"./iframe-hcMr1ntz.js";import"./index-BETB8xs-.js";import"./_commonjsHelpers-2UnFNFTw.js";import"./extends-ClQVCWlZ.js";import"./theme-DQKz66Qf.js";import"../sb-preview/runtime.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},t=new Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]="745f4587-a50a-4463-b115-b9047426d044",e._sentryDebugIdIdentifier="sentry-dbid-745f4587-a50a-4463-b115-b9047426d044")}catch{}})();const D={title:"Common/Divider",component:l,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{type:{control:{type:"radio"},options:["horizontal","vertical"]},size:{control:{type:"number"}},color:{control:{type:"color"}}},args:{type:"horizontal",size:50},decorators:[e=>a("div",{style:{width:"70rem",height:"5rem"},children:a(e,{})})]},r={args:{type:"horizontal",size:100}},o={args:{type:"vertical",size:100}};var s,i,n;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    type: 'horizontal',
+    size: 100
+  }
+}`,...(n=(i=r.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};var c,d,p;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    type: 'vertical',
+    size: 100
+  }
+}`,...(p=(d=o.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};const w=["Horizontal","Vertical"];export{r as Horizontal,o as Vertical,w as __namedExportsOrder,D as default};
---
+++ assets/scroll-BW8Mj9f6.js
@@ -0,0 +1,10 @@
+import{c as t}from"./emotion-react.browser.esm-jro6eB32.js";import{t as o}from"./theme-DQKz66Qf.js";import"./iframe-hcMr1ntz.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},r=new Error().stack;r&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[r]="15b3e45e-fe17-4a7a-95b3-18c27fed338a",e._sentryDebugIdIdentifier="sentry-dbid-15b3e45e-fe17-4a7a-95b3-18c27fed338a")}catch{}})();const n=t`
+  ::-webkit-scrollbar {
+    width: 0.8rem;
+    height: 0.8rem;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: ${o.colors.gray_300};
+    border-radius: 10rem;
+  }
+`;export{n as s};
