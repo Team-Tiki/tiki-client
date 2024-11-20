dc0f30c4bf4d29c1c4e3d2ce2100ffedec8c1a04
rtttr1 <rtttr1@users.noreply.github.com>
2024-11-20 13:18:44 +0000

    deploy: 5b075d97a7ca117080ff694f5b093da74c113e8a

---
+++ assets/ListItem.stories-Chhp5W4t.js
@@ -0,0 +1,46 @@
+import{c,j as g,a as m}from"./emotion-react.browser.esm-jySFJa0C.js";import{r as o}from"./index-C73pvn4u.js";import{F as b}from"./Flex-raOKmZCB.js";import{T as S}from"./Tag-CVf-AYgg.js";import{T as p}from"./Text-DMTDN76W.js";import{t}from"./theme-CkGtBuFl.js";import"./iframe-BHwNxDEU.js";import"./extends-ClQVCWlZ.js";import"./_commonjsHelpers-CzT8XOfQ.js";import"../sb-preview/runtime.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},a=new Error().stack;a&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[a]="d56d8e64-a94e-4050-9154-de964147c6ad",e._sentryDebugIdIdentifier="sentry-dbid-d56d8e64-a94e-4050-9154-de964147c6ad")}catch{}})();const D=e=>o.createElement("svg",{width:"1em",height:"1em",viewBox:"0 0 16 16",fill:"none",xmlns:"http://www.w3.org/2000/svg",...e},o.createElement("mask",{id:"path-1-inside-1_376_7126",fill:"white"},o.createElement("rect",{x:2,y:3,width:12,height:11,rx:1})),o.createElement("rect",{x:2,y:3,width:12,height:11,rx:1,stroke:"#525866",strokeWidth:2.4,mask:"url(#path-1-inside-1_376_7126)"}),o.createElement("path",{d:"M2.5 6H13.5",stroke:"#525866",strokeWidth:1.2}),o.createElement("path",{d:"M5.1 2C5.1 1.66863 4.83137 1.4 4.5 1.4C4.16863 1.4 3.9 1.66863 3.9 2H5.1ZM5.1 3.5V2H3.9V3.5H5.1Z",fill:"#525866"}),o.createElement("path",{d:"M12.1 2C12.1 1.66863 11.8314 1.4 11.5 1.4C11.1686 1.4 10.9 1.66863 10.9 2H12.1ZM12.1 3.5V2H10.9V3.5H12.1Z",fill:"#525866"}),o.createElement("rect",{x:4,y:7,width:3,height:2,rx:.6,fill:"#525866"})),B=e=>{switch(e){case"#FFE6E8":return t.colors.red_200;case"#F8E2CB":return t.colors.yellow_200;case"#C4F2E5":return t.colors.green_200;case"#DCD8FA":return t.colors.purple_200;case"#E2E8F8":return t.colors.blue_200;case"#F8E1F5":return t.colors.pink_200;case"#D3EFFA":return t.colors.sky_200;default:return t.colors.white}},H=c({flexDirection:"column",alignItems:"center",width:"100%",padding:"1.6rem",backgroundColor:t.colors.gray_100,borderRadius:"8px",cursor:"pointer",":hover":{backgroundColor:t.colors.gray_200,transition:"all ease 0.5s"}}),q=c({width:"100%",paddingBottom:"0.6rem",color:t.colors.black}),M=c({width:"100%",paddingBottom:"1.6rem",color:t.colors.gray_800,overflow:"hidden",whiteSpace:"nowrap",textOverflow:"ellipsis"}),V=c({justifyContent:"space-between",alignItems:"center",width:"100%",overflow:"hidden"}),f=c({alignItems:"center",gap:"0.4rem"}),W=e=>c({maxWidth:"18rem",color:B(e),whiteSpace:"nowrap",overflow:"hidden",textOverflow:"ellipsis"}),Z=(e,a,n)=>{let i=0;const d=[];for(const l of e){const r=document.createElement("span");r.style.display="inline-block",r.style.visibility="hidden",r.style.position="absolute",r.innerText=l.content,document.body.appendChild(r);const s=r.offsetWidth;if(document.body.removeChild(r),i+s*2+n>a){l.tagId==0&&d.push(l);break}i+=s*2+n,d.push(l)}return d},k=({title:e,content:a,date:n,tags:i=[],...d})=>{const l=o.useRef(null),r=Z(i,210,4);return g(b,{tag:"li",css:H,...d,children:[m(p,{tag:"body6",css:q,children:e}),m(p,{tag:"body8",css:M,children:a}),g(b,{css:V,children:[g("div",{ref:l,css:[f,{display:"flex",overflow:"hidden"}],children:[r.map(s=>m(S,{css:W(s.bgColor),bgColor:s.bgColor,children:s.content},s.tagId)),r.length<i.length&&g(p,{tag:"body8",css:{color:t.colors.gray_500},children:["+",i.length-r.length]})]}),g(b,{css:f,children:[m(D,{width:16,height:16}),m(p,{tag:"body8",css:{color:t.colors.gray_800},children:`${n.getFullYear()}.${n.getMonth()+1}.${n.getDate()}`})]})]})]})};k.__docgenInfo={description:"",methods:[],displayName:"ListItem",props:{title:{required:!0,tsType:{name:"string"},description:""},content:{required:!0,tsType:{name:"string"},description:""},date:{required:!0,tsType:{name:"Date"},description:""},tags:{required:!1,tsType:{name:"Array",elements:[{name:"signature",type:"object",raw:`{
+  tagId: number;
+  content: string;
+  bgColor: string;
+}`,signature:{properties:[{key:"tagId",value:{name:"number",required:!0}},{key:"content",value:{name:"string",required:!0}},{key:"bgColor",value:{name:"string",required:!0}}]}}],raw:"ListTag[]"},description:"",defaultValue:{value:"[]",computed:!1}}},composes:["HTMLAttributes"]};const K={title:"page/entree/ListItem",component:k,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{title:{control:{type:"text"}},content:{control:{type:"text"}},date:{control:!1},tags:{control:!1}},args:{title:"3차 정기 회의",content:"티키의 3번째 정기 회의록입니다. 티키의 3번째 정기 회의록입니다.",date:new Date}},u={args:{tags:[{tagId:0,content:"meeting",bgColor:"#FFE6E8"},{tagId:1,content:"study",bgColor:"#F8E2CB"},{tagId:2,content:"meeting",bgColor:"#FFE6E8"},{tagId:3,content:"study",bgColor:"#F8E2CB"},{tagId:4,content:"meeting",bgColor:"#FFE6E8"},{tagId:5,content:"study",bgColor:"#F8E2CB"}]},argTypes:{}},y={args:{},argTypes:{}},h={args:{tags:[{tagId:0,content:"meeting",bgColor:"#FFE6E8"}]},argTypes:{}};var E,C,F;u.parameters={...u.parameters,docs:{...(E=u.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    tags: [{
+      tagId: 0,
+      content: 'meeting',
+      bgColor: '#FFE6E8'
+    }, {
+      tagId: 1,
+      content: 'study',
+      bgColor: '#F8E2CB'
+    }, {
+      tagId: 2,
+      content: 'meeting',
+      bgColor: '#FFE6E8'
+    }, {
+      tagId: 3,
+      content: 'study',
+      bgColor: '#F8E2CB'
+    }, {
+      tagId: 4,
+      content: 'meeting',
+      bgColor: '#FFE6E8'
+    }, {
+      tagId: 5,
+      content: 'study',
+      bgColor: '#F8E2CB'
+    }]
+  },
+  argTypes: {}
+}`,...(F=(C=u.parameters)==null?void 0:C.docs)==null?void 0:F.source}}};var w,I,x;y.parameters={...y.parameters,docs:{...(w=y.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {},
+  argTypes: {}
+}`,...(x=(I=y.parameters)==null?void 0:I.docs)==null?void 0:x.source}}};var _,v,T;h.parameters={...h.parameters,docs:{...(_=h.parameters)==null?void 0:_.docs,source:{originalSource:`{
+  args: {
+    tags: [{
+      tagId: 0,
+      content: 'meeting',
+      bgColor: '#FFE6E8'
+    }]
+  },
+  argTypes: {}
+}`,...(T=(v=h.parameters)==null?void 0:v.docs)==null?void 0:T.source}}};const P=["Default","ZeroFile","SomeFile"];export{u as Default,h as SomeFile,y as ZeroFile,P as __namedExportsOrder,K as default};
---
+++ assets/WorkSpaceModal.stories-f453CSfB.js
@@ -0,0 +1,47 @@
+import{a as e,j as v,c as k}from"./emotion-react.browser.esm-jySFJa0C.js";import{B as I}from"./Button-B4fyXLCx.js";import{F as W}from"./Flex-raOKmZCB.js";import{S as w}from"./Select-Ca4AQCwd.js";import"./index-C73pvn4u.js";import"./iframe-BHwNxDEU.js";import{u as x}from"./useOverlay-B08lHsnY.js";import{W as n,a as O,b as C,c as N,d as T}from"./WorkSpaceName-D-NlNj27.js";import"./extends-ClQVCWlZ.js";import"./Button.style-CkUHqsXD.js";import"./theme-CkGtBuFl.js";import"./Label-DCVu2OlF.js";import"./scroll-C6emTpi2.js";import"./_commonjsHelpers-CzT8XOfQ.js";import"../sb-preview/runtime.js";import"./QueryClientProvider-DHAWI4ds.js";import"./path-C4I2mbmP.js";import"./file-x9g9N8Uf.js";import"./Input-Co4VqNyq.js";import"./SupportingText-BRERctdR.js";import"./Heading-Co0Rmlpl.js";import"./Text-DMTDN76W.js";(function(){try{var r=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},t=new Error().stack;t&&(r._sentryDebugIds=r._sentryDebugIds||{},r._sentryDebugIds[t]="5926fca1-25b2-4acf-add1-146f9be08eb1",r._sentryDebugIdIdentifier="sentry-dbid-5926fca1-25b2-4acf-add1-146f9be08eb1")}catch{}})();const ee={title:"Shared/WorkSpaceModal",component:n,parameters:{layout:"centered"},decorators:[r=>e(n,{children:e(r,{})})]},o={render:()=>e(O,{isVisible:!0})},s={render:()=>e(C,{isVisible:!0})},a={render:()=>{const{isOpen:r,toggle:t}=x();return v(W,{tag:"section",styles:{direction:"column",justify:"center",align:"center"},css:V,children:[e(N,{step:"category",title:"새로운 워크 스페이스 생성하기",info:"팀 카테고리를 선택해주세요."}),e("div",{css:{width:"32rem",marginTop:"2.4rem"},children:e(w,{css:{"& ul":{maxHeight:"25rem"}},placeholder:"선택",variant:"outline",isOpen:r,onTrigger:t,onSelect:t,options:[{value:"옵션 1입니다"},{value:"옵션 2입니다"}],className:"select-container"})}),e(I,{css:{width:"100%",marginTop:"18.8rem"},variant:"primary",size:"medium",children:"다음"})]})}},i={render:()=>e(T,{isVisible:!0})},V=k({height:"40rem"});var c,m,p;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: () => {
+    return <WorkSpaceName isVisible={true} />;
+  }
+}`,...(p=(m=o.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var l,d,u;s.parameters={...s.parameters,docs:{...(l=s.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: () => {
+    return <WorkSpaceComplete isVisible={true} />;
+  }
+}`,...(u=(d=s.parameters)==null?void 0:d.docs)==null?void 0:u.source}}};var g,S,f;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: () => {
+    const {
+      isOpen,
+      toggle
+    } = useOverlay();
+    return <Flex tag={'section'} styles={{
+      direction: 'column',
+      justify: 'center',
+      align: 'center'
+    }} css={Style}>
+        <WorkSpaceInfo step="category" title="새로운 워크 스페이스 생성하기" info="팀 카테고리를 선택해주세요." />
+        <div css={{
+        width: '32rem',
+        marginTop: '2.4rem'
+      }}>
+          <Select css={{
+          '& ul': {
+            maxHeight: '25rem'
+          }
+        }} placeholder="선택" variant="outline" isOpen={isOpen} onTrigger={toggle} onSelect={toggle} options={[{
+          value: '옵션 1입니다'
+        }, {
+          value: '옵션 2입니다'
+        }]} className="select-container" />
+        </div>
+        <Button css={{
+        width: '100%',
+        marginTop: '18.8rem'
+      }} variant="primary" size="medium">
+          다음
+        </Button>
+      </Flex>;
+  }
+}`,...(f=(S=a.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};var y,b,h;i.parameters={...i.parameters,docs:{...(y=i.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  render: () => {
+    return <WorkSpaceImage isVisible={true} />;
+  }
+}`,...(h=(b=i.parameters)==null?void 0:b.docs)==null?void 0:h.source}}};const re=["NameStep","CompleteStep","SelectCategoryStep","ImageUploadStep"];export{s as CompleteStep,i as ImageUploadStep,o as NameStep,a as SelectCategoryStep,re as __namedExportsOrder,ee as default};
