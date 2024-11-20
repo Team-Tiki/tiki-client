7cce44a55ade9795a665ca29d6bc7291fc017d46
namdaeun <nde40345@gmail.com>
2024-11-20 17:15:10 +0900

    fix: teamId 없을시 에러페이지로 라우팅되는 이슈 해결

--- src/shared/hook/common/useInitializeTeamId.tsx
+++ src/shared/hook/common/useInitializeTeamId.tsx
@@ -1,24 +1,17 @@
-import { useQuery } from '@tanstack/react-query';
-
-import { getClubInfo } from '@/shared/api/teams';
 import { useTeamIdAction } from '@/shared/store/team';
+
+import { useClubInfoQuery } from '../api/useClubInfoQuery';
 
 export const useInitializeTeamId = () => {
   const { setTeamId } = useTeamIdAction();
 
-  const { data, isSuccess } = useQuery({
-    queryKey: ['clubInfo'],
-    queryFn: () => getClubInfo(),
-    enabled: !localStorage.getItem('teamId'),
-  });
+  const { data, isSuccess } = useClubInfoQuery();
 
-  if (isSuccess && data?.data?.teams.length > 0) {
-    // localStorage에 teamId가 없는 경우
-    const teamId = data.data.teams.find((team) => team.teamId)!.teamId;
+  if (isSuccess && !localStorage.getItem('teamId')) {
+    const teamId = data.belongTeamGetResponses[0].id;
     localStorage.setItem('teamId', teamId.toString());
 
     setTeamId(teamId);
-
     return teamId;
   }
 
