a7f4b7854b34d2f0fba30d34ad738a5089e17405
wuzoo <wndyd9911@gmail.com>
2024-12-03 21:38:10 +0900

    chore: getAbsolutePath, mergeConfig 적용

--- apps/client/.storybook/main.ts
+++ apps/client/.storybook/main.ts
@@ -1,37 +1,48 @@
 import type { StorybookConfig } from '@storybook/react-vite';
-import path from 'path';
+import { dirname, join, resolve } from 'path';
 
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-onboarding',
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-a11y',
-    '@chromatic-com/storybook',
-    '@storybook/addon-interactions',
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/addon-a11y'),
+    getAbsolutePath('@chromatic-com/storybook'),
   ],
-  core: {
-    builder: '@storybook/builder-vite',
-  },
   framework: {
-    name: '@storybook/react-vite',
+    name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
+  docs: {
+    autodocs: 'tag',
+  },
   async viteFinal(config) {
-    config.optimizeDeps = {
-      include: ['@tiki/ui', '@tiki/icon'],
-    };
+    const { mergeConfig } = await import('vite');
+
     if (config.resolve) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        '@': path.resolve(__dirname, '../src'),
-        '@/common': path.resolve(__dirname, '../src/common'),
-        '@/page': path.resolve(__dirname, '../src/page'),
-        '@/shared': path.resolve(__dirname, '../src/shared'),
-      };
+      if (config.resolve) {
+        config.resolve.alias = {
+          ...config.resolve.alias,
+          '@': resolve(__dirname, '../src'),
+          '@/common': resolve(__dirname, '../src/common'),
+          '@/page': resolve(__dirname, '../src/page'),
+          '@/shared': resolve(__dirname, '../src/shared'),
+        };
+      }
     }
-    return config;
+
+    return mergeConfig(config, {
+      optimizeDeps: ['@tiki/ui', '@tiki/icon'],
+    });
   },
 };
 export default config;
