6025e2549b8912a06b8ec7483b89cb30e7dd26ba
namdaeun <nde40345@gmail.com>
2024-12-10 02:25:52 +0900

    feat: 팀아이디 초기화 훅에 클라이언트 적용

--- apps/client/src/shared/hook/common/useInitializeTeamId.tsx
+++ apps/client/src/shared/hook/common/useInitializeTeamId.tsx
@@ -1,14 +1,20 @@
-import { useClubInfoQuery } from '@/shared/hook/api/useClubInfoQuery';
+import { useQuery } from '@tanstack/react-query';
+
+import { client } from '@/shared/api/client';
 import { useTeamIdAction } from '@/shared/store/team';
 
 export const useInitializeTeamId = () => {
   const { setTeamId } = useTeamIdAction();
 
-  const { data, isSuccess } = useClubInfoQuery();
+  const { data, isSuccess } = useQuery({
+    queryKey: ['/api/v1/teams'],
+    queryFn: () => client.GET('/api/v1/teams'),
+    select: (data) => data.data?.data
+  });
 
   if (isSuccess && !localStorage.getItem('teamId')) {
-    const teamId = data.belongTeamGetResponses[0].id;
-    localStorage.setItem('teamId', teamId.toString());
+    const teamId = (data?.teams && data?.teams[0]?.teamId) ?? 0;
+    localStorage.setItem('teamId', teamId!.toString());
 
     setTeamId(teamId);
 
