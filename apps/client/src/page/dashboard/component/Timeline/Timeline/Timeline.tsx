9b47c93ac3590b25c691d3338829d54343feb815
namdaeun <nde40345@gmail.com>
2024-12-15 21:20:01 +0900

    fix: 대시보드 페이지도 oas로 블록 정보 가져옴

--- apps/client/src/page/dashboard/component/Timeline/Timeline/Timeline.tsx
+++ apps/client/src/page/dashboard/component/Timeline/Timeline/Timeline.tsx
@@ -4,11 +4,10 @@ import { useDateContext } from '@/page/archiving/index/DateProvider';
 import Day from '@/page/archiving/index/component/TimeLine/Day/Day';
 import { dayBodyStyle } from '@/page/archiving/index/component/TimeLine/Day/Day.style';
 import TimeBlock from '@/page/archiving/index/component/TimeLine/TimeBlock/TimeBlock';
-import { useGetTimeBlockQuery } from '@/page/archiving/index/hook/api/useGetTimeBlockQuery';
-import { Block } from '@/page/archiving/index/type/blockType';
 import { alignBlocks, createTimeBlock } from '@/page/archiving/index/util/block';
 import { timelineContentStyle } from '@/page/dashboard/component/Timeline/Timeline/Timeline.style';
 
+import { $api } from '@/shared/api/client';
 import { PATH } from '@/shared/constant/path';
 import { useInitializeTeamId } from '@/shared/hook/common/useInitializeTeamId';
 
@@ -19,20 +18,31 @@ const Timeline = () => {
 
   const { currentYear, currentMonth, endDay } = useDateContext();
 
-  const { data } = useGetTimeBlockQuery(teamId, 'executive', currentYear, currentMonth);
+  const { data } = $api.useSuspenseQuery('get', '/api/v1/teams/{teamId}/timeline', {
+    params: {
+      query: {
+        type: 'executive',
+        date: `${currentYear}-${currentMonth.toString().padStart(2, '0')}`,
+      },
+      path: {
+        teamId,
+      },
+    },
+  });
 
-  const timeBlocks: Block[] = data.timeBlocks;
+  const timeBlocks = data.data?.timeBlocks;
+
   const blockFloors = alignBlocks(timeBlocks, endDay, currentMonth, currentYear);
 
   return (
     <>
       <Day />
       <div css={[dayBodyStyle(endDay.getDate()), timelineContentStyle]}>
-        {timeBlocks.map((block) => {
+        {timeBlocks?.map((block) => {
           const { startDate, endDate } = block;
           const { startDate: blockStartDate, endDate: blockEndDate } = createTimeBlock({
-            startDate: new Date(startDate),
-            endDate: new Date(endDate),
+            startDate: startDate ? new Date(startDate) : new Date(),
+            endDate: endDate ? new Date(endDate) : new Date(),
             currentYear,
             currentMonth,
           });
@@ -42,9 +52,9 @@ const Timeline = () => {
               key={block.timeBlockId}
               startDate={blockStartDate}
               endDate={blockEndDate}
-              color={block.color}
-              floor={blockFloors[block.timeBlockId] || 1}
-              blockType={block.blockType}
+              color={block.color!}
+              floor={blockFloors[block.timeBlockId ?? 0]}
+              blockType={block.blockType ?? 'MEETING'}
               onBlockClick={() => {
                 navigate(PATH.ARCHIVING, { state: { selectedBlock: block } });
               }}>
