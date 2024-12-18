87529cbea008284253eade69a50e47f6586986bd
namdaeun <nde40345@gmail.com>
2024-12-18 18:40:56 +0900

    feat: path 추가

--- apps/client/src/common/router/Router.tsx
+++ apps/client/src/common/router/Router.tsx
@@ -23,6 +23,7 @@ import {
 } from '@/common/router/lazy';
 
 import HandoverPage from '@/page/handover/HandoverPage';
+import CreateNotePage from '@/page/handover/note/component/CreateNote/CreateNote';
 
 import { PATH } from '@/shared/constant/path';
 
@@ -176,6 +177,14 @@ const router = createBrowserRouter([
         element: (
           <Suspense>
             <HandoverNotePage />
+          </Suspense>
+        ),
+      },
+      {
+        path: PATH.CREATE_HANDOVER_NOTE,
+        element: (
+          <Suspense>
+            <CreateNotePage />
           </Suspense>
         ),
       },
