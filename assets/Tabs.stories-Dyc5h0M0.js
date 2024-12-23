9f33574fd356208f3b36b137524b6c3d327b6e7e
wuzoo <wuzoo@users.noreply.github.com>
2024-12-23 11:32:53 +0000

    deploy: 23cd61e596b46a7f7cfb4ebf6ed1351a3ba7cbab

---
+++ assets/Tabs.stories-Dyc5h0M0.js
@@ -0,0 +1,18 @@
+import{a as t,F as T,j as r}from"./emotion-react-jsx-runtime.browser.esm-D5nmPLjQ.js";import{b as i,o as u,t as o,c as m}from"./index-DkpI3aA6.js";import{r as f}from"./index-BduvBmAW.js";import"./iframe-B8P7X-_7.js";import"./jsx-runtime-DpmpPDAD.js";import"./index-BqWoICCD.js";import"./_commonjsHelpers-CmZWRUSM.js";import"../sb-preview/runtime.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},a=new e.Error().stack;a&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[a]="50bf7faf-0890-40f4-9236-6c364eba1121",e._sentryDebugIdIdentifier="sentry-dbid-50bf7faf-0890-40f4-9236-6c364eba1121")}catch{}})();const S={title:"Common/Tab",component:i,parameters:{layout:"centered"},tags:["autodocs"],args:{children:t(T,{})}},n={render:()=>{const[e,a]=f.useState(0);return r(i,{children:[r(u,{selectedTab:e,onTabClick:l=>{a(l)},children:[t(o,{variant:"round",children:"선택된 블록"}),t(o,{variant:"round",children:"전체 문서"})]}),r(m,{selectedTab:e,children:[t("div",{children:"1번 탭"}),t("div",{children:"2번 탭"})]})]})}};var s,d,c;n.parameters={...n.parameters,docs:{...(s=n.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: () => {
+    const [selectedTab, setSelectedTab] = useState(0);
+    const handleTabClick = (tabId: number) => {
+      setSelectedTab(tabId);
+    };
+    return <TabRoot>
+        <TabList selectedTab={selectedTab} onTabClick={handleTabClick}>
+          <TabButton variant="round">선택된 블록</TabButton>
+          <TabButton variant="round">전체 문서</TabButton>
+        </TabList>
+        <TabPanel selectedTab={selectedTab}>
+          <div>1번 탭</div>
+          <div>2번 탭</div>
+        </TabPanel>
+      </TabRoot>;
+  }
+}`,...(c=(d=n.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};const _=["Tabtest"];export{n as Tabtest,_ as __namedExportsOrder,S as default};
