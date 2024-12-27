6f7303f3260c7a816eb3b7fbe3d35a55cea3c7d3
Jeongbowoon <happyanne200212@gmail/com>
2024-12-27 08:02:52 -0800

    feat: query 구조짜기

---
+++ apps/client/src/page/handover/hook/api/queries.ts
@@ -0,0 +1,18 @@
+import { $api } from '@/shared/api/client';
+import { useTeamId } from '@/shared/store/team';
+
+export const useNoteData = (createdAt: string, sortOrder: 'ASC' | 'DESC') => {
+  const teamId = useTeamId();
+
+  return $api.useQuery('get', '/api/v1/notes/{teamId}', {
+    params: {
+      path: {
+        teamId,
+      },
+      query: {
+        createdAt: 'dd',
+        sortOrder: 'ASC',
+      },
+    },
+  });
+};
