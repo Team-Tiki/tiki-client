e82ac44867279a4072125f84db35578a7519a014
namdaeun <nde40345@gmail.com>
2024-12-30 16:21:33 +0900

    feat: 합성 컴포넌트 구조

---
+++ apps/client/src/shared/component/Modal/Footer/ModalFooter.tsx
@@ -0,0 +1,54 @@
+export interface FooterButton {
+  text: string;
+  onClick?: () => void;
+  variant: 'primary' | 'secondary' | 'tertiary' | 'outline' | 'underline';
+  disabled?: boolean;
+}
+
+export const getFooterContent = (
+  contentType: string,
+  step: number,
+  buttonClick?: () => void,
+  closeModal?: () => void,
+  isButtonActive: boolean = true
+): FooterButton[] => {
+  switch (contentType) {
+    case 'create-workspace':
+      return [
+        step >= 3 ? { text: '건너뛰기', onClick: buttonClick, variant: 'outline' } : false,
+        {
+          text: step === 4 ? '확인' : '다음으로',
+          onClick: buttonClick,
+          variant: 'primary',
+          disabled: !isButtonActive,
+        },
+      ].filter(Boolean) as FooterButton[];
+
+    /* 디자인 확정시 추후 수정 필요 */
+    case 'create-block':
+      return [
+        { text: '취소', onClick: closeModal, variant: 'outline' },
+        { text: '다음으로', onClick: buttonClick, variant: 'primary' },
+      ];
+
+    case 'deleted':
+      return [
+        { text: '취소', onClick: closeModal, variant: 'outline' },
+        { text: '삭제', onClick: buttonClick, variant: 'primary' },
+      ];
+
+    case 'invite':
+      return [
+        { text: '취소', onClick: closeModal, variant: 'outline' },
+        { text: '초대', onClick: buttonClick, variant: 'primary', disabled: !isButtonActive },
+      ];
+    case 'member-tag':
+    case 'activity-tag':
+      return [
+        { text: '취소', onClick: closeModal, variant: 'outline' },
+        { text: '완료', onClick: buttonClick, variant: 'primary' },
+      ];
+    default:
+      return [];
+  }
+};
