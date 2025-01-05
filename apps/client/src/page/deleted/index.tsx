aae653cbfdb17e9d2792e14c2f35fb55bce063cd
Jeongbowoon <happyanne200212@gmail/com>
2025-01-04 19:43:56 -0800

    feat: 휴지통 delete, restore api mutation 사용해서 구현

--- apps/client/src/page/deleted/index.tsx
+++ apps/client/src/page/deleted/index.tsx
@@ -21,7 +21,7 @@ interface FileData {
 const DeletedPage = () => {
   const teamId = useInitializeTeamId();
 
-  const { data } = $api.useQuery('get', '/api/v1/teams/{teamId}/trash', {
+  const { data, refetch } = $api.useQuery('get', '/api/v1/teams/{teamId}/trash', {
     params: {
       path: {
         teamId,
@@ -29,17 +29,70 @@ const DeletedPage = () => {
     },
   });
 
-  const { ids, canSelect, handleItemClick, handleAllClick, handleToggleSelect } = useMultiSelect<FileData>(
+  const { ids, canSelect, handleItemClick, handleAllClick, handleToggleSelect, handleReset } = useMultiSelect<FileData>(
     'documentId',
     data?.data?.deletedDocuments || []
   );
+
+  const deleteMutation = $api.useMutation('delete', '/api/v1/teams/{teamId}/trash', {
+    onSuccess: () => {
+      refetch();
+    },
+    onError: (error) => {
+      console.error('삭제 실패:', error);
+    },
+  });
+
+  const handleDelete = (docs?: number[]) => {
+    deleteMutation.mutate({
+      params: {
+        path: {
+          teamId,
+        },
+        query: {
+          documentId: docs ? docs : ids,
+        },
+      },
+    });
+    handleReset();
+  };
+
+  const restoreMutation = $api.useMutation('post', '/api/v1/teams/{teamId}/trash', {
+    onSuccess: () => {
+      refetch();
+    },
+    onError: (error) => {
+      console.error('복원 실패:', error);
+    },
+  });
+
+  const handleRestore = () => {
+    restoreMutation.mutate({
+      params: {
+        path: {
+          teamId,
+        },
+        query: {
+          documentId: ids,
+        },
+      },
+    });
+    handleReset();
+  };
 
   return (
     <ContentBox
       variant="deleted"
       title="휴지통"
       description="5.16GB 사용 가능"
-      headerOption={<Button>휴지통 비우기</Button>}
+      headerOption={
+        <Button
+          onClick={() => {
+            data?.data?.deletedDocuments && handleDelete(data?.data?.deletedDocuments.map((item) => item.documentId));
+          }}>
+          휴지통 비우기
+        </Button>
+      }
       contentOption={
         <Flex styles={{ justify: 'space-between', align: 'center' }}>
           {canSelect ? (
@@ -47,8 +100,12 @@ const DeletedPage = () => {
               <Button onClick={handleAllClick} variant="tertiary">
                 전체 선택
               </Button>
-              <Button variant="tertiary">복구</Button>
-              <Button variant="tertiary">영구삭제</Button>
+              <Button variant="tertiary" onClick={handleRestore}>
+                복구
+              </Button>
+              <Button variant="tertiary" onClick={() => handleDelete()}>
+                영구삭제
+              </Button>
             </Flex>
           ) : (
             <Button onClick={handleToggleSelect} variant="tertiary">
