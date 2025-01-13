6e8cd3f29a0aa6ca2d828a807e01ac0578a7fa43
Jeongbowoon <happyanne200212@gmail/com>
2025-01-13 22:48:12 +0900

    feat: generate, Handover섹션 마무리

--- apps/client/src/page/dashboard/component/Handover/HandoverSection.tsx
+++ apps/client/src/page/dashboard/component/Handover/HandoverSection.tsx
@@ -1,28 +1,23 @@
-import { Flex } from '@tiki/ui';
-
-import { useRef } from 'react';
+import { Flex, Spinner } from '@tiki/ui';
 
 import { listItemStyle } from '@/page/dashboard/component/Handover/HandoverSection.style';
 import ListItem from '@/page/dashboard/component/Handover/ListItem/ListItem';
+import { useCurrentDate } from '@/page/dashboard/component/Handover/hook/useCurrentDate';
 import { useNoteData } from '@/page/handover/hook/api/queries';
 
 const HandoverSection = () => {
-  const createdAt = useRef<string>(new Date().toISOString().slice(0, -1)).current;
-
-  const { data } = useNoteData(createdAt);
+  const createdAt = useCurrentDate();
+  const { data, isFetching } = useNoteData(createdAt);
 
   return (
     <Flex css={listItemStyle}>
-      {data?.data?.noteGetResponseList.map((note) => {
-        return (
-          <ListItem
-            key={note.noteId}
-            title={note.title}
-            content={'노트 상세조회 api 연결 필요'}
-            date={note.startDate}
-          />
-        );
-      })}
+      {isFetching ? (
+        <Spinner size={30} />
+      ) : (
+        data?.data?.noteGetResponseList.map((note) => {
+          return <ListItem key={note.noteId} noteId={note.noteId} title={note.title} date={note.startDate} />;
+        })
+      )}
     </Flex>
   );
 };
