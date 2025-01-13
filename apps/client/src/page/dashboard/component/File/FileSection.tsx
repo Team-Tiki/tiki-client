6146315122467963be47bbf7a17ec8e6f0e83055
Jeongbowoon <happyanne200212@gmail/com>
2025-01-13 22:13:01 +0900

    feat: file section api연결

--- apps/client/src/page/dashboard/component/File/FileSection.tsx
+++ apps/client/src/page/dashboard/component/File/FileSection.tsx
@@ -1,28 +1,53 @@
 import { Flex, scrollStyle } from '@tiki/ui';
+import { hasKeyInObject } from '@tiki/utils';
 
 import { dashboradScrollStyle } from '@/page/dashboard/DashboardPage.style';
+import { useDriveData } from '@/page/drive/hook/api/queries';
+import { DocumentItem, FolderItem } from '@/page/drive/type';
 
 import FileGrid from '@/shared/component/FileGrid/FileGrid';
+import FolderGrid from '@/shared/component/FileGrid/FolderGrid';
 import { File } from '@/shared/type/file';
 import { extractFileExtension } from '@/shared/util/file';
 
-import { FileData } from '@/mock/data/drive';
-
 const FileSection = () => {
+  const { data: fileData } = useDriveData();
+
+  const docDataList = fileData.data?.documents || [];
+  const folderDataList = fileData.data?.folders || [];
+
+  const allFileData = [...docDataList, ...folderDataList];
+
   return (
     <Flex css={[{ gap: '1.4rem', padding: '0 0 0.7rem', overflowX: 'scroll' }, scrollStyle, dashboradScrollStyle]}>
-      {FileData.map((file) => {
-        return (
-          <FileGrid
-            key={file.documentId}
-            variant="secondary"
-            name={file.name}
-            type={extractFileExtension(file.name) as File}
-            capacity={file.capacity}
-            createdTime={file.createdTime}
-            url={file.url}
-          />
-        );
+      {allFileData.map((item) => {
+        if (hasKeyInObject(item, 'documentId')) {
+          const file = item as DocumentItem;
+
+          return (
+            <FileGrid
+              key={file.documentId}
+              variant="secondary"
+              name={file.name}
+              type={extractFileExtension(file.name) as File}
+              capacity={file.capacity}
+              createdTime={file.createdTime}
+              url={file.url}
+            />
+          );
+        } else if (hasKeyInObject(item, 'folderId')) {
+          const folder = item as FolderItem;
+
+          return (
+            <FolderGrid
+              key={folder.folderId}
+              variant="secondary"
+              name={folder.name}
+              createdTime={folder.createdTime}
+              path={folder.path}
+            />
+          );
+        }
       })}
     </Flex>
   );
