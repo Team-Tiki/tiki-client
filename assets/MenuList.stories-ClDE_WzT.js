27f7165183383462f589ef70fbc41b5d291b83d1
wuzoo <wuzoo@users.noreply.github.com>
2025-01-13 15:42:02 +0000

    deploy: ae9f4080f940316e472e6f0befbd6c3cc515c53b

---
+++ assets/MenuList.stories-ClDE_WzT.js
@@ -0,0 +1,32 @@
+import{j as s,a as e,F as g}from"./emotion-react-jsx-runtime.browser.esm-BLpwO81r.js";import{C as u,R as h,c as i,T as c}from"./index-Dm3mlpfe.js";import"./index-BmApkmR2.js";import"./index-XAP1w_DE.js";import{u as m}from"./useOverlay-zuyXJw5A.js";import"./iframe-C2BBeU7Z.js";import"./jsx-runtime-kScofH-k.js";import"./_commonjsHelpers-BehdHG3Q.js";import"../sb-preview/runtime.js";(function(){try{var t=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},n=new t.Error().stack;n&&(t._sentryDebugIds=t._sentryDebugIds||{},t._sentryDebugIds[n]="6da137d0-2208-4771-a321-7b9c4b874486",t._sentryDebugIdIdentifier="sentry-dbid-6da137d0-2208-4771-a321-7b9c4b874486")}catch{}})();const O={title:"Common/Menu",component:u,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{children:{control:!1}},args:{onClose:()=>{},children:s(g,{children:[e("button",{children:"trigger button"}),s("ul",{children:[e("li",{children:"item 1"}),e("li",{children:"item 2"})]})]})}},r={render:({})=>{const{isOpen:t,close:n,toggle:o}=m();return s(u,{onClose:n,children:[e("button",{type:"button",onClick:o,children:"Trigger"}),s(h,{isOpen:t,css:{left:"100%",top:0},children:[e(c,{LeftIcon:e(i,{width:16,height:16}),onSelect:o,children:"이름 변경"}),e(c,{LeftIcon:e(i,{width:16,height:16}),onSelect:o,children:"휴지통으로 이동"}),e(c,{LeftIcon:e(i,{width:16,height:16}),onSelect:o,children:"폴더 전체 다운로드"})]})]})},args:{onClose:()=>{}},argTypes:{}};var l,a,d;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: ({}) => {
+    const {
+      isOpen,
+      close,
+      toggle
+    } = useOverlay();
+    return <MenuRoot onClose={close}>
+        <button type="button" onClick={toggle}>
+          Trigger
+        </button>
+        <MenuList isOpen={isOpen} css={{
+        left: '100%',
+        top: 0
+      }}>
+          <MenuItem LeftIcon={<IcPencil width={16} height={16} />} onSelect={toggle}>
+            이름 변경
+          </MenuItem>
+          <MenuItem LeftIcon={<IcPencil width={16} height={16} />} onSelect={toggle}>
+            휴지통으로 이동
+          </MenuItem>
+          <MenuItem LeftIcon={<IcPencil width={16} height={16} />} onSelect={toggle}>
+            폴더 전체 다운로드
+          </MenuItem>
+        </MenuList>
+      </MenuRoot>;
+  },
+  args: {
+    onClose: () => {}
+  },
+  argTypes: {}
+}`,...(d=(a=r.parameters)==null?void 0:a.docs)==null?void 0:d.source}}};const S=["Default"];export{r as Default,S as __namedExportsOrder,O as default};
---
+++ assets/NewModals.stories-F2y7I551.js
@@ -0,0 +1,11 @@
+import{j as s,a as t,F as l}from"./emotion-react-jsx-runtime.browser.esm-BLpwO81r.js";import{M as i,u as p}from"./ModalFunnel-BTGPUBcA.js";import"./iframe-C2BBeU7Z.js";import"./jsx-runtime-kScofH-k.js";import"./index-BmApkmR2.js";import"./_commonjsHelpers-BehdHG3Q.js";import"./index-Dm3mlpfe.js";import"./index-XAP1w_DE.js";import"./emotion-react.browser.esm-Bc39sUzm.js";import"./QueryClientProvider-C49L4e0C.js";import"./path-CCsHwj6c.js";import"./index-p5U0ccZK.js";import"./ellipsis-DaZHQs-E.js";import"./file-CXHR9zBX.js";import"./date-8RG4o7Ni.js";import"./useOutsideClick-zUlqBe3B.js";import"./useOverlay-zuyXJw5A.js";import"../sb-preview/runtime.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},n=new e.Error().stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="a10dbe7f-64a4-4411-ac12-48d689811d4d",e._sentryDebugIdIdentifier="sentry-dbid-a10dbe7f-64a4-4411-ac12-48d689811d4d")}catch{}})();const F={title:"Shared/Modal/ModalsTest",component:i,parameters:{layout:"centered"},args:{isOpen:!1},argTypes:{children:{control:!1}}},o={render:()=>{const e=p();return s(l,{children:[t("button",{onClick:()=>e("invite"),children:"Open Invite Modal"}),t("button",{onClick:()=>e("member-tag"),children:"Open MemberTag Modal"}),t("button",{onClick:()=>e("activity-tag"),children:"Open ActivityTag Modal"}),t(i,{})]})}};var r,a,d;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: () => {
+    const openModal = useOpenModal();
+    return <>
+        <button onClick={() => openModal('invite')}>Open Invite Modal</button>
+        <button onClick={() => openModal('member-tag')}>Open MemberTag Modal</button>
+        <button onClick={() => openModal('activity-tag')}>Open ActivityTag Modal</button>
+        <ModalFunnel />
+      </>;
+  }
+}`,...(d=(a=o.parameters)==null?void 0:a.docs)==null?void 0:d.source}}};const j=["ModalsTest"];export{o as ModalsTest,j as __namedExportsOrder,F as default};
