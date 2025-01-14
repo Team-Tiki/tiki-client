8fd1392c82cb4f6d5f879595329271d34912e312
Jeongbowoon <happyanne200212@gmail/com>
2025-01-15 05:31:28 +0900

    feat: main add icon 추가

---
+++ packages/icon/src/IcMainAdd.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgIcMainAdd = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 40 40" {...props}>
+    <path d="M30 21.667h-8.333V30c0 .917-.75 1.667-1.667 1.667s-1.667-.75-1.667-1.667v-8.333H10c-.917 0-1.667-.75-1.667-1.667s.75-1.667 1.667-1.667h8.333V10c0-.917.75-1.667 1.667-1.667s1.667.75 1.667 1.667v8.333H30c.917 0 1.667.75 1.667 1.667s-.75 1.667-1.667 1.667" />
+  </svg>
+);
+export default SvgIcMainAdd;
