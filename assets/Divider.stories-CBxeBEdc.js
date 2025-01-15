36645936401a3cb338dd2436f07ae2eabccbf563
namdaeun <namdaeun@users.noreply.github.com>
2025-01-15 08:52:37 +0000

    deploy: 750ae590b8d164ee44cb11d3fbc2f46f74c5fa39

---
+++ assets/Divider.stories-CBxeBEdc.js
@@ -0,0 +1,11 @@
+import{a}from"./emotion-react-jsx-runtime.browser.esm-CIvUDXXq.js";import{_ as l}from"./index-Dva4CvGw.js";import"./iframe-yFOjao1W.js";import"./jsx-runtime-FPo-99VL.js";import"./index-B6suwDpA.js";import"./_commonjsHelpers-BR0xJWvp.js";import"./index-DoADfGKy.js";import"../sb-preview/runtime.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},t=new e.Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]="7a029857-2133-43b6-9cfc-d5ac823da801",e._sentryDebugIdIdentifier="sentry-dbid-7a029857-2133-43b6-9cfc-d5ac823da801")}catch{}})();const v={title:"Common/Divider",component:l,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{type:{control:{type:"radio"},options:["horizontal","vertical"]},size:{control:{type:"number"}},color:{control:{type:"color"}}},args:{type:"horizontal",size:50},decorators:[e=>a("div",{style:{width:"70rem",height:"5rem"},children:a(e,{})})]},r={args:{type:"horizontal",size:100}},o={args:{type:"vertical",size:100}};var s,n,i;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    type: 'horizontal',
+    size: 100
+  }
+}`,...(i=(n=r.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};var c,d,p;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    type: 'vertical',
+    size: 100
+  }
+}`,...(p=(d=o.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};const w=["Horizontal","Vertical"];export{r as Horizontal,o as Vertical,w as __namedExportsOrder,v as default};
