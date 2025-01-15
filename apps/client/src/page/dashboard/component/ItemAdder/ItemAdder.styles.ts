e24fed9f9d3c64d4b047542dea99317046f1987a
Jeongbowoon <happyanne200212@gmail/com>
2025-01-15 11:35:43 +0900

    feat: timeline section에 main add 적용

--- apps/client/src/page/dashboard/component/ItemAdder/ItemAdder.styles.ts
+++ apps/client/src/page/dashboard/component/ItemAdder/ItemAdder.styles.ts
@@ -9,9 +9,9 @@ export const adderStyle = (path: string) =>
     justifyContent: 'center',
     alignItems: 'center',
 
-    padding: path === PATH.DRIVE ? '4rem 6rem' : path === PATH.HANDOVER ? '3rem' : '9.8rem 36.8rem',
+    padding: path === PATH.DRIVE ? '4rem 6rem' : path === PATH.HANDOVER ? '3rem' : '9.8rem',
 
-    width: path === PATH.HANDOVER ? '100%' : '',
+    width: path !== PATH.DRIVE ? '100%' : '',
 
     border: 'none',
     borderRadius: '8px',
