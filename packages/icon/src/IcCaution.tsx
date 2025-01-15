476b590a38e3b82045c2137d7baccbdc8c9a353c
namdaeun <nde40345@gmail.com>
2025-01-15 18:52:09 +0900

    chore: solve lint error

--- packages/icon/src/IcCaution.tsx
+++ packages/icon/src/IcCaution.tsx
@@ -1,9 +1,11 @@
 import * as React from "react";
 import type { SVGProps } from "react";
 const SvgIcCaution = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16" {...props}>
-    <path fill="#FE4F60" d="M14.7 8a6.7 6.7 0 1 1-13.4-.001 6.7 6.7 0 0 1 13.4 0" />
-    <path fill="#fff" d="M8.6 10H7.3v1.3h1.3zM8.6 4.7H7.3v4h1.3z" />
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 40 40" {...props}>
+    <path
+      fill="#525866"
+      d="M28.5 24.695 21.668 12.83a1.934 1.934 0 0 0-3.336 0L11.5 24.695a1.84 1.84 0 0 0 0 1.853 1.9 1.9 0 0 0 1.668.952h13.664a1.9 1.9 0 0 0 1.918-1.879c0-.325-.086-.645-.25-.926m-9.125-6.57a.625.625 0 1 1 1.25 0v3.125a.625.625 0 0 1-1.25 0zM20 25a.938.938 0 1 1 0-1.875A.938.938 0 0 1 20 25"
+    />
   </svg>
 );
 export default SvgIcCaution;
