9917b57030911eabe61938d947365c6e175e3047
bowoon <105543811+Bowoon1216@users.noreply.github.com>
2025-01-30 20:19:58 +0900

    [Feat] 대시보드 뷰 1차 기능 QA (#438)
    * fix: dashboard page  인수인계노트 조회 성공
    
    * feat: 노트, 파일 클릭시 각 페이지로 이동
    
    * fix: 헤더 스타일 수정
    
    ---------
    
    Co-authored-by: Jeongbowoon <happyanne200212@gmail/com>

--- apps/client/src/page/dashboard/component/Handover/HandoverSection.tsx
+++ apps/client/src/page/dashboard/component/Handover/HandoverSection.tsx
@@ -1,16 +1,17 @@
 import { Flex, Spinner } from '@tiki/ui';
 
+import { useNavigate } from 'react-router-dom';
+
 import { listItemStyle } from '@/page/dashboard/component/Handover/HandoverSection.style';
 import ListItem from '@/page/dashboard/component/Handover/ListItem/ListItem';
 import ItemAdder from '@/page/dashboard/component/ItemAdder/ItemAdder';
 import { useNoteData } from '@/page/handover/hook/api/queries';
 
 import { PATH } from '@/shared/constant/path';
-import { useCurrentDate } from '@/shared/hook/common/useCurrentDate';
 
 const HandoverSection = () => {
-  const createdAt = useCurrentDate();
-  const { data, isPending } = useNoteData(createdAt);
+  const navigate = useNavigate();
+  const { data, isPending } = useNoteData('');
 
   return (
     <Flex styles={{ direction: 'column', gap: '0.8rem', align: 'center' }} css={listItemStyle}>
@@ -20,7 +21,17 @@ const HandoverSection = () => {
         <Spinner size={30} />
       ) : (
         data?.data?.noteGetResponseList.map((note) => {
-          return <ListItem key={note.noteId} noteId={note.noteId} title={note.title} date={note.startDate} />;
+          return (
+            <ListItem
+              key={note.noteId}
+              noteId={note.noteId}
+              title={note.title}
+              date={note.startDate}
+              onClick={() => {
+                navigate(`${PATH.HANDOVER}/${note.noteId}`);
+              }}
+            />
+          );
         })
       )}
     </Flex>
