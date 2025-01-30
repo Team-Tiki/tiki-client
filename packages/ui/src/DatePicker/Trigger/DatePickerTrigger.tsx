6c7ae5c6ba4652dbc3b3edb789cad74ca08bf741
kimkyuhong <bobo3766@naver.com>
2025-01-30 10:59:41 +0900

    refactor: 데이트피커 날짜 가운데 정렬

--- packages/ui/src/DatePicker/Trigger/DatePickerTrigger.tsx
+++ packages/ui/src/DatePicker/Trigger/DatePickerTrigger.tsx
@@ -1,44 +1,55 @@
-import { format } from "date-fns";
+import { format } from 'date-fns';
 
-import { textStyle } from "@/DatePicker/Trigger/DatePickerTrigger.style";
-import Flex from "@/Flex/Flex";
-import Input from "@/Input/Input";
-import Text from "@/Text/Text";
-import { theme } from "@/theme";
+import { textStyle } from '@/DatePicker/Trigger/DatePickerTrigger.style';
+import Flex from '@/Flex/Flex';
+import Input from '@/Input/Input';
+import Text from '@/Text/Text';
+import { theme } from '@/theme';
 
 interface DatePickerTriggerProps {
   selectedDate: Date | null;
   endDate?: Date | null;
   onClick: () => void;
-  variant: "single" | "range";
+  variant: 'single' | 'range';
 }
 
-const DatePickerTrigger = ({ selectedDate, endDate, onClick, variant }: DatePickerTriggerProps) => {
+const DatePickerTrigger = ({
+  selectedDate,
+  endDate,
+  onClick,
+  variant,
+}: DatePickerTriggerProps) => {
   return (
-    <Flex styles={{ direction: "row", align: "center", gap: "0.4rem" }}>
+    <Flex styles={{ direction: 'row', align: 'center', gap: '0.4rem' }}>
       <Input
-        value={selectedDate ? format(selectedDate, "yyyy.MM.dd") : ""}
+        value={selectedDate ? format(selectedDate, 'yyyy.MM.dd') : ''}
         placeholder="YYYY.MM.DD"
         readOnly
         onClick={onClick}
-        css={{ cursor: "pointer", width: "100%", ...theme.text.body06, "::placeholder": { ...theme.text.body06 } }}
+        css={{
+          textAlign: 'center',
+          cursor: 'pointer',
+          width: '100%',
+          ...theme.text.body06,
+          '::placeholder': { ...theme.text.body06 },
+        }}
       />
-      {variant === "range" && (
+      {variant === 'range' && (
         <>
           <Text tag="body6" css={textStyle}>
             ~
           </Text>
           <Input
-            value={endDate ? format(endDate, "yyyy.MM.dd") : ""}
+            value={endDate ? format(endDate, 'yyyy.MM.dd') : ''}
             placeholder="YYYY.MM.DD"
             readOnly
             onClick={onClick}
             css={{
-              cursor: "pointer",
-              width: "100%",
+              cursor: 'pointer',
+              width: '100%',
               ...theme.text.body06,
-              textAlign: "center",
-              "::placeholder": { ...theme.text.body06 },
+              textAlign: 'center',
+              '::placeholder': { ...theme.text.body06 },
             }}
           />
         </>
