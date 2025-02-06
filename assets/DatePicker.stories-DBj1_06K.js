44a1a3535bff45d78680d41368ae0c426ec31aad
rtttr1 <rtttr1@users.noreply.github.com>
2025-02-06 09:05:44 +0000

    deploy: abf4cfde2c66f8c79e6efeeeead20e04d0cd6875

---
+++ assets/DatePicker.stories-DBj1_06K.js
@@ -0,0 +1,25 @@
+import{a as r,F as f}from"./emotion-react-jsx-runtime.browser.esm-Bno0mMfS.js";import{F as o}from"./index-BxOMTCpg.js";import{r as s}from"./index-ChHZxbfw.js";import"./iframe-YCzp-TpO.js";import"./jsx-runtime-DoRjULTq.js";import"./index-CzD8Iytt.js";import"./_commonjsHelpers-C4le90cc.js";import"../sb-preview/runtime.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},t=new e.Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]="e28e2d6f-f79a-44cd-ba22-f72a370ddc75",e._sentryDebugIdIdentifier="sentry-dbid-e28e2d6f-f79a-44cd-ba22-f72a370ddc75")}catch{}})();const P={title:"Common/DatePicker",component:o,parameters:{layout:"centered"},tags:["autodocs"]},a={render:()=>{const[,e]=s.useState(null),[,t]=s.useState(null);return r(f,{children:r(o,{variant:"single",triggerWidth:"10.3rem",onChange:(d,l)=>{e(d),t(l)},defaultSelectedDate:new Date("2024-05-12")})})}},n={render:()=>{const[,e]=s.useState(null),[,t]=s.useState(null);return r(f,{children:r(o,{variant:"range",triggerWidth:"10.3rem",onChange:(d,l)=>{e(d),t(l)},defaultSelectedDate:new Date("2024-05-12"),defaultEndDate:new Date("2024-05-17")})})}};var u,D,i;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  render: () => {
+    const [, setSelectedDate] = useState<Date | null>(null);
+    const [, setEndDate] = useState<Date | null>(null);
+    const handleDateChange = (start: Date | null, end: Date | null) => {
+      setSelectedDate(start);
+      setEndDate(end);
+    };
+    return <>
+        <DatePicker variant="single" triggerWidth="10.3rem" onChange={handleDateChange} defaultSelectedDate={new Date('2024-05-12')} />
+      </>;
+  }
+}`,...(i=(D=a.parameters)==null?void 0:D.docs)==null?void 0:i.source}}};var g,m,h;n.parameters={...n.parameters,docs:{...(g=n.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: () => {
+    const [, setSelectedDate] = useState<Date | null>(null);
+    const [, setEndDate] = useState<Date | null>(null);
+    const handleDateChange = (start: Date | null, end: Date | null) => {
+      setSelectedDate(start);
+      setEndDate(end);
+    };
+    return <>
+        <DatePicker variant="range" triggerWidth="10.3rem" onChange={handleDateChange} defaultSelectedDate={new Date('2024-05-12')} defaultEndDate={new Date('2024-05-17')} />
+      </>;
+  }
+}`,...(h=(m=n.parameters)==null?void 0:m.docs)==null?void 0:h.source}}};const _=["SingleDatePicker","RangeDatePicker"];export{n as RangeDatePicker,a as SingleDatePicker,_ as __namedExportsOrder,P as default};
---
+++ assets/MenuList.stories-CZ0YKi1l.js
@@ -0,0 +1,32 @@
+import{j as c,a as e,F as g}from"./emotion-react-jsx-runtime.browser.esm-Bno0mMfS.js";import{b as s}from"./index-DACGP-7d.js";import{a as u,W as m,I as i}from"./index-BxOMTCpg.js";import"./index-ChHZxbfw.js";import"./index-CzD8Iytt.js";import{u as h}from"./useOverlay-0ZfnkR7w.js";import"./iframe-YCzp-TpO.js";import"./jsx-runtime-DoRjULTq.js";import"./_commonjsHelpers-C4le90cc.js";import"../sb-preview/runtime.js";(function(){try{var t=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},n=new t.Error().stack;n&&(t._sentryDebugIds=t._sentryDebugIds||{},t._sentryDebugIds[n]="5354b5ba-0dc2-4dec-b80c-dace5cfc8bb0",t._sentryDebugIdIdentifier="sentry-dbid-5354b5ba-0dc2-4dec-b80c-dace5cfc8bb0")}catch{}})();const S={title:"Common/Menu",component:u,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{children:{control:!1}},args:{onClose:()=>{},children:c(g,{children:[e("button",{children:"trigger button"}),c("ul",{children:[e("li",{children:"item 1"}),e("li",{children:"item 2"})]})]})}},r={render:({})=>{const{isOpen:t,close:n,toggle:o}=h();return c(u,{onClose:n,children:[e("button",{type:"button",onClick:o,children:"Trigger"}),c(m,{isOpen:t,css:{left:"100%",top:0},children:[e(i,{LeftIcon:e(s,{width:16,height:16}),onSelect:o,children:"이름 변경"}),e(i,{LeftIcon:e(s,{width:16,height:16}),onSelect:o,children:"휴지통으로 이동"}),e(i,{LeftIcon:e(s,{width:16,height:16}),onSelect:o,children:"폴더 전체 다운로드"})]})]})},args:{onClose:()=>{}},argTypes:{}};var l,d,a;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: ({}) => {
+    const {
+      isOpen,
+      close,
+      toggle
+    } = useOverlay();
+    return <MenuRoot onClose={close}>
+        <button type="button" onClick={toggle}>
+          Trigger
+        </button>
+        <MenuList isOpen={isOpen} css={{
+        left: '100%',
+        top: 0
+      }}>
+          <MenuItem LeftIcon={<IcPencil width={16} height={16} />} onSelect={toggle}>
+            이름 변경
+          </MenuItem>
+          <MenuItem LeftIcon={<IcPencil width={16} height={16} />} onSelect={toggle}>
+            휴지통으로 이동
+          </MenuItem>
+          <MenuItem LeftIcon={<IcPencil width={16} height={16} />} onSelect={toggle}>
+            폴더 전체 다운로드
+          </MenuItem>
+        </MenuList>
+      </MenuRoot>;
+  },
+  args: {
+    onClose: () => {}
+  },
+  argTypes: {}
+}`,...(a=(d=r.parameters)==null?void 0:d.docs)==null?void 0:a.source}}};const _=["Default"];export{r as Default,_ as __namedExportsOrder,S as default};
