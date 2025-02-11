821b0a547a8513dd4fb722d13f392d86975f21ba
kimkyuhong <bobo3766@naver.com>
2025-02-12 01:02:54 +0900

    feat: 블록정보의 상태들 끌어올리기

--- apps/client/src/page/archiving/index/component/TimeBlockBar/BlockInfo/BlockInfo.tsx
+++ apps/client/src/page/archiving/index/component/TimeBlockBar/BlockInfo/BlockInfo.tsx
@@ -1,66 +1,30 @@
 import { Button, CommandButton, DatePicker, Flex, Heading, Input, Text } from '@tiki/ui';
 import { format } from 'date-fns';
 
-import { SyntheticEvent, useEffect, useRef, useState } from 'react';
-
-import { useQueryClient } from '@tanstack/react-query';
-
 import {
   containerStyle,
   periodStyle,
   titleInputStyle,
 } from '@/page/archiving/index/component/TimeBlockBar/BlockInfo/BlockInfo.style';
+import { BlockInfoType } from '@/page/archiving/index/component/TimeBlockBar/TimeBlockBar';
 import { circleStyle } from '@/page/archiving/index/component/TimeBlockBar/TimeBlockBar.style';
 import { BLOCK_ICON } from '@/page/archiving/index/constant/icon';
 
-import { $api } from '@/shared/api/client';
-import { useInitializeTeamId } from '@/shared/hook/common/useInitializeTeamId';
 import { useDrawerContent } from '@/shared/store/drawer';
-import { useTimeBlockId } from '@/shared/store/timeBlockId';
 import { Validate } from '@/shared/util/validate';
 
-type BlockInfoProps = {
+interface BlockInfoProps extends BlockInfoType {
   isEditable: boolean;
   onEditClick: () => void;
-};
+  canSubmit: boolean;
+  setBlockInfo: React.Dispatch<React.SetStateAction<BlockInfoType>>;
+}
 
-const BlockInfo = ({ isEditable, onEditClick }: BlockInfoProps) => {
-  const { name, startDate, endDate, color, blockType } = { ...useDrawerContent() };
-  const timeBlockId = useTimeBlockId();
-  const teamId = useInitializeTeamId();
-
-  const queryClient = useQueryClient();
-
-  const initialData = useRef({ name, startDate, endDate });
-
-  const [blockInfo, setBlockInfo] = useState<{ name: string; startDate: string; endDate: string }>({
-    name: name ?? '',
-    startDate: startDate ?? '',
-    endDate: endDate ?? '',
-  });
-
-  // 초기값과 수정된 데이터 비교해서 폼 제출여부 가능 여부 확인
-  const canSubmit = JSON.stringify(initialData.current) !== JSON.stringify(blockInfo);
-
-  useEffect(() => {
-    setBlockInfo({
-      name: name ?? '',
-      startDate: startDate ?? '',
-      endDate: endDate ?? '',
-    });
-  }, [name, startDate, endDate]);
-
-  const { mutate } = $api.useMutation('patch', '/api/v1/teams/{teamId}/time-block/{timeBlockId}', {
-    onSuccess: () => {
-      onEditClick();
-      queryClient.invalidateQueries({ queryKey: ['get', '/api/v1/teams/{teamId}/timeline'] });
-      initialData.current = blockInfo;
-    },
-  });
-
+const BlockInfo = ({ isEditable, onEditClick, canSubmit, setBlockInfo, name, startDate, endDate }: BlockInfoProps) => {
+  const { color, blockType } = { ...useDrawerContent() };
   const handleDateChange = (start: Date | null, end: Date | null) => {
-    handleblockInfoChange('startDate', format(start ?? blockInfo.startDate, 'yyyy-MM-dd'));
-    handleblockInfoChange('endDate', format(end ?? blockInfo.endDate, 'yyyy-MM-dd'));
+    handleblockInfoChange('startDate', format(start ?? startDate, 'yyyy-MM-dd'));
+    handleblockInfoChange('endDate', format(end ?? endDate, 'yyyy-MM-dd'));
   };
 
   const handleblockInfoChange = (key: string, value: string) => {
@@ -73,26 +37,8 @@ const BlockInfo = ({ isEditable, onEditClick }: BlockInfoProps) => {
     handleblockInfoChange('name', blockName);
   };
 
-  const handleSubmit = (e: SyntheticEvent<Element, Event>) => {
-    e.preventDefault();
-
-    mutate({
-      params: {
-        path: {
-          teamId,
-          timeBlockId,
-        },
-      },
-      body: {
-        name: blockInfo.name,
-        startDate: blockInfo.startDate,
-        endDate: blockInfo.endDate,
-      },
-    });
-  };
-
   return (
-    <form css={containerStyle}>
+    <div css={containerStyle}>
       <Flex styles={{ justify: 'space-between', align: 'center', marginTop: '7.4rem' }}>
         <Flex css={circleStyle(color ?? '')}>
           {BLOCK_ICON.find((icon) => icon.name === blockType)?.icon(color ?? '')}
@@ -104,7 +50,6 @@ const BlockInfo = ({ isEditable, onEditClick }: BlockInfoProps) => {
             variant="fourth"
             commandKey={isEditable ? 'S' : 'E'}
             size="xSmall"
-            onClick={handleSubmit}
             disabled={!canSubmit}>
             저장
           </CommandButton>
@@ -128,13 +73,9 @@ const BlockInfo = ({ isEditable, onEditClick }: BlockInfoProps) => {
 
       <Flex styles={{ direction: 'column', gap: '1rem', marginTop: '1.8rem' }}>
         {isEditable ? (
-          <Input
-            css={titleInputStyle}
-            value={blockInfo.name}
-            onChange={(event) => handleBlockNameChange(event.target.value)}
-          />
+          <Input css={titleInputStyle} value={name} onChange={(event) => handleBlockNameChange(event.target.value)} />
         ) : (
-          <Heading tag="H6">{blockInfo.name}</Heading>
+          <Heading tag="H6">{name}</Heading>
         )}
 
         {isEditable ? (
@@ -142,16 +83,16 @@ const BlockInfo = ({ isEditable, onEditClick }: BlockInfoProps) => {
             onChange={handleDateChange}
             variant="range"
             triggerWidth="100%"
-            defaultSelectedDate={new Date(blockInfo.startDate)}
-            defaultEndDate={new Date(blockInfo.endDate)}
+            defaultSelectedDate={new Date(startDate)}
+            defaultEndDate={new Date(endDate)}
           />
         ) : (
           <Text tag="body6" css={periodStyle}>
-            {blockInfo.startDate} ~ {blockInfo.endDate}
+            {startDate} ~ {endDate}
           </Text>
         )}
       </Flex>
-    </form>
+    </div>
   );
 };
 
--- apps/client/src/page/archiving/index/component/TimeBlockBar/TimeBlockBar.tsx
+++ apps/client/src/page/archiving/index/component/TimeBlockBar/TimeBlockBar.tsx
@@ -1,30 +1,94 @@
 import { IcClose } from '@tiki/icon';
 import { Flex } from '@tiki/ui';
 
-import { useState } from 'react';
+import { SyntheticEvent, useEffect, useRef, useState } from 'react';
+
+import { useQueryClient } from '@tanstack/react-query';
 
 import BlockInfo from '@/page/archiving/index/component/TimeBlockBar/BlockInfo/BlockInfo';
 import TaggedNotes from '@/page/archiving/index/component/TimeBlockBar/TaggedNotes/TaggedNotes';
 import { closeBtnStyle } from '@/page/archiving/index/component/TimeBlockBar/TimeBlockBar.style';
 import UploadedDocuments from '@/page/archiving/index/component/TimeBlockBar/UploadedDocumentss';
 
-import { useDrawerAction } from '@/shared/store/drawer';
+import { $api } from '@/shared/api/client';
+import { useInitializeTeamId } from '@/shared/hook/common/useInitializeTeamId';
+import { useDrawerAction, useDrawerContent } from '@/shared/store/drawer';
+import { useTimeBlockId } from '@/shared/store/timeBlockId';
+
+export type BlockInfoType = {
+  name: string;
+  startDate: string;
+  endDate: string;
+};
 
 const TimeBlockBar = () => {
   const [isEditable, setIsEditable] = useState(false);
 
   const { closeDrawer } = useDrawerAction();
 
+  const timeBlockId = useTimeBlockId();
+  const teamId = useInitializeTeamId();
+
+  const queryClient = useQueryClient();
+  const { name, startDate, endDate } = { ...useDrawerContent() };
+
+  const initialData = useRef({ name, startDate, endDate });
+
+  const [blockInfo, setBlockInfo] = useState({} as BlockInfoType);
+
+  // 초기값과 수정된 데이터 비교해서 폼 제출여부 가능 여부 확인
+  const canSubmit = JSON.stringify(initialData.current) !== JSON.stringify(blockInfo);
+
+  useEffect(() => {
+    setBlockInfo({
+      name: name ?? '',
+      startDate: startDate ?? '',
+      endDate: endDate ?? '',
+    });
+  }, [name, startDate, endDate]);
+
   const handleEditClick = () => {
     setIsEditable((prevState) => !prevState);
+  };
+
+  const { mutate } = $api.useMutation('patch', '/api/v1/teams/{teamId}/time-block/{timeBlockId}', {
+    onSuccess: () => {
+      handleEditClick();
+      queryClient.invalidateQueries({ queryKey: ['get', '/api/v1/teams/{teamId}/timeline'] });
+      initialData.current = blockInfo;
+    },
+  });
+
+  const handleSubmit = (e: SyntheticEvent<Element, Event>) => {
+    e.preventDefault();
+
+    mutate({
+      params: {
+        path: {
+          teamId,
+          timeBlockId,
+        },
+      },
+      body: {
+        name: blockInfo.name,
+        startDate: blockInfo.startDate,
+        endDate: blockInfo.endDate,
+      },
+    });
   };
 
   return (
     <section>
       <IcClose width={16} height={16} css={closeBtnStyle} onClick={closeDrawer} />
 
-      <Flex styles={{ direction: 'column', gap: '3.6rem' }}>
-        <BlockInfo isEditable={isEditable} onEditClick={handleEditClick} />
+      <Flex tag="form" styles={{ direction: 'column', gap: '3.6rem' }} onSubmit={handleSubmit}>
+        <BlockInfo
+          isEditable={isEditable}
+          onEditClick={handleEditClick}
+          canSubmit={canSubmit}
+          setBlockInfo={setBlockInfo}
+          {...blockInfo}
+        />
         <TaggedNotes isEditable={isEditable} />
         <UploadedDocuments isEditable={isEditable} />
       </Flex>
