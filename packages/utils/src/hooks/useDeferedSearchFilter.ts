f1bbba3b1b161d0fcb3d6a46266fccade4cc70df
wuzoo <wndyd9911@gmail.com>
2025-02-12 23:17:56 +0900

    refactor: 한글 검색 허용

--- packages/utils/src/hooks/useDeferedSearchFilter.ts
+++ packages/utils/src/hooks/useDeferedSearchFilter.ts
@@ -14,16 +14,15 @@ export const useDeferredSearchFilter = <T extends object>(list: Data<T>[], value
   /** 상태 업데이트 시 이전 값과 지연된 값이 일치하지 않으면 isStale (아직 최신 데이터가 로드 전임을 UI로 나타내기 위함) */
   const isStale = value !== deferredValue;
 
-  const filteredData = list.filter((item) =>
-    Object.values(item).some((value) => {
-      if (typeof value === "object") {
-        /** deep comparing 대신 객체를 문자열로 만들어 검사 */
-        return JSON.stringify(Object.values(value!)).includes(deferredValue);
-      } else {
-        return String(value).includes(deferredValue);
-      }
-    }),
-  );
+  const filteredData = list.filter((item) => {
+    if (typeof item === "object") {
+      return Object.values(item).some((value) =>
+        String(value).replace(/\s+/g, "").normalize("NFC").includes(deferredValue),
+      );
+    } else {
+      return String(value).replace(/\s+/g, "").normalize("NFC").includes(deferredValue);
+    }
+  });
 
   return { deferredValue, filteredData, isStale };
 };
