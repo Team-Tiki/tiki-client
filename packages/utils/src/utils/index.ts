8ccc25d171a55235d1db3b91a4290116f36bacba
wuzoo <wndyd9911@gmail.com>
2025-02-13 02:10:26 +0900

    feat: isNil

--- packages/utils/src/utils/index.ts
+++ packages/utils/src/utils/index.ts
@@ -1,3 +1,7 @@
 export const hasKeyInObject = <T extends object>(object: T, key: string | number | symbol): key is keyof T => {
   return Object.hasOwn(object, key);
 };
+
+export const isNil = <T>(value: T | undefined | null): value is null | undefined => {
+  return value === null || value === undefined;
+};
