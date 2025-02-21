12cfa938676fbc85b59bf6d64a0fd5893acf1cc5
bowoon1216 <happyanne200212@gmail.com>
2025-02-21 16:16:33 +0900

    fix: git storybook build error solved

--- apps/client/.storybook/main.ts
+++ apps/client/.storybook/main.ts
@@ -1,16 +1,17 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 import { dirname, join, resolve } from 'path';
-import { fileURLToPath } from 'url';
+//import { fileURLToPath } from 'url';
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
+/*
 function getAbsolutePath(value: string): any {
   return dirname(import.meta.resolve(join(value, 'package.json')));
-}
+}*/
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+//const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
