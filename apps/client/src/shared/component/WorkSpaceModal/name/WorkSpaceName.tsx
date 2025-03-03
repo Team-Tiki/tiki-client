001c04005668494644046b4b4c733d0f63aadf0c
namdaeun <nde40345@gmail.com>
2025-03-03 16:08:43 +0900

    feat: workspace 팀명 글자수 제한

--- apps/client/src/shared/component/WorkSpaceModal/name/WorkSpaceName.tsx
+++ apps/client/src/shared/component/WorkSpaceModal/name/WorkSpaceName.tsx
@@ -4,21 +4,23 @@ import { useState } from 'react';
 
 import { Modal } from '@/shared/component/Modal';
 import { inputWrapperStyle } from '@/shared/component/WorkSpaceModal/name/WorkSpaceName.style';
+import { MAX_TEAM_NAME, WORKSPACE_MODAL } from '@/shared/constant';
 import { useFunnel } from '@/shared/hook/common/useFunnel';
 import { useWorkSpaceContext } from '@/shared/hook/common/useWorkSpaceContext';
 
 const WorkSpaceName = () => {
-  const [inputValue, setInputValue] = useState('');
-
-  const { setFormData } = useWorkSpaceContext();
+  const { formData, setFormData } = useWorkSpaceContext();
   const { nextStep } = useFunnel();
+
+  const [inputValue, setInputValue] = useState(formData.name);
+
+  const isDisabled = inputValue.trim().length === 0;
+  const isNameError = inputValue.length > MAX_TEAM_NAME;
 
   const handleNext = () => {
     setFormData({ name: inputValue });
     nextStep();
   };
-
-  const isDisabled = inputValue.trim().length === 0;
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
@@ -32,6 +34,9 @@ const WorkSpaceName = () => {
             placeholder="팀, 동아리, 조직 이름 입력"
             value={inputValue}
             onChange={handleInputChange}
+            isError={isNameError}
+            supportingText={isNameError ? WORKSPACE_MODAL.NAME : ''}
+            maxLength={MAX_TEAM_NAME}
             css={inputWrapperStyle}
           />
         </div>
