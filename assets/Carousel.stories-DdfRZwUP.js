ea887676b12cb89b08d9280ce6ef78a5361264c7
Bowoon1216 <Bowoon1216@users.noreply.github.com>
2025-03-15 03:55:53 +0000

    deploy: 61a7fa0db92641eb636333fb7ed8a34240fd9f68

---
+++ assets/Carousel.stories-DdfRZwUP.js
@@ -0,0 +1,35 @@
+import{a as t,j as n}from"./emotion-react-jsx-runtime.browser.esm-BZbqHjf_.js";import{c as m}from"./emotion-react.browser.esm-BF0Sq6j_.js";import{s as h,t as g}from"./index-B-uMTWfr.js";import{o as l,n as u,s as p,t as f,r as w}from"./index-CwVEUIMc.js";import"./iframe-D4GlSkuB.js";import"./jsx-runtime-5dgXpFJU.js";import"./index-CLAFn_Ue.js";import"./_commonjsHelpers-GGoETqZm.js";import"./index-jVWMfZfp.js";import"../sb-preview/runtime.js";(function(){try{var r=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},e=new r.Error().stack;e&&(r._sentryDebugIds=r._sentryDebugIds||{},r._sentryDebugIds[e]="774f18d2-28de-4a6c-8013-62f7cd1df34d",r._sentryDebugIdIdentifier="sentry-dbid-774f18d2-28de-4a6c-8013-62f7cd1df34d")}catch{}})();const b=""+new URL("../img/advBannerClub1.png",import.meta.url).href,y=""+new URL("../img/advBannerClub2.png",import.meta.url).href,v=""+new URL("../img/advBannerClub3.png",import.meta.url).href,E={title:"Common/Carousel",component:l,parameters:{layout:"centered"},tags:["autodocs"],args:{children:[]},decorators:[r=>t("div",{style:{width:"100vw"},children:t(r,{})})]},C=m({"::before":{content:'""',position:"absolute",top:0,left:0,width:"16rem",height:"100%",background:u.colors.black_shade}}),s=m({position:"absolute",top:0,bottom:0,display:"flex",justifyContent:"center",alignItems:"center",margin:"auto 0",width:"3.2rem",height:"3.2rem",zIndex:u.zIndex.overlayBottom,border:"none",borderRadius:"16px",background:"rgb(255,255,255,0.3)",cursor:"pointer"}),i={render:()=>{const r=Array.from({length:10}).map((e,o)=>o+1);return t(l,{autoLoop:!0,renderedLeftArrow:e=>t("button",{onClick:e,css:[s,{left:"1.6rem"}],children:t(h,{width:"1.6rem",height:"1.6rem"})}),renderedRightArrow:e=>t("button",{onClick:e,css:[s,{right:"1.6rem"}],children:t(g,{width:"1.6rem",height:"1.6rem"})}),children:r.map((e,o)=>n(w,{css:C,index:o,children:[n("div",{css:{position:"absolute",padding:"3.2rem"},children:[n(p,{css:{color:"white"},children:[e,"번째 슬라이드"]}),t(f,{css:{width:"200px"},variant:"primary",children:"이동하기"})]}),t("img",{css:{objectFit:"cover"},src:o%3===0?b:o%3===1?y:v,alt:`img${e}`})]},e))})}};var a,d,c;i.parameters={...i.parameters,docs:{...(a=i.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: () => {
+    const arr = Array.from({
+      length: 10
+    }).map((_, i) => i + 1);
+    return <Carousel autoLoop={true} renderedLeftArrow={(onClick: (e: MouseEvent<HTMLButtonElement>) => void) => <button onClick={onClick} css={[arrowStyle, {
+      left: '1.6rem'
+    }]}>
+            <IcArrowLeftWhite width="1.6rem" height="1.6rem" />
+          </button>} renderedRightArrow={(onClick: (e: MouseEvent<HTMLButtonElement>) => void) => <button onClick={onClick} css={[arrowStyle, {
+      right: '1.6rem'
+    }]}>
+            <IcArrowRightWhite width="1.6rem" height="1.6rem" />
+          </button>}>
+        {arr.map((num, idx) => <CarouselItem css={shadowStyle} key={num} index={idx}>
+            <div css={{
+          position: 'absolute',
+          padding: '3.2rem'
+        }}>
+              <Heading css={{
+            color: 'white'
+          }}>{num}번째 슬라이드</Heading>
+              <Button css={{
+            width: '200px'
+          }} variant="primary">
+                이동하기
+              </Button>
+            </div>
+            <img css={{
+          objectFit: 'cover'
+        }} src={idx % 3 === 0 ? img1 : idx % 3 === 1 ? img2 : img3} alt={\`img\${num}\`} />
+          </CarouselItem>)}
+      </Carousel>;
+  }
+}`,...(c=(d=i.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};const S=["Default"];export{i as Default,S as __namedExportsOrder,E as default};
--- assets/DeleteModal.stories-DyBsIgC7.js
+++
@@ -1,9 +0,0 @@
-import{j as p,a as r,F as i}from"./emotion-react-jsx-runtime.browser.esm-CRVNaqHF.js";import{M as s}from"./ModalFunnel-DhCov7J5.js";import{u as l}from"./modal-DKkxhU-z.js";import"./iframe-BEuLWfLr.js";import"./jsx-runtime-CVIRmt7j.js";import"./index-Bj5HBvKk.js";import"./_commonjsHelpers-DeSiNFYy.js";import"./index-CBihQNVD.js";import"./index-97XqX8Ct.js";import"./index-B-uMTWfr.js";import"./QueryClientProvider-DB4VPLnE.js";import"./path-zBoMYNd5.js";import"./emotion-react.browser.esm-Ecb5K7YF.js";import"./ellipsis-CbmJ5wTo.js";import"./index-Df-F3U1f.js";import"./date-B7o9b3ho.js";import"./file-DStkZ3H7.js";import"../sb-preview/runtime.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},o=new e.Error().stack;o&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[o]="c87c16b1-72a0-49d2-82ea-9d30a800c5b0",e._sentryDebugIdIdentifier="sentry-dbid-c87c16b1-72a0-49d2-82ea-9d30a800c5b0")}catch{}})();const E={title:"Shared/Modal/Deleted",component:s,parameters:{layout:"centered"}},t={render:()=>{const e=l();return p(i,{children:[r("button",{onClick:()=>e("deleted"),children:"Open Trash Deleted Modal"}),r(s,{})]})}};var n,d,a;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
-  render: () => {
-    const openModal = useOpenModal();
-    return <>
-        <button onClick={() => openModal('deleted')}>Open Trash Deleted Modal</button>
-        <ModalFunnel />
-      </>;
-  }
-}`,...(a=(d=t.parameters)==null?void 0:d.docs)==null?void 0:a.source}}};const S=["Deleted"];export{t as Deleted,S as __namedExportsOrder,E as default};
