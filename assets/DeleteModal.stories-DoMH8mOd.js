71d14cc4cdc52b736b645c56c1cb653d9c904668
rtttr1 <rtttr1@users.noreply.github.com>
2025-03-29 08:33:45 +0000

    deploy: cdf7f8b388d0e8fb20dee9cb5e0449e6c4d1d86c

--- assets/DeleteModal.stories-DoMH8mOd.js
+++
@@ -1,9 +0,0 @@
-import{j as p,a as r,F as i}from"./emotion-react-jsx-runtime.browser.esm-MHVvHxQU.js";import{M as s}from"./ModalFunnel-Drv11pML.js";import{u as l}from"./modal-D4UIn-Ib.js";import"./iframe-C3iAWkxp.js";import"./jsx-runtime-DPjXxOIO.js";import"./index-ld75EQ8z.js";import"./_commonjsHelpers-Dkn7kFgy.js";import"./index-2Z6D3Xs3.js";import"./index-L7qVR4lt.js";import"./index-B-uMTWfr.js";import"./QueryClientProvider-KujTluPV.js";import"./path-P6ZnWgW6.js";import"./emotion-react.browser.esm-_q-vksow.js";import"./ellipsis-Ci53S7nF.js";import"./index-CmjrCQFG.js";import"./date-o2ehZnXm.js";import"./file-C-KfVEx8.js";import"../sb-preview/runtime.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},o=new e.Error().stack;o&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[o]="c87c16b1-72a0-49d2-82ea-9d30a800c5b0",e._sentryDebugIdIdentifier="sentry-dbid-c87c16b1-72a0-49d2-82ea-9d30a800c5b0")}catch{}})();const E={title:"Shared/Modal/Deleted",component:s,parameters:{layout:"centered"}},t={render:()=>{const e=l();return p(i,{children:[r("button",{onClick:()=>e("deleted"),children:"Open Trash Deleted Modal"}),r(s,{})]})}};var n,d,a;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
-  render: () => {
-    const openModal = useOpenModal();
-    return <>
-        <button onClick={() => openModal('deleted')}>Open Trash Deleted Modal</button>
-        <ModalFunnel />
-      </>;
-  }
-}`,...(a=(d=t.parameters)==null?void 0:d.docs)==null?void 0:a.source}}};const S=["Deleted"];export{t as Deleted,S as __namedExportsOrder,E as default};
---
+++ assets/TabButton.stories-CS1Gi_Fn.js
@@ -0,0 +1,11 @@
+import{d}from"./index-CH8S3uAY.js";import"./iframe-DdmgaAPU.js";import"./index-wBPCgvhT.js";import"./_commonjsHelpers-UriGOo5i.js";import"./jsx-runtime-H8DOynrT.js";import"./index-X03-L1Cb.js";import"../sb-preview/runtime.js";(function(){try{var r=typeof window<"u"?window:typeof global<"u"?global:typeof self<"u"?self:{},o=new r.Error().stack;o&&(r._sentryDebugIds=r._sentryDebugIds||{},r._sentryDebugIds[o]="e3673317-e041-4216-8da0-89377863d800",r._sentryDebugIdIdentifier="sentry-dbid-e3673317-e041-4216-8da0-89377863d800")}catch{}})();const g={title:"Common/TabButton",component:d,parameters:{layout:"centered"},tags:["autodocs"],args:{children:"TabButton",tabId:0,isSelected:!1}},t={args:{variant:"round"},argTypes:{variant:{control:"radio",options:["round"]}}};var e,n,a;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    variant: 'round'
+  },
+  argTypes: {
+    variant: {
+      control: 'radio',
+      options: ['round']
+    }
+  }
+}`,...(a=(n=t.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const b=["RoundTabButton"];export{t as RoundTabButton,b as __namedExportsOrder,g as default};
